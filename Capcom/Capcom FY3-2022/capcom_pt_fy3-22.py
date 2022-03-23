5ffddb92174960562b759c67d3d02d754384b39d
r134x7 <98866524+r134x7@users.noreply.github.com>
2022-03-24 01:25:16 +1100

    Added capcom platinum titles list

---
+++ Capcom/Capcom FY3-2022/capcom_pt_fy3-22.py
@@ -0,0 +1,210 @@
+from operator import itemgetter
+
+header_names1 = [  " Capcom - Platinum Titles                    ", " Release Date ", " Platform                                    ", "   Units   ", ] #header
+row_names0 = [" 1st Quarter ", " 2nd Quarter ", " 3rd Quarter ", " 4th Quarter ", " FY3/22 Cumulative ", " Life-To-Date "] # row names
+row_names1 = ['{0: <106}'.format(elem) for elem in row_names0] # format width
+
+x = 3 # current earnings quarter, set to 1, 2, 3, or 4
+x2 = 1 # to print all titles set to 0, to print only titles have figures this fy set to 1.
+
+#Input the cumulative figure for each quarter, [0] release date, [1] title name, [2] platforms, [3] Quarter 1, [4] Quarter 2, [5] Quarter 3, [6] Quarter 4, [7] LTD figure at end of the last fiscal year, [8] miscellaneous
+title1   = [" Jan 2018 ", " Monster Hunter: World ", " PS4, Xbox One, PC, DL ", 17.3, 17.5, 17.80, 17.80, 17.10, 1,]
+title2   = [" Jan 2017 ", " RESIDENT EVIL 7 biohazard ", " PS4, Xbox One, PC, DL ", 9.8, 10.2, 10.60, 10.60, 9.0, 0,]
+title3   = [" Jan 2019 ", " Resident Evil 2 ", " PS4, Xbox One, PC, DL ", 8.6, 8.9, 9.30, 9.30, 8.1, 0,]
+title4   = [" Sep 2019 ", " Monster Hunter World: Iceborne ", " PS4, Xbox One, PC, DL ", 8.2, 8.5, 8.80, 8.80, 7.7, 0,]
+title5   = [" Mar 2009 ", " Resident Evil 5 ", " PS3, Xbox 360, DL ", 7.9, 8.0, 8.10, 8.10, 7.8, 0,]
+title6   = [" Oct 2012 ", " Resident Evil 6 ", " PS3, Xbox 360, DL ", 7.9, 8.0, 8.10, 8.10, 7.8, 0,]
+title7   = [" Mar 2021 ", " Monster Hunter Rise ", " NSW, DL ", 7.3, 7.5, 7.70, 7.70, 4.8, 0,]
+title8   = [" Jun 1992 ", " Street Fighter II ", " SNES ", 6.30, 6.30, 6.30, 6.30, 6.30, 0,]
+title9   = [" Feb 2016 ", " Street Fighter V ", " PS4, PC, DL ", 5.8, 6.0, 6.10, 6.10, 5.5, 0,]
+title10	 = [" May 2021 ", " Resident Evil Village ", " PS4, PS5, Xbox One, XSX, PC, DL ", 4.5, 4.8, 5.70, 5.70, 0, 0,]
+title11	 = [" Jan 1998 ", " Resident Evil 2 ", " PS ", 4.96, 4.96, 4.96, 4.96, 4.96, 0,]
+title12	 = [" Apr 2020 ", " Resident Evil 3 ", " PS4, Xbox One, PC, DL ", 4.4, 4.6, 4.90, 4.90, 4.0, 0,]
+title13	 = [" Dec 2010 ", " Monster Hunter Freedom 3 ", " PSP, DL ", 4.90, 4.90, 4.90, 4.90, 4.90, 0,]
+title14	 = [" Mar 2019 ", " Devil May Cry 5 ", " PS4, Xbox One, PC, DL ", 4.5, 4.7, 4.80, 4.80, 4.30, 0,]
+title15	 = [" Mar 2017 ", " Monster Hunter Generations Ultimate ", " 3DS, NSW, DL ", 4.3, 4.3, 4.40, 4.40, 4.30, 0,]
+title16	 = [" Nov 2015 ", " Monster Hunter Generations ", " 3DS, DL ", 4.30, 4.30, 4.30, 4.30, 4.30, 0,]
+title17	 = [" Oct 2014 ", " Monster Hunter 4 Ultimate ", " 3DS, DL ", 4.20, 4.20, 4.20, 4.20, 4.20, 0,]
+title18	 = [" Sep 2013 ", " Monster Hunter 4 ", " 3DS, DL ", 4.10, 4.10, 4.10, 4.10, 4.10, 0,]
+title19	 = [" Jul 1993 ", " Street Fighter II Turbo ", " SNES ", 4.10, 4.10, 4.10, 4.10, 4.10, 0,]
+title20	 = [" Mar 2008 ", " Monster Hunter Freedom Unite ", " PSP, DL ", 3.80, 3.80, 3.80, 3.80, 3.80, 0,]
+title21	 = [" Sep 1999 ", " Resident Evil 3 Nemesis ", " PS ", 3.50, 3.50, 3.50, 3.50, 3.50, 0,]
+title22	 = [" Feb 2009 ", " Street Fighter IV ", " PS3, Xbox 360, DL ", 3.40, 3.40, 3.40, 3.40, 3.40, 0,]
+title23	 = [" Nov 2014 ", " Resident Evil ", " PS3, DL ", 3.1, 3.2, 3.30, 3.30, 3.0, 0,]
+title24	 = [" Jan 2016 ", " Resident Evil 0: HD Remaster ", " PS3, PS4, Xbox One, PC, DL ", 3.0, 3.1, 3.20, 3.20, 2.9, 0,]
+title25	 = [" Sep 2010 ", " Dead Rising 2 ", " PS3, Xbox 360, PC, DL ", 3.10, 3.10, 3.10, 3.10, 3.10, 0,]
+title26	 = [" Jan 2008 ", " Devil May Cry 4 ", " PS3, Xbox 360, DL ", 3.0, 3.0, 3.0, 3.0, 3.0, 0,]
+title27	 = [" Nov 2013 ", " Dead Rising 3 ", " Xbox One, DL ", 2.9, 3.0, 3.00, 3.00, 2.9, 0,]
+title28	 = [" Mar 2015 ", " Resident Evil Revelations 2 ", " PS3, PS4, Xbox 360, Xbox One, PC, DL ", 2.8, 2.8, 2.90, 2.90, 2.7, 0,]
+title29	 = [" Mar 1996 ", " Resident Evil ", " PS ", 2.75, 2.75, 2.75, 2.75, 2.75, 0,]
+title30	 = [" Dec 2011 ", " Monster Hunter 3 Ultimate ", " 3DS, DL ", 2.60, 2.60, 2.60, 2.60, 2.60, 0,]
+title31	 = [" Mar 2012 ", " Resident Evil: Operation Raccoon City ", " PS3, Xbox 360, DL ", 2.6, 2.60, 2.60, 2.60, 2.5, 0,]
+title32	 = [" Jan 2013 ", " DmC Devil May Cry ", " PS3, Xbox 360, DL ", 2.50, 2.50, 2.60, 2.60, 2.50, 0,]
+title33	 = [" Aug 2016 ", " Resident Evil 4 ", " PS4, Xbox One, DL ", 2.30, 2.4, 2.50, 2.50, 2.20, 0,]
+title34	 = [" Feb 2007 ", " Monster Hunter Freedom 2 ", " PSP ", 2.40, 2.40, 2.40, 2.40, 2.40, 0,]
+title35	 = [" Jun 2016 ", " Resident Evil 5 ", " PS4, Xbox One, DL ", 2.20, 2.3, 2.40, 2.40, 2.10, 0,]
+title36	 = [" Jul 1999 ", " Dino Crisis ", " PS ", 2.40, 2.40, 2.40, 2.40, 2.40, 0,]
+title37	 = [" Mar 2016 ", " Resident Evil 6 ", " PS4, Xbox One, DL ", 2.30, 2.30, 2.30, 2.30, 2.30, 0,]
+title38	 = [" Feb 2010 ", " Resident Evil 5: Gold Edition ", " PS3, Xbox 360, DL ", 2.30, 2.30, 2.30, 2.30, 2.30, 0,]
+title39	 = [" Dec 2005 ", " Resident Evil 4 ", " PS2 ", 2.30, 2.30, 2.30, 2.30, 2.30, 0,]
+title40	 = [" Apr 2013 ", " Dragon’s Dogma: Dark Arisen ", " PS3, Xbox 360, DL ", 2.20, 2.20, 2.20, 2.20, 2.10, 0,]
+title41	 = [" Feb 2011 ", " Marvel vs. Capcom 3: Fate of Two Worlds ", " PS3, Xbox 360 ", 2.20, 2.20, 2.20, 2.20, 2.20, 0,]
+title42	 = [" May 2013 ", " Resident Evil Revelations ", " PS3, Xbox 360, Wii U, PC, DL ", 2.10, 2.10, 2.20, 2.20, 2.10, 0,]
+title43	 = [" Aug 2001 ", " Devil May Cry ", " PS2 ", 2.16, 2.16, 2.16, 2.16, 2.16, 0,]
+title44	 = [" Mar 2002 ", " Onimusha 2: Samurai’s Destiny ", " PS2 ", 2.10, 2.10, 2.10, 2.10, 2.10, 0,]
+title45	 = [" May 2018 ", " Street Fighter 30th Anniversary Collection ", " PS4, Xbox One, NSW, PC, DL ", 1.9, 2.0, 2.10, 2.10, 1.8, 0,]
+title46	 = [" May 2010 ", " Lost Planet 2 ", " PS3, Xbox 360, DL ", 2.10, 2.10, 2.10, 2.10, 2.0, 0,]
+title47	 = [" Jan 2001 ", " Onimusha: Warlords ", " PS2 ", 2.02, 2.02, 2.02, 2.02, 2.02, 0,]
+title48	 = [" May 2007 ", " Resident Evil 4 Wii edition ", " Wii, DL ", 2.00, 2.00, 2.00, 2.00, 2.00, 0,]
+title49	 = [" Jun 1994 ", " Super Street Fighter II ", " SNES ", 2.00, 2.00, 2.00, 2.00, 2.00, 0,]
+title50	 = [" Aug 2009 ", " Monster Hunter Tri ", " Wii ", 1.90, 1.90, 1.90, 1.90, 1.90, 0,]
+title51	 = [" Apr 2010 ", " Super Street Fighter IV ", " PS3, Xbox 360, DL ", 1.90, 1.90, 1.90, 1.90, 1.90, 0,]
+title52	 = [" Mar 2012 ", " Street Fighter X Tekken ", " PS3, Xbox 360, DL ", 1.90, 1.90, 1.90, 1.90, 1.80, 0,]
+title53	 = [" Aug 2006 ", " Dead Rising ", " Xbox 360, DL ", 1.80, 1.80, 1.80, 1.80, 1.80, 0,]
+title54	 = [" Jun 2015 ", " Devil May Cry 4 Special Edition ", " PS4, Xbox One, DL ", 1.70, 1.80, 1.80, 1.80, 1.60, 0,]
+title55	 = [" Sep 2017 ", " Marvel vs. Capcom: Infinite ", " PS4, Xbox One, PC, DL ", 1.70, 1.70, 1.80, 1.80, 1.70, 0,]
+title56	 = [" Dec 2017 ", " Okami HD ", " PS4, Xbox One, NSW, DL ", 1.70, 1.70, 1.80, 1.80, 1.60, 0,]
+title57	 = [" Feb 2014 ", " Resident Evil 4: Ultimate HD Edition ", " PC, DL ", 1.60, 1.70, 1.80, 1.80, 1.40, 0,]
+title58	 = [" Oct 2017 ", " Dragon’s Dogma: Dark Arisen ", " PS4, Xbox One, DL ", 1.60, 1.60, 1.70, 1.70, 1.60, 0,]
+title59	 = [" Jan 2003 ", " Devil May Cry 2 ", " PS2 ", 1.70, 1.70, 1.70, 1.70, 1.70, 0,]
+title60	 = [" Sep 1993 ", " Street Fighter II’ Special Champion Edition ", " MD ", 1.65, 1.65, 1.65, 1.65, 1.65, 0,]
+title61	 = [" Jun 1986 ", " Ghosts’n Goblins ", " NES ", 1.64, 1.64, 1.64, 1.64, 1.64, 0,]
+title62	 = [" Dec 2006 ", " Lost Planet Extreme Condition ", " Xbox 360, DL ", 1.60, 1.60, 1.60, 1.60, 1.60, 0,]
+title63	 = [" Jan 2005 ", " Resident Evil 4 ", " GC ", 1.60, 1.60, 1.60, 1.60, 1.60, 0,]
+title64	 = [" Feb 2004 ", " Onimusha 3: Demon Siege ", " PS2 ", 1.52, 1.52, 1.52, 1.52, 1.52, 0,]
+title65	 = [" Dec 1988 ", " Mega Man 2 ", " NES ", 1.51, 1.51, 1.51, 1.51, 1.51, 0,]
+title66	 = [" Aug 2014 ", " Ultra Street Fighter IV ", " PS3, Xbox 360, PC, DL ", 1.50, 1.50, 1.50, 1.50, 1.50, 0,]
+title67	 = [" Nov 2013 ", " DuckTales: Remastered ", " PS3, Xbox 360, Wii U, DL ", 1.40, 1.40, 1.50, 1.50, 1.40, 0,]
+title68	 = [" Dec 1990 ", " Final Fight ", " SNES ", 1.48, 1.48, 1.48, 1.48, 1.48, 0,]
+title69	 = [" Dec 2003 ", " Resident Evil Outbreak ", " PS2 ", 1.45, 1.45, 1.45, 1.45, 1.45, 0,]
+title70	 = [" Jul 2021 ", " Monster Hunter Stories 2: Wings of Ruin ", " NSW, PC, DL ", 0, 1.30, 1.40, 1.40, 0, 0,]
+title71	 = [" Oct 2010 ", " Dead Rising 2 Off The Record ", " PS3, Xbox 360, DL ", 1.40, 1.40, 1.40, 1.40, 1.40, 0,]
+title72	 = [" Oct 2018 ", " Mega Man 11 ", " PS4, Xbox One, NSW, DL ", 1.30, 1.40, 1.40, 1.40, 1.30, 0,]
+title73	 = [" Feb 2019 ", " Phoenix Wright: Ace Attorney Trilogy ", " PS4, NSW, DL ", 1.20, 1.30, 1.40, 1.40, 1.10, 0,]
+title74	 = [" Jul 2009 ", " Marvel vs. Capcom 2: New Age Of Heroes ", " DL (PS3, Xbox 360) ", 1.40, 1.40, 1.40, 1.40, 1.40, 0,]
+title75	 = [" Mar 2001 ", " Resident Evil Code: Veronica X ", " PS2 ", 1.40, 1.40, 1.40, 1.40, 1.40, 0,]
+title76	 = [" Mar 2002 ", " Resident Evil ", " GC ", 1.35, 1.35, 1.35, 1.35, 1.35, 0,]
+title77	 = [" Dec 2003 ", " Mega Man Battle Network 4 ", " GBA ", 1.35, 1.35, 1.35, 1.35, 1.35, 0,]
+title78	 = [" May 2012 ", " Dragon’s Dogma ", " PS3, Xbox 360 ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title79	 = [" Dec 2005 ", " Monster Hunter Freedom ", " PSP, DL ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title80	 = [" Jun 2013 ", " Remember Me ", " PS3, Xbox 360, PC, DL ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title81	 = [" Feb 2011 ", " Super Street Fighter IV 3D Edition ", " 3DS, DL ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title82	 = [" Nov 2007 ", " Resident Evil: The Umbrella Chronicles ", " Wii ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title83	 = [" Mar 2017 ", " Ultimate Marvel vs. Capcom 3 ", " PS4, Xbox One, DL ", 1.20, 1.20, 1.30, 1.30, 1.10, 0,]
+title84	 = [" Feb 2005 ", " Devil May Cry 3 ", " PS2 ", 1.30, 1.30, 1.30, 1.30, 1.30, 0,]
+title85	 = [" Nov 2002 ", " Resident Evil 0 ", " GC ", 1.25, 1.25, 1.25, 1.25, 1.25, 0,]
+title86	 = [" Dec 2016 ", " Dead Rising 4 ", " Xbox One, PC, DL	", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title87	 = [" Mar 2012 ", " Resident Evil 4 ", " DL (PS3, Xbox 360) ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title88	 = [" Aug 2015 ", " Mega Man Legacy Collection ", " PS4, Xbox One, DL ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title89	 = [" Sep 2000 ", " Dino Crisis 2 ", " PS ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title90	 = [" Nov 2011 ", " Ultimate Marvel vs. Capcom 3 ", " PS3, Xbox 360, DL ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title91	 = [" Sep 2016 ", " Dead Rising ", " PS4, Xbox One, DL ", 1.20, 1.20, 1.20, 1.20, 1.10, 0,]
+title92	 = [" Mar 2015 ", " DMC Devil May Cry Definitive Edition ", " PS4, Xbox One, DL ", 1.20, 1.20, 1.20, 1.20, 1.10, 0,]
+title93	 = [" Mar 2012 ", " Devil May Cry HD Collection ", " PS3, Xbox 360, DL ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title94	 = [" Aug 1998 ", " Resident Evil Director’s Cut Dual Shock ", " PS ", 1.20, 1.20, 1.20, 1.20, 1.20, 0,]
+title95	 = [" Dec 1993 ", " Mega Man X ", " SNES ", 1.16, 1.16, 1.16, 1.16, 1.16, 0,]
+title96	 = [" Feb 2000 ", " Resident Evil Code: Veronica ", " DC ", 1.14, 1.14, 1.14, 1.14, 1.14, 0,]
+title97	 = [" Sep 1986 ", " Commando ", " NES ", 1.14, 1.14, 1.14, 1.14, 1.14, 0,]
+title98	 = [" Sep 1997 ", " Resident Evil Director’s Cut ", " PS ", 1.13, 1.13, 1.13, 1.13, 1.13, 0,]
+title99	 = [" Jun 2011 ", " Super Street Fighter IV Arcade Edition ", " PS3, Xbox 360, DL ", 1.10, 1.10, 1.10, 1.10, 1.10, 0,]
+title100 = [" Jun 2009 ", " Bionic Commando ", " PS3, Xbox 360, PC, DL ", 1.10, 1.10, 1.10, 1.10, 1.10, 0,]
+title101 = [" Nov 2017 ", " Resident Evil Revelations Collection ", " NSW, DL ", 1.0, 1.0, 1.10, 1.10, 0, 0,]
+title102 = [" Oct 1991 ", " Super Ghouls’n Ghosts ", " SNES ", 1.09, 1.09, 1.09, 1.09, 1.09, 0,]
+title103 = [" Sep 1990 ", " Mega Man 3 ", " NES ", 1.08, 1.08, 1.08, 1.08, 1.08, 0,]
+title104 = [" May 1993 ", " Final Fight 2 ", " SNES ", 1.03, 1.03, 1.03, 1.03, 1.03, 0,]
+title105 = [" Feb 2014 ", " Strider ", " DL (PS3, PS4, Xbox 360, Xbox One, PC) ", 1.00, 1.00, 1.00, 1.00, 1.00, 0,]
+title106 = [" Dec 1998 ", " Street Fighter Alpha 3 ", " PS ", 1.00, 1.00, 1.00, 1.00, 1.00, 0,]
+title107 = [" Feb 2006 ", " Devil May Cry 3 Special Edition ", " PS2 ", 1.00, 1.00, 1.00, 1.00, 1.00, 0,] 
+
+misc1 = ["* Excludes shipments of Monster Hunter World: Iceborne Master Edition"]
+
+misc2 = ["NES: Nintendo Entertainment System, SNES: Super Nintendo Entertainment System,"]
+misc2 = ["GBA: Game Boy Advance, 3DS: Nintendo 3DS, GC: Nintendo GameCube, MD: Mega Drive/Genesis,"]
+misc3 = ["DC: DreamCast, PS: PlayStation®, PS2: PlayStation®2, PS3: PlayStation®3, PS4: PlayStation®4,"]
+misc4 = ["PSP: PSP® (PlayStation®Portable), PSV: PlayStation®Vita, DL: Full-game download, NSW: Nintendo Switch"]
+
+misc5 = ["DL (Full-game download) includes all digital download units for each platform."]
+
+lb1 = "###" # line break
+
+capture1 = [] # empty array
+
+for i in range(107): # for loop to get each title variable into an array
+    take1 = "title" + str(i+1) # convert to string
+    take2 = globals()[take1] # changes the string to a variable
+    capture1.append(take2) # puts the variable into the array
+
+capture2 = sorted(capture1, reverse=True, key=itemgetter(6)) # the titles are sorted by the global fy number of the 4th quarter.
+
+for i in range (len(capture2)): # for adding in ranks after sorting
+    rank = " Rank " + str(i+1) + " "
+    rank1 = '{0: <11}'.format(rank)
+    capture2[i].append(rank1)
+
+def c_print (y): # y: use title1, title2, etc
+
+    y1 = [y[3] - y[7], y[4]  - y[3], y[5] - y[4], y[6] - y[5]] #quarterly calculation
+
+    y1.append(y1[0] + y1[1] + y1[2] + y1[3] ) #adds fy cumulative to array becomes y1[4]
+    y1.append(y[7] + y1[0] + y1[1] + y1[2] + y1[3]) #adds LTD becomes y1[5]
+    y[0] = '{0: <14}'.format(y[0])
+    y[1] = '{0: <45}'.format(y[1])
+    y[2] = '{0: <45}'.format(y[2])
+    y2 = [ '{:.2f}M '.format(elem) for elem in y1] #formats all integers to string to add ¥ and M
+    y3 = [ '{0: >11}'.format(elem) for elem in y2] #format width
+
+    if x2 == 0:
+        print("|" + y[1] + "|" + y[0] + "|" + y[2] + "|" + y[9] + "|") # title name and rank
+        if y1[4] != 0:
+            print("+----------------------------------------------------------------------------------------------------------------------+")
+        for i in range(x):
+            if y1[i] != 0:
+                print("|"  +  row_names1[i] +  "|" + y3[i] + "|"   )
+        else:   #print fy cumulative
+            print("+======================================================================================================================+") #border
+            if y1[4] != 0:
+                print("|"  +  row_names1[4] +  "|" + y3[4] + "|"   )
+                        #print ltd
+            print("|"  +  row_names1[5] +  "|" + y3[5] + "|"   )
+            print("+----------------------------------------------------------------------------------------------------------------------+")
+            if y[8] != 0:
+                print(misc1)
+                print("+----------------------------------------------------------------------------------------------------------------------+")
+    
+    if y1[4] != 0 and x2 == 1:
+        print("|" + y[1] + "|" + y[0] + "|" + y[2] + "|" + y[9] + "|") # title name and rank
+        if y1[4] != 0:
+            print("+----------------------------------------------------------------------------------------------------------------------+")
+        for i in range(x):
+            if y1[i] != 0:
+                print("|"  +  row_names1[i] +  "|" + y3[i] + "|"   )
+        else:   #print fy cumulative
+            print("+======================================================================================================================+") #border
+            if y1[4] != 0:
+                print("|"  +  row_names1[4] +  "|" + y3[4] + "|"   )
+                        #print ltd
+            print("|"  +  row_names1[5] +  "|" + y3[5] + "|"   )
+            print("+----------------------------------------------------------------------------------------------------------------------+")
+            if y[8] != 0:
+                print(misc1)
+                print("+----------------------------------------------------------------------------------------------------------------------+")
+
+
+    return
+
+# print header
+print("+----------------------------------------------------------------------------------------------------------------------+")
+print("|" + header_names1[0] + "|" + header_names1[1] + "|" + header_names1[2] + "|" + header_names1[3] + "|")
+print("+----------------------------------------------------------------------------------------------------------------------+")
+
+for i in range (len(capture2)):
+    c_print(capture2[i])
+# c_print(capture2[7])
+# c_print(capture2[8])
+# c_print(capture2[9])
+
+print(lb1)
+print(misc2)
+print(misc3)
+print(misc4)
+print(misc5)
