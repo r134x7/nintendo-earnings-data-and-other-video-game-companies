9f82bb4079746f13ba305a26f1cc65222cef6f87
r134x7 <r134x7@gmx.com>
2022-04-15 23:02:48 +1000

    refactored code, optimised strings for mobile output

--- Nintendo/Nintendo FY3-2022/sip1fy3-22.py
+++ Nintendo/Nintendo FY3-2022/sip1fy3-22.py
@@ -1,103 +1,131 @@
-hd1 = [  " Proportion of overseas sales ", " Proportion of hardware sales ", " Proportion of first party software sales ", " Digital Sales                ", " Proportion of Digital Sales  ", "Proportion of DL Ver. of Packaged Software Sales"] #header
-rw1 = [" 1st Quarter       ", " 2nd Quarter       ", " 3rd Quarter       ", " 4th Quarter       ", " 1st Half          ",  " 1st Three Quarters", " FY3/22 Cumulative "] # row names
-lb1 = "###" # line break
+header_1 = [  " Proportion of overseas sales ", " Proportion of hardware sales ", " Proportion of first party software sales ", " Digital Sales                ", " Proportion of Digital Sales  ", "Proportion of DL Ver. of Packaged Software Sales"] #header
+row_1 = [" 1st Quarter       ", " 2nd Quarter       ", " 3rd Quarter       ", " 4th Quarter       ", " 1st Half          ",  " 1st Three Quarters", " FY3/22 Cumulative "] # row names
+line_break_1 = "###" # line break
 
-header = [("+------------------------------+") + ("\n|"  +  hd1[0] + "|") + ("\n+------------------------------+"), ("+------------------------------+") + ("\n|"  +  hd1[1] + "|") + ("\n+------------------------------+"), ("+------------------------------+") + ("\n|"  +  " Proportion of first party    " + "|") + ("\n|" + " software sales               " + "|") + ("\n+------------------------------+"), ("+------------------------------+") + ("\n|"  +  hd1[3] + "|") + ("\n+------------------------------+"), ("+------------------------------+") + ("\n|"  +  hd1[4] + "|") + ("\n+------------------------------+"), ("+-------------------------------------+") + ("\n|"  +  " Proportion of downloadable versions " + "|") + ("\n|" + " of Packaged Software Sales          " + "|") + ("\n+-------------------------------------+")] # [0] Print proportion of overseas sales, [1] print proportion of hardware sales, [2] print Proportion of first party software sales, [3] print Digital Sales, [4] print proportion of digital sales, [5] Print Proportion of DL Ver. of Packaged SW Sales
+border_line = "+" + "-"*30 + "+"
+border_line_double = ["+" + "="*30 + "+", "+" + "="*37 + "+"] 
 
-footer = [("+------------------------------+") + ("\n(※ Proportion of overseas (outside of Japan)") + ("\nsales to total sales)"), ("+------------------------------+") + ("\n(※ Proportion of hardware (including accessories) sales") + ("\nto total dedicated videogame platform sales)"), ("+------------------------------+") + ("\n(※ Proportion of first-party software sales") + ("\nto total dedicated video game software sales)"), ("+------------------------------+") + ("\n(\"※ Digital sales include a) downloadable versions of packaged software,") + ("\nb) download-only software, c) add-on content and d) Nintendo Switch Online, etc.") + ("\n＊ \"Downloadable versions of packaged software\" indicates the") + ("\ndownloadable version of software that is offered both physically and digitally.\")"), ("+------------------------------+") + ("\n(※ Proportion of digital sales to total dedicated ") + ("\n video game software sales )"), ("+-------------------------------------+") + ("\n(※ Proportion of downloadable versions of packaged software sales ") + ("\n to total digital sales as indicated above: a/(a+b+c+d) )")] # [0] Print proportion of overseas sales, [1] print proportion of hardware sales, [2] print Proportion of first party software sales, [3] print Digital Sales, [4] print proportion of digital sales, [5] Print Proportion of DL Ver. of Packaged SW Sales
+header = [border_line + ("\n|"  +  header_1[0] + "|") + ("\n+" + "-"*30 + "+"), border_line + ("\n|"  +  header_1[1] + "|") + ("\n+" + "-"*30 + "+"), border_line + ("\n|"  +  " Proportion of first party    " + "|") + ("\n|" + " software sales               " + "|") + ("\n+" + "-"*30 + "+"), border_line + ("\n|"  +  header_1[3] + "|") + ("\n+" + "-"*30 + "+"), border_line + ("\n|"  +  header_1[4] + "|") + ("\n+" + "-"*30 + "+"), ("+" + "-"*37 + "+") + ("\n|"  +  " Proportion of downloadable versions " + "|") + ("\n|" + " of Packaged Software Sales          " + "|") + ("\n+" + "-"*37 + "+")] # [0] Print proportion of overseas sales, [1] print proportion of hardware sales, [2] print Proportion of first party software sales, [3] print Digital Sales, [4] print proportion of digital sales, [5] Print Proportion of DL Ver. of Packaged SW Sales
 
-x = 3 # x relates to the current earnings quarter, set to 1, 2, 3 or 4.
+footer = [border_line + ("\n(※ Proportion of overseas (outside of Japan)") + ("\nsales to total sales)"), border_line + ("\n(※ Proportion of hardware \n (including accessories) sales") + ("\nto total dedicated video game platform sales)"), border_line + ("\n(※ Proportion of first-party software sales") + ("\nto total dedicated video game software sales)"), border_line + ("\n(\"※ Digital sales include a) downloadable \nversions of packaged software,") + ("\nb) download-only software, \nc) add-on content and \nd) Nintendo Switch Online, etc.") + ("\n＊\"Downloadable versions of packaged software\" \nindicates the") + ("downloadable version of \nsoftware that is offered both physically \nand digitally.\")"), border_line + ("\n(※ Proportion of digital sales to total \ndedicated ") + ("video game software sales )"), ("+" + "-"*37 + "+") + ("\n(※ Proportion of downloadable versions of \npackaged software sales ") + ("to total digital sales \nas indicated above: a/(a+b+c+d) )")] # [0] Print proportion of overseas sales, [1] print proportion of hardware sales, [2] print Proportion of first party software sales, [3] print Digital Sales, [4] print proportion of digital sales, [5] Print Proportion of DL Ver. of Packaged SW Sales
 
-#Proportion of Overseas Sales
-poos1 = [77.9, 78.7, 79.6, 66.66, 78.3, 79.0, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+current_quarter = 3 # current_quarter relates to the current earnings quarter, set to 1, 2, 3 or 4.
 
-#Proportion of hardware sales
-pohs1 = [47.6, 45.2, 53.9, 66.66, 46.5, 50.4, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+proportion_of_overseas_sales = [77.9, 78.7, 79.6, 66.66, 78.3, 79.0, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-#Proportion of first party software sales
-pofpss1 = [72.3, 68.9, 84.1, 66.66, 70.6, 77.7, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+proportion_of_hardware_sales = [47.6, 45.2, 53.9, 66.66, 46.5, 50.4, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-#Digital sales
-ds1 = [75.9, 68.2, 110.8, 66.66, 144.2, 255.0, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+proportion_of_first_party_software_sales = [72.3, 68.9, 84.1, 66.66, 70.6, 77.7, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-#Proportion of Digital Sales
-pods1 = [46.9, 43.2, 35.3, 66.66, 45.1, 40.2, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+digital_sales = [75.9, 68.2, 110.8, 66.66, 144.2, 255.0, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-#Proportion of DL Ver. of Packaged SW Sales
-podlss1 = [52.1, 49.1, 56.3, 66.66, 50.7, 53.1, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
+proportion_of_digital_sales = [46.9, 43.2, 35.3, 66.66, 45.1, 40.2, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-def c_print (y, z, a, b, delta, theta): 
-    # variables created inside a function are local and cannot be used outside a function
+proportion_of_download_version_of_packaged_software_sales = [52.1, 49.1, 56.3, 66.66, 50.7, 53.1, 33.33] #Input figures, [0] 1st quarter, [1] 2nd quarter, [2] 3rd quarter, [3] 4th quarter, [4] 1st half, [5] first three quarters, [6] fiscal year cumulative
 
-        y1, z1, a1, b1, delta1, theta1 = ['{:}% '.format(elem) for elem in y], ['{:}% '.format(elem) for elem in z], ['{:}% '.format(elem) for elem in a], ['¥{:}B '.format(elem) for elem in b], ['{:}% '.format(elem) for elem in delta], ['{:}% '.format(elem) for elem in theta]
+def format_to_string (y, i):
 
-        y2, z2, a2, b2, delta2, theta2 = ['{0: >10}'.format(elem) for elem in y1], ['{0: >10}'.format(elem) for elem in z1], ['{0: >10}'.format(elem) for elem in a1], ['{0: >10}'.format(elem) for elem in b1], ['{0: >10}'.format(elem) for elem in delta1], [ '{0: >17}'.format(elem) for elem in theta1]
+    if y != digital_sales:
+        y1 = ['{:}% '.format(elem) for elem in y]
+    else: y1 = ['¥{:}B '.format(elem) for elem in y]
 
-        for i in range (6):
-            if range (i) == range (0):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + y2[i] + "|") 
-                else:
-                    print("+==============================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + y2[i+4] + "|")
-                    else: 
-                        print(footer[0])
-            elif range (i) == range (1):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + z2[i] + "|") 
-                else:
-                    print("+==============================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + z2[i+4] + "|")
-                    else: 
-                        print(footer[1])
-            elif range (i) == range (2):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + a2[i] + "|") 
-                else:
-                    print("+==============================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + a2[i+4] + "|")
-                    else: 
-                        print(footer[2])
-            elif range (i) == range (3):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + b2[i] + "|") 
-                else:
-                    print("+==============================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + b2[i+4] + "|")
-                    else: 
-                        print(footer[3])
-            elif range (i) == range (4):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + delta2[i] + "|") 
-                else:
-                    print("+==============================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + delta2[i+4] + "|")
-                    else: 
-                        print(footer[4])
-            elif range (i) == range (5):
-                print(header[i])
-                for i in range (x):
-                    print("|"  +  rw1[i] + "|" + theta2[i] + "|") 
-                else:
-                    print("+=====================================+")
-                    for i in range (x-1): 
-                        print("|"  +  rw1[i+4] + "|" + theta2[i+4] + "|")
-                    else: 
-                        print(footer[5])
+    if y != proportion_of_download_version_of_packaged_software_sales:
+        y2 = ['{0: >10}'.format(elem) for elem in y1]
+    else: y2 = ['{0: >17}'.format(elem) for elem in y1]
 
-        return
+    return to_print(y2, i)
 
-c_print (poos1, pohs1, pofpss1, ds1, pods1, podlss1)
+def to_print(y2, i):
+
+    print(header[i])
+    for j in range (current_quarter):
+        print("|"  +  row_1[j] + "|" + y2[j] + "|") 
+    else:
+        if i != 5:
+            print(border_line_double[0])
+        else: print(border_line_double[1])
+        for j in range (current_quarter-1): 
+            print("|"  +  row_1[j+4] + "|" + y2[j+4] + "|")
+        else: 
+            print(footer[i])
+
+    return  
+
+for_loop_list = [proportion_of_overseas_sales, proportion_of_hardware_sales, proportion_of_first_party_software_sales, digital_sales, proportion_of_digital_sales, proportion_of_download_version_of_packaged_software_sales]
+
+for i in range(6):
+    format_to_string(for_loop_list[i], i)
+
+# def c_print (y, z, a, b, delta, theta): 
+#     # variables created inside a function are local and cannot be used outside a function
+
+#         y1, z1, a1, b1, delta1, theta1 = ['{:}% '.format(elem) for elem in y], ['{:}% '.format(elem) for elem in z], ['{:}% '.format(elem) for elem in a], ['¥{:}B '.format(elem) for elem in b], ['{:}% '.format(elem) for elem in delta], ['{:}% '.format(elem) for elem in theta]
+
+#         y2, z2, a2, b2, delta2, theta2 = ['{0: >10}'.format(elem) for elem in y1], ['{0: >10}'.format(elem) for elem in z1], ['{0: >10}'.format(elem) for elem in a1], ['{0: >10}'.format(elem) for elem in b1], ['{0: >10}'.format(elem) for elem in delta1], ['{0: >17}'.format(elem) for elem in theta1]
+
+#         for i in range (6):
+#             if range (i) == range (0):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + y2[i] + "|") 
+#                 else:
+#                     print(border_line)
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + y2[i+4] + "|")
+#                     else: 
+#                         print(footer[0])
+#             elif range (i) == range (1):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + z2[i] + "|") 
+#                 else:
+#                     print(border_line)
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + z2[i+4] + "|")
+#                     else: 
+#                         print(footer[1])
+#             elif range (i) == range (2):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + a2[i] + "|") 
+#                 else:
+#                     print(border_line)
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + a2[i+4] + "|")
+#                     else: 
+#                         print(footer[2])
+#             elif range (i) == range (3):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + b2[i] + "|") 
+#                 else:
+#                     print(border_line)
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + b2[i+4] + "|")
+#                     else: 
+#                         print(footer[3])
+#             elif range (i) == range (4):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + delta2[i] + "|") 
+#                 else:
+#                     print(border_line)
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + delta2[i+4] + "|")
+#                     else: 
+#                         print(footer[4])
+#             elif range (i) == range (5):
+#                 print(header[i])
+#                 for i in range (current_quarter):
+#                     print("|"  +  row_1[i] + "|" + theta2[i] + "|") 
+#                 else:
+#                     print("+=====================================+")
+#                     for i in range (current_quarter-1): 
+#                         print("|"  +  row_1[i+4] + "|" + theta2[i+4] + "|")
+#                     else: 
+#                         print(footer[5])
+
+#         return
 
 # Old code++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # #Proportion of Overseas Sales
