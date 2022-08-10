a4f7453266169b1d5dd6cbb8e25bf8ddd5832282
r134x7 <r134x7@gmx.com>
2022-08-10 15:11:51 +1000

    had to change type structure to make it easier to apply functions

--- webpage/src/utils/earnings-logic.tsx
+++ webpage/src/utils/earnings-logic.tsx
@@ -1,10 +1,24 @@
 
-interface Quarters {
-    firstQuarter: number,
-    secondQuarter: number,
-    thirdQuarter: number,
-    fourthQuarter: number,
-}
+// interface Quarters {
+//     firstQuarter: number,
+//     secondQuarter: number,
+//     thirdQuarter: number,
+//     fourthQuarter: number,
+// }
+
+
+interface Quarter {
+    quarter: number,
+  }
+  
+  const netSales: Quarter[] = [
+      {quarter: 307460},
+      {quarter: 624272},
+      {quarter: 1320219},
+      {quarter: 1695344},
+  ]
+  
+  const netSalesDifference: Quarter[] = quarterlyCalculation(netSales)
 
 interface Forecasts {
     currentFiscalYearForecast: number,
@@ -14,14 +28,14 @@ interface Forecasts {
     thirdForecastRevision?: number,
 }
 
-type Consolidated = Quarters | Forecasts;
+// type Consolidated = Quarters | Forecasts;
 
-const netSales: Quarters = {
-    firstQuarter: 307460,
-    secondQuarter: 624272,
-    thirdQuarter: 1320219,
-    fourthQuarter: 1695344,
-};
+// const netSales: Quarters = {
+//     firstQuarter: 307460,
+//     secondQuarter: 624272,
+//     thirdQuarter: 1320219,
+//     fourthQuarter: 1695344,
+// };
 
 const netSalesForecasts: Forecasts = {
     currentFiscalYearForecast: 1600000,
@@ -32,29 +46,41 @@ const netSalesForecasts: Forecasts = {
 // try using array.map.filter.reduce
 // rest pararmeters and spread syntax where needed
 // template literals
-const netSales2: Quarters = quarterlyCalculation(netSales);
+// const netSales2: Quarters = quarterlyCalculation(netSales);
 
-export function quarterlyCalculation(quarters: Quarters) {
+// export function quarterlyCalculation(quarters: Quarters) {
+export function quarterlyCalculation(quarters: Quarter[]) {
+    
 
-    const keys: string[] = Object.keys(quarters)
-    const values: number[] = Object.values(quarters)
+    const calc = quarters.map((x,i, array) => {
+        //  return (i === 0) ? x : x.quarter + x.quarter
+        //  return (i === 0) ? x : {quarter: x.quarter - quarters[i-1].quarter} // Finally!!
+         return (i === 0) ? x : { quarter: x.quarter - array[i-1].quarter} // Finally figured out how to do it correctly with its own parameters
+        //  return (i === 0) ? x : {...x} - {...x}
+      })
+      console.log(calc)
+    
+      return calc
+    
+    // const keys: string[] = Object.keys(quarters)
+    // const values: number[] = Object.values(quarters)
 
-    const quarterDifference = Object.values(quarters).map((x, i) => {
-      if (i === 0) {
-        return x
-      } else {
-        let y = x - values[i-1] // you need to copy the array to do this since you cannot reference an element's index...
-        return y
-      }
-    })
+    // const quarterDifference = Object.values(quarters).map((x, i) => {
+    //   if (i === 0) {
+    //     return x
+    //   } else {
+    //     let y = x - values[i-1] // you need to copy the array to do this since you cannot reference an element's index...
+    //     return y
+    //   }
+    // })
 
-    const reshape = keys.map((x, i) => {
-        return {
-          [x]: quarterDifference[i] 
-        }
-    }).reduce((acc, cur) => ({...acc, ...cur}), {}); // source for solution, but typescript won't let me assert Quarter to it... https://stackoverflow.com/questions/43957032/spread-syntax-not-working-as-expected-with-array-of-objects#:~:text=When%20spreading%20into%20an%20Object%2C%20the%20value%20of,each%20property%20is%20its%20index%20in%20the%20array.
+    // const reshape = keys.map((x, i) => {
+        // return {
+        //   [x]: quarterDifference[i] 
+        // }
+    // }).reduce((acc, cur) => ({...acc, ...cur}), {}); // source for solution, but typescript won't let me assert Quarter to it... https://stackoverflow.com/questions/43957032/spread-syntax-not-working-as-expected-with-array-of-objects#:~:text=When%20spreading%20into%20an%20Object%2C%20the%20value%20of,each%20property%20is%20its%20index%20in%20the%20array.
 
-    return reshape as Quarters
+    // return reshape as Quarters
 
   // alternative method which is much less complicated than the above solution, doesn't require type assertion and is only okay for objects that don't increase in count.
 //   const quarterDifference: Quarters = {
