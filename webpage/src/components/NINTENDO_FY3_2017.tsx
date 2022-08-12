487a7ae731b03f5a67f35bc199a93405b9bc7f94
r134x7 <r134x7@gmx.com>
2022-08-12 17:25:52 +1000

    setting up nintendo fy3/2017 component

---
+++ webpage/src/components/NINTENDO_FY3_2017.tsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import { Code, SegmentedControl, Anchor, Text, Stack, Pagination, Group, Space, Switch } from "@mantine/core";
+import { useSelector } from "react-redux";
+
+import { Line, Bar } from "react-chartjs-2";
+import { Chart, registerables } from 'chart.js'; // required to actually get chart.js with react-chartjs-2 to work
+Chart.register(...registerables); // to get the package working, source: https://www.chartjs.org/docs/next/getting-started/integration.html
+
+export default function NINTENDO_FY3_17() {
+
+    const [data, setData] = useState("");
+    const [value, setValue] = useState("");
+    const [sources, setSources] = useState(<></>)
+
+    const state: any = useSelector(state => state);
+
+    const [activePage, setPage] = useState(1);
+    const [secondDataRef, setSecondDataRef] = useState(2)
+    const [checked, setChecked] = useState(false);
+    const [barChecked, setBarChecked] = useState(false);
+
+    
+
+    useEffect(() => {
+        (value === "Consolidated Operating Results")
+            ? setData(consolidatedOperatingResults)
+            : (value === "WW Hardware/Software units, Mobile/IP related income")
+            ? setData(nintendoHardwareSoftwareMobile)
+            : (value === "Key/Digital Sales Indicator")
+            ? setData(keyIndicators)
+            : (value === "FY Million-Seller Titles")
+            ? setData(fyMillionSellers)
+            : (value === "Regional Hardware/Software units")
+            ? setData(regionalHWSW)
+            : (value === "Top Selling Titles")
+            ? setData(topSellingTitles)
+            : setData("");
+
+        (value === "Data Sources")
+            ? setSources(DATA_SOURCES)
+            : setSources(<></>)
+
+    }, [value])
+
+    function DATA_SOURCES() {
+
+        return (
+            <Text>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2016/160727e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2016/160727e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2016/161026e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2016/161026e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170131e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170131e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170427e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170427e.pdf
+                    </Anchor>
+                        4th Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170427_6e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170427_6e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        )
+    };
+
+    return (
+
+        <div>  
+            <SegmentedControl
+                    fullWidth 
+                    orientation="vertical"
+                    value={value}
+                    onChange={setValue}
+                    data={[ "Data Sources",
+                            "Consolidated Operating Results", 
+                            "WW Hardware/Software units, Mobile/IP related income", 
+                            "Key/Digital Sales Indicator", 
+                            "FY Million-Seller Titles", 
+                            "Regional Hardware/Software units", 
+                            "Top Selling Titles",]}
+            />
+            
+            {sources}
+            <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
+        </div>
+        
+    );
+}
