7eb6226902d6a68a307afa5180406350d9715bc4
r134x7 <r134x7@gmx.com>
2022-08-15 12:46:23 +1000

    test is working, not passed yet

--- webpage/src/__tests__/kpi-logic.test.tsx
+++ webpage/src/__tests__/kpi-logic.test.tsx
@@ -5,21 +5,29 @@ const proportionOfOverseasSales: KPIQuarter[] = [
         name: " Proportion of overseas sales ",
         quarter: " 1st Quarter ",
         number: 79.9,
+        footer: `(※ Proportion of overseas (outside of Japan)
+        sales to total sales)`,
     },
     {
         name: " Proportion of overseas sales ",
         quarter: " 2nd Quarter ",
         number: 78.9,
+        footer: `(※ Proportion of overseas (outside of Japan)
+        sales to total sales)`,
     },
     {
         name: " Proportion of overseas sales ",
         quarter: " 3rd Quarter ",
         number: 77.9,
+        footer: `(※ Proportion of overseas (outside of Japan)
+        sales to total sales)`,
     },
     {
         name: " Proportion of overseas sales ",
         quarter: " 4th Quarter ",
         number: 76.9,
+        footer: `(※ Proportion of overseas (outside of Japan)
+        sales to total sales)`,
     },
 ]
 
@@ -28,43 +36,73 @@ const digitalSales: KPIQuarter[] = [
         name: " Digital Sales ",
         quarter: " 1st Quarter ",
         number: 79.9,
+        footer: `("※ Digital sales include a) downloadable
+        versions of packaged software,
+        b) download-only software,
+        c) add-on content and
+        d) Nintendo Switch Online, etc.
+        ＊"Downloadable versions of packaged software"
+        indicates the downloadable version of
+        software that is offered both physically
+        and digitally.")`,
     },
     {
         name: " Digital Sales ",
         quarter: " 2nd Quarter ",
         number: 78.9,
+        footer: `("※ Digital sales include a) downloadable
+        versions of packaged software,
+        b) download-only software,
+        c) add-on content and
+        d) Nintendo Switch Online, etc.
+        ＊"Downloadable versions of packaged software"
+        indicates the downloadable version of
+        software that is offered both physically
+        and digitally.")`,
     },
     {
         name: " Digital Sales ",
         quarter: " 3rd Quarter ",
         number: 77.9,
+        footer: `("※ Digital sales include a) downloadable
+        versions of packaged software,
+        b) download-only software,
+        c) add-on content and
+        d) Nintendo Switch Online, etc.
+        ＊"Downloadable versions of packaged software"
+        indicates the downloadable version of
+        software that is offered both physically
+        and digitally.")`,
     },
     {
         name: " Digital Sales ",
         quarter: " 4th Quarter ",
         number: 76.9,
+        footer: `("※ Digital sales include a) downloadable
+        versions of packaged software,
+        b) download-only software,
+        c) add-on content and
+        d) Nintendo Switch Online, etc.
+        ＊"Downloadable versions of packaged software"
+        indicates the downloadable version of
+        software that is offered both physically
+        and digitally.")`,
     },
 ]
 
-const currentQuarter = 4;
+// const currentQuarter = 4;
 
 const printSectionQuarters = (sectionQuarter: KPIQuarter[], currentQuarter: number) => { // to use Net Sales Difference, Operating Income Difference or Net Profit Difference
 
-    return sectionDifference.filter((elem, index) => index < currentQuarter).map((elem, index) => {
+    return sectionQuarter.filter((elem, index) => index < currentQuarter).map((elem, index) => {
 
-        let printSectionDifferenceYoY: string = (sectionYoY[index].quarter > 0) 
-                                ? `+${sectionYoY[index].quarter}% `
-                                : `${sectionYoY[index].quarter}% `;
-        let printSectionYoYFixed: string = (printSectionDifferenceYoY.length >= 9)
-                                ? printSectionDifferenceYoY
-                                : " ".repeat(9 - printSectionDifferenceYoY.length) + printSectionDifferenceYoY
-        //  let x = `${elem.quarter.toLocaleString('ja-JP', { style: 'currency', currency: 'JPY' })}M ` // cannot use currency settings as it creates border misalignment due to ¥ becoming wider. 
-        let printSection: string = `¥${elem.quarter.toLocaleString("en")}M `; // this setting allows use of thousands separator ","
-        let printSectionFixed: string = (printSection.length === 14)
+        let printSection: string = (sectionQuarter === digitalSales) 
+            ? `¥${elem.quarter}B `
+            : `${elem.quarter}% `; 
+        let printSectionFixed: string = (printSection.length >= 10)
                                   ? printSection
-                                  : " ".repeat(14 - printSection.length) + printSection;
-        let printQuarterRow: string = `${rowQuartersApplied[index].quarter}`;  
-        return "|" + printQuarterRow + "|" + printSectionFixed + "|" + printSectionYoYFixed + "|" // old note, used "\n" at end here: must affix a new line \n, was also affixing tabs \t to align but realised I could adjust the template literal 
+                                  : " ".repeat(10 - printSection.length) + printSection;
+        return "|" + elem.name + "|" + printSectionFixed + "|"
     }).reduce((prev, next, index, array) => {
         return (array[index] === array[currentQuarter -1])
                   ? prev + `\n+${"-".repeat(38)}+\n` + next
@@ -72,3 +110,24 @@ const printSectionQuarters = (sectionQuarter: KPIQuarter[], currentQuarter: numb
     })
 
 };
+
+test('Print Section Quarters type A', () => {
+    let currentQuarter = 1;
+
+    const test = printSectionQuarters(proportionOfOverseasSales, currentQuarter)
+
+let pythonOutput = 
+`+------------------------------+
+| Proportion of overseas sales |
++------------------------------+
+| 1st Quarter       |    79.9% |
++==============================+
++------------------------------+
+(※ Proportion of overseas (outside of Japan)
+sales to total sales)
+`;
+
+    console.log(printSectionQuarters)
+  
+    expect(test).toMatch(pythonOutput);
+  });
--- webpage/src/utils/kpi-logic.tsx
+++ webpage/src/utils/kpi-logic.tsx
@@ -2,10 +2,12 @@ export interface KPIQuarter {
     name: string,
     quarter: string,
     number: number,
+    footer: string,
 }
 
 export interface KPICumulatives {
     name: string,
     quarter: string,
     number: number,
+    footer: string,
 }
