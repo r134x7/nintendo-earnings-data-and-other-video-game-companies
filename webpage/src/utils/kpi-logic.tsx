72dba1658aa97a4815b8898b906d3a8a9479a132
r134x7 <r134x7@gmx.com>
2022-08-15 13:53:39 +1000

    added footer interface

--- webpage/src/utils/kpi-logic.tsx
+++ webpage/src/utils/kpi-logic.tsx
@@ -25,17 +25,20 @@ export interface Header {
 }
 
 export interface Footer {
-
+    proportionOfOverseasSales: string,
+    proportionOfHardwareSales: string,
+    proportionOfFirstPartySoftwareSales: string,
+    digitalSales: string,
+    proportionOfDigitalSales: string,
+    proportionOfDLverPackagedSoftware: string,
 }
 
-const header: Header = {
-    companyName: " Nintendo Co., Ltd.",
-    proportionOfOverseasSales: " Proportion of overseas sales ",
-    proportionOfHardwareSales: " Proportion of hardware sales ",
-    proportionOfFirstPartySoftwareSales: "| Proportion of first party    |\n| software sales               |",
-    digitalSales: " Digital Sales                ",
-    proportionOfDigitalSales: " Proportion of Digital Sales  ",
-    proportionOfDLverPackagedSoftware: "| Proportion of downloadable   |\n| versions of Packaged         |\n| Software Sales               |",
-    fiscalYear: "FY3/2017 ",
-    title: "   Key/Digital Sales Indicators   ",
+const footer: Footer = {
+    proportionOfOverseasSales: "|(※ Proportion of overseas (outside of Japan)\n| sales to total sales)",
+    proportionOfHardwareSales: "|(※ Proportion of hardware  \n|(including accessories) sales to total \n| dedicated video game platform sales)",
+    proportionOfFirstPartySoftwareSales: "|(※ Proportion of first-party software sales\n| to total dedicated video game software sales)",
+    digitalSales: "|(\"※ Digital sales include a) downloadable \n| versions of packaged software, \n|b) download-only software,\n|c) add-on content and \n|d) Nintendo Switch Online, etc. \n＊\"Downloadable versions of packaged software\"\n| indicates the downloadable version of\n| software that is offered both physically\n and digitally.\")",
+    proportionOfDigitalSales: "|(※ Proportion of digital sales to total\n| dedicated video game software sales )",
+    proportionOfDLverPackagedSoftware: "|(※ Proportion of downloadable versions of \n| packaged software sales to total digital \n| sales as indicated above: a/(a+b+c+d) )",
 }
+
