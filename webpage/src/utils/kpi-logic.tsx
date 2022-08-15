e2e7bb8ece77f7c039da5f806f47f6b64b04ee20
r134x7 <r134x7@gmx.com>
2022-08-15 13:25:07 +1000

    added header interface

--- webpage/src/utils/kpi-logic.tsx
+++ webpage/src/utils/kpi-logic.tsx
@@ -11,3 +11,31 @@ export interface KPICumulatives {
     number: number,
     footer: string,
 }
+
+export interface Header {
+    companyName: string,
+    proportionOfOverseasSales: string,
+    proportionOfHardwareSales: string,
+    proportionOfFirstPartySoftwareSales: string,
+    digitalSales: string,
+    proportionOfDigitalSales: string,
+    proportionOfDLverPackagedSoftware: string,
+    fiscalYear: string,
+    title: string,
+}
+
+export interface Footer {
+
+}
+
+const header: Header = {
+    companyName: " Nintendo Co., Ltd.",
+    proportionOfOverseasSales: " Proportion of overseas sales ",
+    proportionOfHardwareSales: " Proportion of hardware sales ",
+    proportionOfFirstPartySoftwareSales: "| Proportion of first party    |\n| software sales               |",
+    digitalSales: " Digital Sales                ",
+    proportionOfDigitalSales: " Proportion of Digital Sales  ",
+    proportionOfDLverPackagedSoftware: "| Proportion of downloadable   |\n| versions of Packaged         |\n| Software Sales               |",
+    fiscalYear: "FY3/2017 ",
+    title: "   Key/Digital Sales Indicators   ",
+}
