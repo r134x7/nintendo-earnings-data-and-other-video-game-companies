775a97079a05f9bb19ca3dd862c2d9c8dce9bc2a
r134x7 <r134x7@gmx.com>
2022-08-15 18:25:28 +1000

    added union type to KPI interface to fix printSection functions

--- webpage/src/utils/kpi-logic.tsx
+++ webpage/src/utils/kpi-logic.tsx
@@ -1,9 +1,11 @@
 export interface KPIQuarter {
+    type: "percentage" | "currency",
     quarter: string,
     value: number,
 }
 
 export interface KPICumulative {
+    type: "percentage" | "currency",
     quarter: string,
     value: number,
 }
@@ -29,3 +31,21 @@ export interface Footer {
     proportionOfDLverPackagedSoftware: string,
 }
 
+const printSectionQuarters = (sectionQuarter: KPIQuarter[], currentQuarter: number) => { 
+
+    return sectionQuarter.filter((elem, index) => index < currentQuarter).map((elem, index) => {
+
+        let printSection: string = (elem.type === "currency") 
+            ? `¥${elem.value}B `
+            : `${elem.value}% `; 
+        let printSectionFixed: string = (printSection.length >= 10)
+                                  ? printSection
+                                  : " ".repeat(10 - printSection.length) + printSection;
+        return "|" + elem.quarter + "|" + printSectionFixed + "|"
+    }).reduce((prev, next, index, array) => {
+        return (array[index] === array[currentQuarter -1])
+                  ? prev + `\n` + next
+                  : prev + `\n` + next 
+    })
+
+};
