8e70517f4033d26205eed1091eef13c840f45a08
r134x7 <r134x7@gmx.com>
2022-08-16 01:30:18 +1000

    prepared new types

--- webpage/src/data/nintendo/Nintendo-FY3-2017/earnings-fy3-17.tsx
+++ webpage/src/data/nintendo/Nintendo-FY3-2017/earnings-fy3-17.tsx
@@ -42,6 +42,52 @@ import {
         },
     ]
 
+    const netSalesLastFy: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " 1st Quarter ",
+            value: 90223
+        },
+        {
+            type: "currency",
+            quarter: " 2nd Quarter ",
+            value: 204182
+        },
+        {
+            type: "currency",
+            quarter: " 3rd Quarter ",
+            value: 425664
+        },
+        {
+            type: "currency",
+            quarter: " 4th Quarter ",
+            value: 504459
+        },
+    ]
+
+    const netSalesForecasts: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " FY3/17 Forecast ",
+            value: 500000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 1 ",
+            value: 470000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 2 ",
+            value: 470000
+        },
+        {
+            type: "currency",
+            quarter: " FY3/18 Forecast ",
+            value: 750000
+        },
+    ]
+
     // const netSales: Quarter[] = [
     //     {quarter: 61969},  // first quarter
     //     {quarter: 136812},  // second quarter
@@ -49,61 +95,199 @@ import {
     //     {quarter: 489095}, // fourth quarter
     // ]
 
-    const netSalesLastFy: Quarter[] = [
-        {quarter: 90223}, // first quarter
-        {quarter: 204182}, // second quarter
-        {quarter: 425664}, // third quarter
-        {quarter: 504459}, // fourth quarter
+    // const netSalesLastFy: Quarter[] = [
+    //     {quarter: 90223}, // first quarter
+    //     {quarter: 204182}, // second quarter
+    //     {quarter: 425664}, // third quarter
+    //     {quarter: 504459}, // fourth quarter
+    // ]
+
+    // const netSalesForecasts: Forecasts[] = [ // any forecast revisions need to be placed between current and next
+    //     {forecast: 500000}, // current Fiscal Year Forecast
+    //     {forecast: 470000}, // first forecast revision
+    //     {forecast: 470000}, // second forecast revision
+    //     {forecast: 750000}, // next Fiscal Year Forecast
+    // ]
+
+    const operatingIncome: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " 1st Quarter ",
+            value: -5134
+        },
+        {
+            type: "currency",
+            quarter: " 2nd Quarter ",
+            value: -5947
+        },
+        {
+            type: "currency",
+            quarter: " 3rd Quarter ",
+            value: 26315
+        },
+        {
+            type: "currency",
+            quarter: " 4th Quarter ",
+            value: 29362
+        },
     ]
 
-    const netSalesForecasts: Forecasts[] = [ // any forecast revisions need to be placed between current and next
-        {forecast: 500000}, // current Fiscal Year Forecast
-        {forecast: 470000}, // first forecast revision
-        {forecast: 470000}, // second forecast revision
-        {forecast: 750000}, // next Fiscal Year Forecast
+    const operatingIncomeLastFY: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " 1st Quarter ",
+            value: 1149
+        },
+        {
+            type: "currency",
+            quarter: " 2nd Quarter ",
+            value: 8977
+        },
+        {
+            type: "currency",
+            quarter: " 3rd Quarter ",
+            value: 42485
+        },
+        {
+            type: "currency",
+            quarter: " 4th Quarter ",
+            value: 32881
+        },
     ]
 
-    const operatingIncome: Quarter[] = [
-        {quarter: -5134}, // first quarter
-        {quarter: -5947}, // second quarter
-        {quarter: 26315}, // third quarter
-        {quarter: 29362}, // fourth quarter
-    ]
-    
-    const operatingIncomeLastFY: Quarter[] = [
-          {quarter: 1149}, // first quarter
-          {quarter: 8977}, // second quarter
-          {quarter: 42485}, // third quarter
-          {quarter: 32881}, // fourth quarter
-    ]
-    
-    const operatingIncomeForecasts: Forecasts[] = [ // forecast revisions need to be placed between current and next
-          {forecast: 45000}, // current Fiscal Year Forecast
-          {forecast : 30000}, // first forecast revision
-          {forecast: 20000}, // second forecast revision
-          {forecast: 65000}, // next Fiscal Year Forecast
+    const operatingIncomeForecasts: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " FY3/17 Forecast ",
+            value: 45000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 1 ",
+            value: 30000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 2 ",
+            value: 20000
+        },
+        {
+            type: "currency",
+            quarter: " FY3/18 Forecast ",
+            value: 65000
+        },
     ]
 
-    const netIncome: Quarter[] = [
-        {quarter: -24534}, // first quarter
-        {quarter: 38299}, // second quarter
-        {quarter: 102969}, // third quarter
-        {quarter: 102574}, // fourth quarter
-    ]
+    // const operatingIncome: Quarter[] = [
+    //     {quarter: -5134}, // first quarter
+    //     {quarter: -5947}, // second quarter
+    //     {quarter: 26315}, // third quarter
+    //     {quarter: 29362}, // fourth quarter
+    // ]
     
-    const netIncomeLastFY: Quarter[] = [
-          {quarter: 8284}, // first quarter
-          {quarter: 11466}, // second quarter
-          {quarter: 40558}, // third quarter
-          {quarter: 16505}, // fourth quarter
-    ]
+    // const operatingIncomeLastFY: Quarter[] = [
+    //       {quarter: 1149}, // first quarter
+    //       {quarter: 8977}, // second quarter
+    //       {quarter: 42485}, // third quarter
+    //       {quarter: 32881}, // fourth quarter
+    // ]
     
-    const netIncomeForecasts: Forecasts[] = [ // forecast revisions need to be placed between current and next
-            {forecast: 35000}, // current Fiscal Year Forecast
-            {forecast: 50000}, // first forecast revision
-            {forecast: 90000}, // second forecast revision
-            {forecast: 45000}, // next Fiscal Year Forecast
+    // const operatingIncomeForecasts: Forecasts[] = [ // forecast revisions need to be placed between current and next
+    //       {forecast: 45000}, // current Fiscal Year Forecast
+    //       {forecast: 30000}, // first forecast revision
+    //       {forecast: 20000}, // second forecast revision
+    //       {forecast: 65000}, // next Fiscal Year Forecast
+    // ]
+
+    const netIncome: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " 1st Quarter ",
+            value: -24534  
+        },
+        {
+            type: "currency",
+            quarter: " 2nd Quarter ",
+            value: 38299 
+        },
+        {
+            type: "currency",
+            quarter: " 3rd Quarter ",
+            value: 102969
+        },
+        {
+            type: "currency",
+            quarter: " 4th Quarter ",
+            value: 102574
+        },
     ]
+
+    const netIncomeLastFY: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " 1st Quarter ",
+            value: 8284 
+        },
+        {
+            type: "currency",
+            quarter: " 2nd Quarter ",
+            value: 11466
+        },
+        {
+            type: "currency",
+            quarter: " 3rd Quarter ",
+            value: 40558
+        },
+        {
+            type: "currency",
+            quarter: " 4th Quarter ",
+            value: 16505
+        },
+    ]
+
+    const netIncomeForecasts: EarningsQuarter[] = [
+        {
+            type: "currency",
+            quarter: " FY3/17 Forecast ",
+            value: 35000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 1 ",
+            value: 50000
+        },
+        {
+            type: "currency",
+            quarter: " FCST Revision 2 ",
+            value: 90000
+        },
+        {
+            type: "currency",
+            quarter: " FY3/18 Forecast ",
+            value: 45000
+        },
+    ]
+
+    // const netIncome: Quarter[] = [
+    //     {quarter: -24534}, // first quarter
+    //     {quarter: 38299}, // second quarter
+    //     {quarter: 102969}, // third quarter
+    //     {quarter: 102574}, // fourth quarter
+    // ]
+    
+    // const netIncomeLastFY: Quarter[] = [
+    //       {quarter: 8284 }, // first quarter
+    //       {quarter: 11466}, // second quarter
+    //       {quarter: 40558}, // third quarter
+    //       {quarter: 16505}, // fourth quarter
+    // ]
+    
+    // const netIncomeForecasts: Forecasts[] = [ // forecast revisions need to be placed between current and next
+    //         {forecast: 35000}, // current Fiscal Year Forecast
+    //         {forecast: 50000}, // first forecast revision
+    //         {forecast: 90000}, // second forecast revision
+    //         {forecast: 45000}, // next Fiscal Year Forecast
+    // ]
 
     const header: Header = {
         companyName: " Nintendo Co., Ltd.",
