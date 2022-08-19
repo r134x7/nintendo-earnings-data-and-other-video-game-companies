f777012620c21b2395d1c19f918c615dd6b62c37
r134x7 <r134x7@gmx.com>
2022-08-20 00:50:30 +1000

    refactoring kpi logic

--- webpage/src/utils/kpi-logic.tsx
+++ webpage/src/utils/kpi-logic.tsx
@@ -1,11 +1,6 @@
-export type KPIQuarter = {
-    type: "percentage" | "currency",
-    quarter: string,
-    value: number,
-}
-
-export type KPICumulative = {
-    type: "percentage" | "currency",
+export type KPDIndicators = {
+    category: "quarterly" | "cumulative", 
+    units: "percentage" | "currency",
     quarter: string,
     value: number,
 }
@@ -31,42 +26,68 @@ export type Footer = {
     proportionOfDLverPackagedSoftware: string,
 }
 
-const printSectionQuarters = (sectionQuarter: KPIQuarter[], currentQuarter: number) => { 
+const printSectionQuarters = (sectionDifference: KPDIndicators[], currentQuarter: number) => { 
 
-    return sectionQuarter.filter((elem, index) => index < currentQuarter).map((elem, index) => {
+    return sectionDifference.filter((elem, index) => { 
+            return (elem.category === "quarterly")
+                ? index < currentQuarter
+                : currentQuarter >= 2 && index < currentQuarter -1         
+    }).map((elem, index) => {
 
-        let printSection: string = (elem.type === "currency") 
-            ? `¥${elem.value}B `
-            : `${elem.value}% `; 
-        let printSectionFixed: string = (printSection.length >= 10)
-                                  ? printSection
-                                  : " ".repeat(10 - printSection.length) + printSection;
-        return "|" + elem.quarter + "|" + printSectionFixed + "|"
+        if (elem.category === "quarterly") {
+            
+            let printSection: string = (elem.units === "currency") 
+                ? `¥${elem.value}B `
+                : `${elem.value}% `; 
+            let printSectionFixed: string = (printSection.length >= 10)
+                                      ? printSection
+                                      : " ".repeat(10 - printSection.length) + printSection;
+            return "|" + elem.quarter + "|" + printSectionFixed + "|"
+        } else {
+
+            let printSectionCml: string = (elem.units === "currency") 
+                ? `¥${elem.value}B `
+                : `${elem.value}% `; 
+    
+                let printSectionCmlFixed: string = (printSectionCml.length >= 10)
+                              ? printSectionCml
+                              : " ".repeat(10 - printSectionCml.length) + printSectionCml;
+            return "|" + elem.quarter + "|" + printSectionCmlFixed + "|"
+        }
     }).reduce((prev, next, index, array) => {
-        return (array[index] === array[currentQuarter -1])
-                  ? prev + `\n` + next
-                  : prev + `\n` + next 
+
+        if (sectionDifference[index].category === "quarterly" ) {
+            
+            return (array[index] === array[currentQuarter -1])
+                      ? prev + `\n` + next
+                      : prev + `\n` + next 
+        } else {
+
+            return (array[index] === array[currentQuarter -2])
+                    ? prev + `\n` + next 
+                    : prev + `\n` + next 
+        }
     })
 
 };
 
-const printSectionCumulative = (sectionCumulative: KPICumulative[], currentQuarter: number) => { 
+// const printSectionCumulative = (sectionCumulative: KPICumulative[], currentQuarter: number) => { 
     
-    return  sectionCumulative.filter((elem, index) => (currentQuarter >= 2 && index < currentQuarter -1)).map((elem, index) => {
+//     return  sectionCumulative.filter((elem, index) => (currentQuarter >= 2 && index < currentQuarter -1)).map((elem, index) => {
     
-    let printSectionCml: string = (elem.type === "currency") 
-        ? `¥${elem.value}B `
-        : `${elem.value}% `; 
-    let printSectionCmlFixed: string = (printSectionCml.length >= 10)
-                              ? printSectionCml
-                              : " ".repeat(10 - printSectionCml.length) + printSectionCml;
-    return "|" + elem.quarter + "|" + printSectionCmlFixed + "|"
-    }).reduce((prev, next, index, array) => {
-    return (array[index] === array[currentQuarter -2])
-            ? prev + `\n` + next 
-            : prev + `\n` + next 
-  })
-};
+//     let printSectionCml: string = (elem.type === "currency") 
+//         ? `¥${elem.value}B `
+//         : `${elem.value}% `; 
+//     let printSectionCmlFixed: string = (printSectionCml.length >= 10)
+//                               ? printSectionCml
+//                               : " ".repeat(10 - printSectionCml.length) + printSectionCml;
+//     return "|" + elem.quarter + "|" + printSectionCmlFixed + "|"
+//     }).reduce((prev, next, index, array) => {
+//     return (array[index] === array[currentQuarter -2])
+//             ? prev + `\n` + next 
+//             : prev + `\n` + next 
+//   })
+// };
 
 export const printHead = (header: Header) => 
 `+${"-".repeat(30)}+
@@ -75,7 +96,7 @@ export const printHead = (header: Header) =>
 ${header.title}
 +${"-".repeat(30)}+`;
 
-export const printProportionOfOverseasSales = (header: Header, footer: Footer, proportionOfOverseasSalesQtr: KPIQuarter[], proportionOfOverseasSalesCml: KPICumulative[], currentQuarter: number) => 
+export const printBody = (header: Header, footer: Footer, quarter: KPDIndicators[], cumulative: KPDIndicators[], currentQuarter: number) => 
 `+${"-".repeat(30)}+
 ${header.proportionOfOverseasSales}
 +${"-".repeat(30)}+
@@ -84,47 +105,56 @@ ${printSectionQuarters(proportionOfOverseasSalesQtr, currentQuarter)}
 +${"-".repeat(30)+"+"}
 ${footer.proportionOfOverseasSales}`;
 
-export const printProportionOfHardwareSales = (header: Header, footer: Footer, proportionOfHardwareSalesQtr: KPIQuarter[], proportionOfHardwareSalesCml: KPICumulative[], currentQuarter: number) => 
-`+${"-".repeat(30)}+
-${header.proportionOfHardwareSales}
-+${"-".repeat(30)}+
-${printSectionQuarters(proportionOfHardwareSalesQtr, currentQuarter)}
-+${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfHardwareSalesCml, currentQuarter) : "=".repeat(30)+"+" }
-+${"-".repeat(30)+"+"}
-${footer.proportionOfHardwareSales}`;
+// export const printProportionOfOverseasSales = (header: Header, footer: Footer, proportionOfOverseasSalesQtr: KPIQuarter[], proportionOfOverseasSalesCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.proportionOfOverseasSales}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(proportionOfOverseasSalesQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfOverseasSalesCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.proportionOfOverseasSales}`;
 
-export const printProportionOfFirstPartySoftwareSales = (header: Header, footer: Footer, proportionOfFirstPartySoftwareSalesQtr: KPIQuarter[], proportionOfFirstPartySoftwareSalesCml: KPICumulative[], currentQuarter: number) => 
-`+${"-".repeat(30)}+
-${header.proportionOfFirstPartySoftwareSales}
-+${"-".repeat(30)}+
-${printSectionQuarters(proportionOfFirstPartySoftwareSalesQtr, currentQuarter)}
-+${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfFirstPartySoftwareSalesCml, currentQuarter) : "=".repeat(30)+"+" }
-+${"-".repeat(30)+"+"}
-${footer.proportionOfFirstPartySoftwareSales}`;
+// export const printProportionOfHardwareSales = (header: Header, footer: Footer, proportionOfHardwareSalesQtr: KPIQuarter[], proportionOfHardwareSalesCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.proportionOfHardwareSales}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(proportionOfHardwareSalesQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfHardwareSalesCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.proportionOfHardwareSales}`;
 
-export const printDigitalSales = (header: Header, footer: Footer, digitalSalesQtr: KPIQuarter[], digitalSalesCml: KPICumulative[], currentQuarter: number) => 
-`+${"-".repeat(30)}+
-${header.digitalSales}
-+${"-".repeat(30)}+
-${printSectionQuarters(digitalSalesQtr, currentQuarter)}
-+${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(digitalSalesCml, currentQuarter) : "=".repeat(30)+"+" }
-+${"-".repeat(30)+"+"}
-${footer.digitalSales}`;
+// export const printProportionOfFirstPartySoftwareSales = (header: Header, footer: Footer, proportionOfFirstPartySoftwareSalesQtr: KPIQuarter[], proportionOfFirstPartySoftwareSalesCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.proportionOfFirstPartySoftwareSales}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(proportionOfFirstPartySoftwareSalesQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfFirstPartySoftwareSalesCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.proportionOfFirstPartySoftwareSales}`;
 
-export const printProportionOfDigitalSales = (header: Header, footer: Footer, proportionOfDigitalSalesQtr: KPIQuarter[], proportionOfDigitalSalesCml: KPICumulative[], currentQuarter: number) => 
-`+${"-".repeat(30)}+
-${header.proportionOfDigitalSales}
-+${"-".repeat(30)}+
-${printSectionQuarters(proportionOfDigitalSalesQtr, currentQuarter)}
-+${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfDigitalSalesCml, currentQuarter) : "=".repeat(30)+"+" }
-+${"-".repeat(30)+"+"}
-${footer.proportionOfDigitalSales}`;
+// export const printDigitalSales = (header: Header, footer: Footer, digitalSalesQtr: KPIQuarter[], digitalSalesCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.digitalSales}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(digitalSalesQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(digitalSalesCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.digitalSales}`;
 
-export const printProportionOfDLverPackagedSoftware = (header: Header, footer: Footer, proportionOfDLverPackagedSoftwareQtr: KPIQuarter[], proportionOfDLverPackagedSoftwareCml: KPICumulative[], currentQuarter: number) => 
-`+${"-".repeat(30)}+
-${header.proportionOfDLverPackagedSoftware}
-+${"-".repeat(30)}+
-${printSectionQuarters(proportionOfDLverPackagedSoftwareQtr, currentQuarter)}
-+${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfDLverPackagedSoftwareCml, currentQuarter) : "=".repeat(30)+"+" }
-+${"-".repeat(30)+"+"}
-${footer.proportionOfDLverPackagedSoftware}`;
+// export const printProportionOfDigitalSales = (header: Header, footer: Footer, proportionOfDigitalSalesQtr: KPIQuarter[], proportionOfDigitalSalesCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.proportionOfDigitalSales}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(proportionOfDigitalSalesQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfDigitalSalesCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.proportionOfDigitalSales}`;
+
+// export const printProportionOfDLverPackagedSoftware = (header: Header, footer: Footer, proportionOfDLverPackagedSoftwareQtr: KPIQuarter[], proportionOfDLverPackagedSoftwareCml: KPICumulative[], currentQuarter: number) => 
+// `+${"-".repeat(30)}+
+// ${header.proportionOfDLverPackagedSoftware}
+// +${"-".repeat(30)}+
+// ${printSectionQuarters(proportionOfDLverPackagedSoftwareQtr, currentQuarter)}
+// +${(currentQuarter > 1) ? "=".repeat(30)+"+\n" + printSectionCumulative(proportionOfDLverPackagedSoftwareCml, currentQuarter) : "=".repeat(30)+"+" }
+// +${"-".repeat(30)+"+"}
+// ${footer.proportionOfDLverPackagedSoftware}`;
