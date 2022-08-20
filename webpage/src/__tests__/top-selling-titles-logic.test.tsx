9fb39f6e6d457d036c9b924ad8e2a971de7e893f
r134x7 <r134x7@gmx.com>
2022-08-21 02:34:17 +1000

    setting up top selling titles logic test

---
+++ webpage/src/__tests__/top-selling-titles-logic.test.tsx
@@ -0,0 +1,57 @@
+import { Titles } from "../utils/top-selling-titles-logic";
+
+const title1: Titles[] = [
+    {
+        title: " Mario Kart 8 Deluxe ",
+        period: " 1st Quarter ",
+        value: 37.08,
+    },
+    {
+        title: " Mario Kart 8 Deluxe ",
+        period: " 2nd Quarter ",
+        value: 38.74,
+    },
+    {
+        title: " Mario Kart 8 Deluxe ",
+        period: " 3rd Quarter ",
+        value: 43.35,
+    },
+    {
+        title: " Mario Kart 8 Deluxe ",
+        period: " 4th Quarter ",
+        value: 45.33,
+    },
+    {
+        title: " Mario Kart 8 Deluxe ",
+        period: " Q4 Last FY ",
+        value: 35.39,
+    },
+]
+
+const title2: Titles[] = [
+    {
+        title: " Super Mario Odyssey ", 
+        period: " 1st Quarter ",
+        value: 21.40,
+    },
+    {
+        title: " Super Mario Odyssey ", 
+        period: " 2nd Quarter ",
+        value: 21.95,
+    },
+    {
+        title: " Super Mario Odyssey ", 
+        period: " 3rd Quarter ",
+        value: 23.02,
+    },
+    {
+        title: " Super Mario Odyssey ", 
+        period: " 4th Quarter ",
+        value: 23.50,
+    },
+    {
+        title: " Super Mario Odyssey ", 
+        period: " Q4 Last FY ",
+        value: 20.83,
+    },
+]
