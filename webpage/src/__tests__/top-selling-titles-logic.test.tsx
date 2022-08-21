fe2e20d173fcdaecd95f947bd40af68d6918174f
r134x7 <r134x7@gmx.com>
2022-08-21 17:36:03 +1000

    cleaned up code.

--- webpage/src/__tests__/top-selling-titles-logic.test.tsx
+++ webpage/src/__tests__/top-selling-titles-logic.test.tsx
@@ -57,14 +57,6 @@ const title2: Titles[] = [
     },
 ]
 
-// going to need to group the arrays
-// going to need to sort the arrays by the fourth quarter value of the current fy
-// going to need to use sort inside map??? sort mutates
-// create a function to sort then
-// use array destructuring for the output of the function
-// add rank? to the Titles type
-// map rank to each array.........
-// 
 // quarterly calculation...
 // reference earnings logic qc
 // no first halves, no first three quarters used
@@ -77,11 +69,9 @@ const title2: Titles[] = [
 // usual formatting, reference earnings logic
 // 
 
-// for using .sort and not mutating the original arrays use spread syntax, source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-// eg const sorted = [...array].sort()
-// source also has a mapping sort method
+// for using .sort and not mutating the original arrays use mapping sort method, source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 
-test("Arrays are sorted in descending order using 4th quarter as reference", () => {
+test("Arrays are sorted in descending order using current quarter as reference", () => {
 
     let currentQuarter = 4;
 
@@ -91,69 +81,20 @@ test("Arrays are sorted in descending order using 4th quarter as reference", () 
     ] as const;
 
     const testCollection = collection.map((elem, index, array) => {
-        // const [testOne, testTwo] = collection.map((elem, index, array) => {
-            // let x: Titles[] = [...elem]
-            return elem
-        // return x[currentQuarter-1]
-        // return x[index] === x[currentQuarter-1]
-    }).sort((a, b) => { // descending currently
+            return elem // we need to create a new array that is identical to the original due to sort's mutating properties.
+    }).sort((b, a) => { // (b,a) is ascending order, (a,b) sorts in descending order
         return (a[currentQuarter-1].value > b[currentQuarter-1].value)
             ? 1
             : (a[currentQuarter-1].value < b[currentQuarter-1].value)
             ? -1
             : 0
     }).map((elem, index) => {
-        // let x: Titles[] = [...elem]
-        // console.log(x)
-        // let y: Titles[] = [{...x, rank: index+1}]
-        // })
-        // return y
-        // let x: Titles[] = [...elem]
-        // console.log(elem)
-        const y: Titles[] = [...elem].map((elemTwo, indexTwo) => {
+        // x is a nested map so that the actual elements of the array can be accessed, the level above is arrays being the elements since it is a collection of arrays
+        const x: Titles[] = [...elem].map((elemTwo) => {
             return {...elemTwo, rank: index+1} 
         })
-        return y
+        return x // x which is the returned array is now returned to the array of arrays
     })
     
-    // const testOne = title1.filter((elem, index) => {
-        // return index === currentQuarter-1
-    // })
-    // .map((elem, index, array) => {
-        // return { index, value: elem.value }
-    // })
-
-
-    // console.log(testOne)
-    // console.log(testTwo)
     console.log(testCollection)
-
-    
-    // testCollection.sort((a, b) => { // sorts in descending order
-    // testCollection.sort((b, a) => { // sorts in ascending order
-    //     return (a.value > b.value) 
-    //         ? 1
-    //         : (a.value < b.value) 
-    //         ? -1
-    //         : 0
-    // })
-
-    // console.log(testCollection)
-
-
-    // const result: Titles[] = testCollection.map((elem, index) => {
-    //     return {...elem, rank: index+1}
-    // })
-    // // const result = testCollection.map((elem) => title1[elem.index])
-    // console.log(result)
-
-    // const newCollection = collection.map((elem, index, array) => {
-    //     if (result[index].title === elem[currentQuarter-1].title) {
-    //         return {...elem, rank: result[index].rank}
-    //     } else {
-    //         return elem
-    //     }
-    // })
-
-    // console.log(newCollection)
 })
