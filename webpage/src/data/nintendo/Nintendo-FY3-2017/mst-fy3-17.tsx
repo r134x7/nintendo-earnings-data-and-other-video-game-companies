4f6cf92c1aa67211ff47b062dc05e7775e2cbd02
r134x7 <r134x7@gmx.com>
2022-08-25 18:45:34 +1000

    setting up logic and fy3/17 data

---
+++ webpage/src/data/nintendo/Nintendo-FY3-2017/mst-fy3-17.tsx
@@ -0,0 +1,107 @@
+import {
+    Titles,
+    Header,
+    decimateCalculation,
+    quarterlyCalculation,
+} from "../../../utils/fy-million-seller-titles-logic";
+
+const currentQuarter = 4;
+
+const title1: Titles[] = [
+    {
+        title: " The Legend of Zelda: Breath of the Wild ",
+        period: " 1st Quarter  ",
+        regionA: "   Japan ",
+        valueA: 0,
+        regionB: " Overseas",
+        valueB: 0,
+        regionC: "   WW FY ",
+        valueC: 0,
+        regionD: "  WW LTD ",
+        valueD: 0,
+    },
+    {
+        title: " The Legend of Zelda: Breath of the Wild ",
+        period: " 2nd Quarter  ",
+        regionA: "   Japan ",
+        valueA: 0,
+        regionB: " Overseas",
+        valueB: 0,
+        regionC: "   WW FY ",
+        valueC: 0,
+        regionD: "  WW LTD ",
+        valueD: 0,
+    },
+    {
+        title: " The Legend of Zelda: Breath of the Wild ",
+        period: " 3rd Quarter  ",
+        regionA: "   Japan ",
+        valueA: 0,
+        regionB: " Overseas",
+        valueB: 0,
+        regionC: "   WW FY ",
+        valueC: 0,
+        regionD: "  WW LTD ",
+        valueD: 0,
+    },
+    {
+        title: " The Legend of Zelda: Breath of the Wild ",
+        period: " 4th Quarter  ",
+        regionA: "   Japan ",
+        valueA: 39,
+        regionB: " Overseas",
+        valueB: 237,
+        regionC: "   WW FY ",
+        valueC: 276,
+        regionD: "  WW LTD ",
+        valueD: 276,
+    },
+    {
+        title: " The Legend of Zelda: Breath of the Wild ",
+        period: " 1st Quarter  ",
+        regionA: "   Japan ",
+        valueA: 0,
+        regionB: " Overseas",
+        valueB: 0,
+        regionC: "   WW FY ",
+        valueC: 0,
+        regionD: "  WW LTD ",
+        valueD: 0,
+    },
+]
+
+const collection = [
+    title1,
+] as const;
+
+const sortedCollection = collection.map((elem, index, array) => {
+            return elem // we need to create a new array that is identical to the original due to sort's mutating properties.
+    }).sort((b, a) => { // (b,a) is descending order, (a,b) sorts in ascending order
+        return (a[currentQuarter-1].valueC > b[currentQuarter-1].valueC)
+            ? 1
+            : (a[currentQuarter-1].valueC < b[currentQuarter-1].valueC)
+            ? -1
+            : 0 // 4th quarter WW FY is index 11
+    }).map((elem, index) => {
+        // x is a nested map so that the actual elements of the array can be accessed, the level above is arrays being the elements since it is a collection of arrays
+        const x: Titles[] = [...elem].map((elemTwo) => {
+            return {...elemTwo, rank: index+1} 
+        })
+        return x // x which is the returned array is now returned to the array of arrays
+    })
+
+export const [
+    title1Sorted,
+] = sortedCollection.map((elem) => {
+    return decimateCalculation(elem)
+})
+
+export const [
+    title1Difference,
+
+] = sortedCollection.map((elem) => {
+    return decimateCalculation(elem)
+}).map((elem) => {
+    return quarterlyCalculation(elem)
+})
+
