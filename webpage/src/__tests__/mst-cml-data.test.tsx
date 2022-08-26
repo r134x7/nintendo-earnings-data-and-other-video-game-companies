5fd416d03411983cfba4c2fd030eb527f2ca711c
r134x7 <r134x7@gmx.com>
2022-08-26 22:43:44 +1000

    finding way to filter by index...

--- webpage/src/__tests__/mst-cml-data.test.tsx
+++ webpage/src/__tests__/mst-cml-data.test.tsx
@@ -5,6 +5,8 @@ import { collection as fy3_20_collection } from "../data/nintendo/Nintendo-FY3-2
 import { collection as fy3_21_collection } from "../data/nintendo/Nintendo-FY3-2021/mst-fy3-21";
 import { collection as fy3_22_collection } from "../data/nintendo/Nintendo-FY3-2022/mst-fy3-22";
 
+import { Titles } from "../utils/fy-million-seller-titles-logic"
+
     const totalCollection = [
         fy3_17_collection,
         fy3_18_collection,
@@ -13,6 +15,30 @@ import { collection as fy3_22_collection } from "../data/nintendo/Nintendo-FY3-2
         fy3_21_collection,
         fy3_22_collection,
     ] as const;
+
+    function accumulate(title: Titles[][]) {
+
+        const japanTitle1 = title.map((elem, index, array) => {
+            return elem[0].valueA
+        }).reduce((prev, next) => prev + next)
+    
+        
+        const overseasTitle1 = title.map((elem, index, array) => {
+            return elem[0].valueB
+        }).reduce((prev, next) => prev + next)
+        
+        
+        const title1Flat = title.flatMap((flat) => flat).reduce((prev, next) => {
+            return {...prev, ...next}
+        })
+    
+        return {
+                ...title1Flat, 
+                valueA: japanTitle1, 
+                valueB: overseasTitle1, 
+                valueC: 0
+            }
+    } 
 
 test("mapping and filtering one title...", () => {
 
@@ -98,4 +124,85 @@ test("filtering to a title with the same name and then reduce...", () => {
     // console.log(title1);
     // valueA sum: 122 + 62 + 61 + 33 = 278
         
+})
+
+test("combine methods into one function", () => {
+
+    const flatCollection = totalCollection.flatMap((elem) => elem).map((elem) => {
+        return elem.filter((secondElem, index, array) => {
+            return secondElem.valueC !== 0 && secondElem.period === " 4th Quarter  "
+        })
+    }).filter((elem) => elem.length !== 0)
+
+    const title1 = flatCollection.map((elem, index) => {
+        return elem.filter((secondElem, secondIndex) => {
+            return secondElem.title === " Super Mario Party "
+        })
+    }).filter((elem) => elem.length !== 0)
+
+    function accumulate(title: Titles[][]) {
+
+        const japanTitle1 = title.map((elem, index, array) => {
+            return elem[0].valueA
+        }).reduce((prev, next) => prev + next)
+    
+        
+        const overseasTitle1 = title.map((elem, index, array) => {
+            return elem[0].valueB
+        }).reduce((prev, next) => prev + next)
+        
+        
+        const title1Flat = title.flatMap((flat) => flat).reduce((prev, next) => {
+            return {...prev, ...next}
+        })
+    
+        return {
+                ...title1Flat, 
+                valueA: japanTitle1, 
+                valueB: overseasTitle1, 
+                valueC: 0
+            }
+    } 
+
+    console.log(accumulate(title1));
+    
+
+})
+
+test("sort the titles into separate arrays...", () => {
+
+    // const flatCollection = totalCollection.flatMap((elem) => elem).map((elem) => {
+    //     return elem.filter((secondElem, index, array) => {
+    //         return secondElem.valueC !== 0 && secondElem.period === " 4th Quarter  "
+    //     })
+    // }).filter((elem) => elem.length !== 0)
+
+    // console.log(flatCollection);
+
+    // console.log(totalCollection);
+
+    // console.log(totalCollection[1][0]);
+
+    const testTitle1 = totalCollection.map((elem, index, array) => {
+        // console.log(elem[0]);
+        
+        return elem[0]
+
+    }).filter((elem) => elem.length !== 0)
+
+    console.log(testTitle1);
+
+    const filterTitle1 = testTitle1.map((elem) => {
+        return elem.filter((secondElem, index, array) => {
+            return secondElem.valueC !== 0 && secondElem.period === " 4th Quarter  "
+        })
+    }).filter((elem) => elem.length !== 0)
+
+    console.log(filterTitle1);
+    
+    console.log(accumulate(filterTitle1));
+    
+    
+    
+    
 })
