5b1245bd5c307da373592f0ad463fbee883d83a4
r134x7 <r134x7@gmx.com>
2022-08-31 21:16:22 +1000

    added fy3/18 data

--- webpage/src/data/nintendo/Nintendo-FY3-2018/nsw-hardware-software-fy3-18.tsx
+++ webpage/src/data/nintendo/Nintendo-FY3-2018/nsw-hardware-software-fy3-18.tsx
@@ -1,2 +1,280 @@
+import {
+    Section,
+    Header,
+    printHead,
+    printSections,
+    quarterlyCalculation,
+    yearOnYearCalculation
+} from "../../../utils/hardware-software-units-logic";
 
-export const well = null
+const currentQuarter = 4;
+
+const nintendoSwitchHardwareTotal: Section[] = [
+    {
+        name: " Hardware Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 197,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 489,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 1213,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 1505,
+    },
+    {
+        name: " Hardware Total ",
+        period: " Last FY Cumulative ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 274,
+    },
+]
+
+const nintendoSwitchHardwareTotalForecast: Section[] = [
+    {
+        name: " Hardware Total ",
+        period: "Forecast ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 1000,
+    },
+    {
+        name: " Hardware Total ",
+        period: "Forecast ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 1400,
+    },
+    {
+        name: " Hardware Total ",
+        period: "Forecast ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 1500,
+    },
+    {
+        name: " Hardware Total ",
+        period: "Forecast ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 2000,
+    },
+]
+
+const nintendoSwitchHardwareTotalLastFY: Section[] = [
+    {
+        name: " Hardware Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Hardware Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 274,
+    },
+]
+
+const nintendoSwitchSoftwareTotal: Section[] = [
+    {
+        name: " Software Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 814,
+    },
+    {
+        name: " Software Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 2202,
+    },
+    {
+        name: " Software Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 4710,
+    },
+    {
+        name: " Software Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 6351,
+    },
+    {
+        name: " Software Total ",
+        period: " Last FY Cumulative ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 546,
+    },
+]
+
+const nintendoSwitchSoftwareTotalForecast: Section[] = [
+    {
+        name: " Software Total ",
+        period: "Forecast ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 3500,
+    },
+    {
+        name: " Software Total ",
+        period: "Forecast ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 5000,
+    },
+    {
+        name: " Software Total ",
+        period: "Forecast ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 5300,
+    },
+    {
+        name: " Software Total ",
+        period: "Forecast ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 10000,
+    }
+]
+
+const nintendoSwitchSoftwareTotalLastFY: Section[] = [
+    {
+        name: " Software Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Software Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Software Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "units",
+        value: 0,
+    },
+    {
+        name: " Software Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "units",
+        value: 546,
+    },
+]
+
+const header: Header = {
+    fiscalYear: " FY3/2017 ",
+    nextFiscalYearShort: " FY3/18 ",
+    switchHeader: "| Nintendo Switch   |",
+    secondHeader: "| Sales Units and Forecast     |",
+}
+
+export const quarterlyCollection = [
+    nintendoSwitchHardwareTotal,
+    nintendoSwitchHardwareTotalLastFY,
+    nintendoSwitchSoftwareTotal,
+    nintendoSwitchSoftwareTotalLastFY,
+] as const;
+
+const filteredCollection = [
+    nintendoSwitchHardwareTotal,
+    nintendoSwitchSoftwareTotal,
+] as const;
+
+const [quarterHardwareTotal, quarterHardwareTotalLastFY, quarterSoftwareTotal, quarterSoftwareTotalLastFY] = quarterlyCollection.map((elem, index) => {
+
+    return (index % 2 === 0)
+            ? quarterlyCalculation(elem).filter((elem, index, array) => index !== array.length-1) // filter out last fy cumulative
+            : quarterlyCalculation(elem) // last FY numbers...
+})
+
+const [nintendoSwitchHardwareTotalFiltered, nintendoSwitchSoftwareTotalFiltered] = filteredCollection.map((elem) => {
+    return elem.filter((secondElem, index, array) => {
+        return index !== array.length-1 
+    })
+})
+
+const yearOnYearCollection = [
+    quarterHardwareTotal,
+    quarterHardwareTotalLastFY,
+    quarterSoftwareTotal,
+    quarterSoftwareTotalLastFY,
+    nintendoSwitchHardwareTotalFiltered,
+    nintendoSwitchHardwareTotalLastFY,
+    nintendoSwitchSoftwareTotalFiltered,
+    nintendoSwitchSoftwareTotalLastFY,
+] as const;
+
+const [quarterlySwitchHardwareTotalYoy, quarterlySwitchSoftwareTotalYoy, cumulativeSwitchHardwareTotalYoy, cumulativeSwitchSoftwareTotalYoy] = yearOnYearCollection.map((elem, index, array) => {
+    return (index % 2 === 0)
+            ? yearOnYearCalculation(array[index], array[index+1])
+            : [];
+}).filter((elem) => elem.length !== 0) // filter out zero length arrays
+
+
+const [nintendoSwitchHardwareTotalCml, nintendoSwitchSoftwareTotalCml] = [
+    nintendoSwitchHardwareTotal,
+    nintendoSwitchSoftwareTotal,
+].map((elem) => {
+    return elem.filter((secondElem, index, array) => {
+        return index !== 0 // filter out first quarters
+    })
+})
+
+const printOne = printHead(header)
+
+const printTwo = printSections(header, quarterHardwareTotal, quarterlySwitchHardwareTotalYoy, nintendoSwitchHardwareTotalCml, cumulativeSwitchHardwareTotalYoy, nintendoSwitchHardwareTotalForecast, currentQuarter)
+
+const printThree = printSections(header, quarterSoftwareTotal, quarterlySwitchSoftwareTotalYoy, nintendoSwitchSoftwareTotalCml, cumulativeSwitchSoftwareTotalYoy, nintendoSwitchSoftwareTotalForecast, currentQuarter)
+
+export const printHarwareSoftware = 
+`${printOne}
+${printTwo}
+${printThree}`;
