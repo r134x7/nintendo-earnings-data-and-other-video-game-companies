d7eb316b9e07a6562c1c3edc7f507180804c40c2
r134x7 <r134x7@gmx.com>
2022-09-13 20:56:06 +1000

    put graphs to component, changed labels

--- webpage/src/components/NINTENDO_FY3_2021.tsx
+++ webpage/src/components/NINTENDO_FY3_2021.tsx
@@ -10,6 +10,8 @@ import { printRegions } from "../data/nintendo/Nintendo-FY3-2021/regional-hw-sw-
 import GRAPH_NINTENDO_EARNINGS_FY3_21 from "../graphs/nintendo/Nintendo-FY3-2021/GRAPH_NINTENDO_EARNINGS_FY3_21";
 import GRAPH_NINTENDO_KPI_FY3_21 from "../graphs/nintendo/Nintendo-FY3-2021/GRAPH_NINTENDO_KPI_FY3_21";
 import GRAPH_NINTENDO_TOP_SELLING_TITLES_SWITCH_FY3_21 from "../graphs/nintendo/Nintendo-FY3-2021/GRAPH_NINTENDO_TOP_SELLING_TITLES_SWITCH_FY3_21";
+import GRAPH_NINTENDO_MST_FY3_21 from "../graphs/nintendo/Nintendo-FY3-2021/GRAPH_NINTENDO_MST_FY3_21";
+import GRAPH_NINTENDO_NSW_HW_SW_FY3_21 from "../graphs/nintendo/Nintendo-FY3-2021/GRAPH_NINTENDO_NSW_HW_SW_FY3_21";
 
 export default function NINTENDO_FY3_21() {
 
@@ -24,7 +26,7 @@ export default function NINTENDO_FY3_21() {
             ? setData(consolidatedOperatingResults)
             : (value === "Global Hardware/Software units, Mobile/IP related income")
             ? setData(nintendoHardwareSoftwareMobile)
-            : (value === "Key/Digital Sales Indicator")
+            : (value === "Key/Digital Sales Indicators")
             ? setData(keyIndicators)
             : (value === "FY Million-Seller Titles")
             ? setData(fyMillionSellers)
@@ -108,7 +110,7 @@ export default function NINTENDO_FY3_21() {
                     data={[ "Data Sources",
                             "Consolidated Operating Results", 
                             "Global Hardware/Software units, Mobile/IP related income", 
-                            "Key/Digital Sales Indicator", 
+                            "Key/Digital Sales Indicators", 
                             "FY Million-Seller Titles", 
                             "Regional Hardware/Software units", 
                             "Top Selling Titles",]}
@@ -118,10 +120,14 @@ export default function NINTENDO_FY3_21() {
             <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
             {(value === "Consolidated Operating Results")
                 ? <GRAPH_NINTENDO_EARNINGS_FY3_21 />
-                : (value === "Key/Digital Sales Indicator")
+                : (value === "Global Hardware/Software units, Mobile/IP related income")
+                ? <GRAPH_NINTENDO_NSW_HW_SW_FY3_21 />
+                : (value === "Key/Digital Sales Indicators")
                 ? <GRAPH_NINTENDO_KPI_FY3_21 />
+                : (value === "FY Million-Seller Titles")
+                ? <GRAPH_NINTENDO_MST_FY3_21 /> 
                 : (value === "Top Selling Titles")
-                ? <GRAPH_NINTENDO_TOP_SELLING_TITLES_SWITCH_FY3_21 /> 
+                ? <GRAPH_NINTENDO_TOP_SELLING_TITLES_SWITCH_FY3_21 />
                 : null
             }
             <Space h="xl" />
