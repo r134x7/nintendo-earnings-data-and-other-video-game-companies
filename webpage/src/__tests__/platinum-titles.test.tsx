3690552018af02bdb3b378a8154946c83a6779ef
r134x7 <r134x7@gmx.com>
2022-09-15 16:17:45 +1000

    setting up platinum titles logic using FY million seller logic

---
+++ webpage/src/__tests__/platinum-titles.test.tsx
@@ -0,0 +1,30 @@
+export type Titles = {
+    title: string,
+    period: " 1st Quarter  " | " 2nd Quarter  " | " 3rd Quarter  " | " 4th Quarter  " | " Last FY Total ",
+    regionA: "   Japan ", 
+    valueA: number,
+    regionB: " Overseas",
+    valueB: number,
+    regionC: "   WW FY ",
+    valueC: number,
+    regionD: "  WW LTD ",
+    valueD: number,
+    rank?: number,
+    label?: " New! " | " Recurring ",
+    miscellaneous?: string,
+}
+
+
+export type Header = {
+    switchHeader: "| Nintendo Switch FY Million-Seller Titles |",
+    secondHeader: "| Title and Rank                           |",
+    thirdHeader: "| Units                                    |",
+    fiscalYear: string,
+    areaHeader: "| Area         |   Japan | Overseas|",
+    globalHeader: "| Global       |   WW FY |  WW LTD |",
+    switchSummaryHeader: "| Nintendo Switch FY    |\n| Million-Seller Titles |\n",
+    japanSummaryHeader: "| Japan                           |",
+    overseasSummaryHeader: "| Overseas                        |",
+    globalFYSummaryHeader: "| Global FY                       |",
+    globalLTDSummaryHeader: "| Global LTD                      |",
+}
