cfdd3249ca44dba06653bfbeb0cfc432863f1ec6
r134x7 <r134x7@gmx.com>
2022-09-18 12:10:41 +1000

    fixed imports, finished setting up all print

--- webpage/src/components/capcom/CAPCOM_FY3_2023.tsx
+++ webpage/src/components/capcom/CAPCOM_FY3_2023.tsx
@@ -1,1 +1,63 @@
-export {}
+import { useState, useEffect } from "react";
+import { Code, SegmentedControl, Anchor, Text, Stack, Space } from "@mantine/core";
+import { useSelector } from "react-redux";
+import { printFYPlatinumTitles } from "../../data/capcom/Capcom_FY3_2023/platinum_titles_fy3_23";
+
+export default function CAPCOM_FY3_23() {
+
+    const [data, setData] = useState("");
+    const [value, setValue] = useState("");
+    const [sources, setSources] = useState(<></>)
+
+    const state: any = useSelector(state => state);
+
+    useEffect(() => {
+        (value === "FY Platinum Titles")
+            ? setData(platinumFYTitles)
+            : setData("");
+
+        (value === "Data Sources")
+            ? setSources(DATA_SOURCES)
+            : setSources(<></>)
+
+    }, [value])
+
+    function DATA_SOURCES() {
+
+        return (
+            <Text>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        )
+    };
+
+    const platinumFYTitles = printFYPlatinumTitles;
+
+    return (
+
+        <div>  
+            <SegmentedControl
+                    fullWidth 
+                    orientation="vertical"
+                    value={value}
+                    onChange={setValue}
+                    data={[ "Data Sources",
+                            "FY Platinum Titles",
+                        ]}
+            />
+            
+            {sources}
+            <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
+            <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+        </div>
+        
+    );
+}
