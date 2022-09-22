e23bff9c5608c4deb702a76dbb222b25ce371db2
r134x7 <r134x7@gmx.com>
2022-09-23 01:18:30 +1000

    creating real data to use instead of test data...

---
+++ webpage/src/data/bandaiNamco/Bandai_Namco_FY3_2021/bandai_namco_annual_report_fy3_21.tsx
@@ -0,0 +1,11 @@
+import { Series, Header } from "../../../__tests__/bandai_namco_fact_sheet_logic.test";
+
+const series1: Series = 
+    {
+        title: " Super Robot Wars ",
+        releaseDate: " April 1991 ",
+        fyEndMonth: " March 2021 ",
+        value: 19.24,
+        valueLastFY: 19.10,
+        valueLastTwoFYs: 18.77,
+    };
