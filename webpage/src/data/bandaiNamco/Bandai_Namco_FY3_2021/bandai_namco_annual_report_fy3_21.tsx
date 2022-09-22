8c1ecf73c166a9784547528673771358ff27c346
r134x7 <r134x7@gmx.com>
2022-09-23 01:31:56 +1000

    data setup

--- webpage/src/data/bandaiNamco/Bandai_Namco_FY3_2021/bandai_namco_annual_report_fy3_21.tsx
+++ webpage/src/data/bandaiNamco/Bandai_Namco_FY3_2021/bandai_namco_annual_report_fy3_21.tsx
@@ -9,3 +9,44 @@ const series1: Series =
         valueLastFY: 19.10,
         valueLastTwoFYs: 18.77,
     };
+
+const series2: Series = 
+    {
+        title: " Tekken ",
+        releaseDate: " March 1995 ",
+        fyEndMonth: " March 2021 ",
+        value: 52.51,
+        valueLastFY: 50.52,
+        valueLastTwoFYs: 48.39,
+    };
+
+const series3: Series = 
+    {
+        title: " Tales... of ",
+        releaseDate: " December 1995 ",
+        fyEndMonth: " March 2021 ",
+        value: 24.84,
+        valueLastFY: 23.86,
+        valueLastTwoFYs: 21.60,
+    };
+
+const series4: Series = 
+    {
+        title: " Ultimate Ninja Storm ",
+        releaseDate: " January 2009 ",
+        fyEndMonth: " March 2021 ",
+        value: 20.80,
+        valueLastFY: 15.66,
+        valueLastTwoFYs: 13.95,
+    };
+
+const series5: Series = 
+    {
+        title: " Dark Souls ",
+        releaseDate: " September 2011 ",
+        fyEndMonth: " March 2021 ",
+        value: 29.34,
+        valueLastFY: 25.22,
+        valueLastTwoFYs: 22.19,
+        miscellaneous: "Note: Total for overseas sales for which BANDAI NAMCO Entertainment Inc. was the original seller (excluding domestic sales by FromSoftware, Inc.)",
+    };
