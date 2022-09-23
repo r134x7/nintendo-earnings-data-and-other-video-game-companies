ef496bb51f74ba275886d530a3ad4691f99db2f1
r134x7 <r134x7@gmx.com>
2022-09-23 16:04:00 +1000

    setup logic, setup data for fy3/21

--- webpage/src/data/bandaiNamco/Bandai_Namco_FY3_2021/bandai_namco_annual_report_fy3_21.tsx
+++ webpage/src/data/bandaiNamco/Bandai_Namco_FY3_2021/bandai_namco_annual_report_fy3_21.tsx
@@ -1,4 +1,9 @@
-import { Series, Header } from "../../../__tests__/bandai_namco_fact_sheet_logic.test";
+import { 
+        Header,
+        Series,
+        printHead,
+        printSeriesOutput,
+    } from "../../../utils/bandai_namco_annual_report_logic";
 
 const series1: Series = 
     {
@@ -51,6 +56,17 @@ const series5: Series =
         miscellaneous: "Note: Total for overseas sales for which BANDAI NAMCO Entertainment Inc. was the original seller (excluding domestic sales by FromSoftware, Inc.)",
     };
 
+const header: Header = {
+    bandaiNamcoHeader: "| Bandai Namco - IP Series Data  |",
+    secondHeader: "| First appearance to recent FY |",
+    thirdHeader: "| Rank                           |",
+    fourthHeader: "| Units                          |",
+    ltd: "| Life-To-Date       |",
+    fiscalYear:  "| FY3/21 Cumulative  |",
+    fiscalYearYoY: "| FY3/21 Cml. YoY%   |",
+    summaryHeader: " Placeholder ",
+}
+
 export const collection = [
     series1,
     series2,
@@ -58,3 +74,27 @@ export const collection = [
     series4,
     series5,
 ] as const;
+
+const sortedFYCollection: Series[] = collection.filter((elem, index, array) => {
+            // return elem.value - elem.valueLastFY !== 0 // probably shouldn't make two separate tables for FY and ALL...
+            return elem // forgetting filter doesn't do anything here...
+            // we need to create a new array that is identical to the original due to sort's mutating properties. filter titles that sold units this FY
+    }).sort((b, a) => { // (b,a) is descending order, (a,b) sorts in ascending order
+        return (a.value - a.valueLastFY > b.value - b.valueLastFY)
+            ? 1
+            : (a.value - a.valueLastFY < b.value - b.valueLastFY)
+            ? -1
+            : 0
+    }).map((elem, index) => {
+        return {...elem, rank: index+1}
+    })
+
+    let printedSeries = sortedFYCollection.map((elem) => {
+        return printSeriesOutput(elem)(header)(42)(11)(32);
+    }).reduce((prev, next) => prev + "\n" + next)
+
+    let printOne = printHead(header);
+
+export const printSeriesFY =
+`${printOne}
+${printedSeries}`;
