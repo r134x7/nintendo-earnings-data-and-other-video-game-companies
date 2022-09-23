7250626b628a969b06c37fb97937bdcd0801a988
r134x7 <r134x7@gmx.com>
2022-09-23 16:19:56 +1000

    put fy3/20 data to component

---
+++ webpage/src/components/bandaiNamco/BANDAI_NAMCO_FY3_2020.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { Code, SegmentedControl, Anchor, Text, Stack, Space } from "@mantine/core";
+import { useSelector } from "react-redux";
+import { printSeriesFY } from "../../data/bandaiNamco/Bandai_Namco_FY3_2020/bandai_namco_annual_report_fy3_20" 
+
+export default function BANDAI_NAMCO_FY3_2020() {
+
+    const [data, setData] = useState("");
+    const [value, setValue] = useState("");
+    const [sources, setSources] = useState(<></>)
+
+    const state: any = useSelector(state => state);
+
+    useEffect(() => {
+        (value === "FY Series IP")
+            ? setData(SeriesFY)
+            : setData("");
+
+        (value === "Data Sources")
+            ? setSources(DATA_SOURCES)
+            : setSources(<></>)
+
+    }, [value])
+
+    function DATA_SOURCES() {
+
+        return (
+            <Text>
+                <Stack align="center">
+                        Bandai Namco Group Fact Book 2020: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/integrated/pdf/2020EN_fact.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/integrated/pdf/2020EN_fact.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        )
+    };
+
+    const SeriesFY = printSeriesFY;
+
+    return (
+
+        <div>  
+            <SegmentedControl
+                    fullWidth 
+                    orientation="vertical"
+                    value={value}
+                    onChange={setValue}
+                    data={[ "Data Sources",
+                            "FY Series IP",
+                        ]}
+            />
+            
+            {sources}
+            <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
+            <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+            <Space h="xl" />
+        </div>
+        
+    );
+}
