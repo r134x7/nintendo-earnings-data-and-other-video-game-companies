1d35bb2623b8182e4b912193eda8196b03668528
r134x7 <r134x7@gmx.com>
2022-09-28 03:47:40 +1000

    changed variable names

--- webpage/src/__tests__/fizzBuzz.test.tsx
+++ webpage/src/__tests__/fizzBuzz.test.tsx
@@ -11,22 +11,23 @@ export const fizzBuzz = (index: number) => {
         };
     };
 
-    return (array: [string, number][]) => {
-        array.map(elem => helper(elem));
+    return (list: [string, number][]) => {
+        list.map(elem => helper(elem));
 
         return (!output) ? index : output;
     };
 };
 
 test("fizz...", () => {
-    let y: [string, number][] = [
+    let tupleList: [string, number][] = [
         ["Fizz", 3],
         ["Buzz", 5],
         ["Bazz", 7],
     ];
 
-    let x = Array.from({length: 100},(v,index) => fizzBuzz(index+1)(y)
-        ).reduce((acc, next) => acc + "\n" + next);
+    let x = Array.from({length: 100},(v,index) => fizzBuzz(index+1)(tupleList))
+                 .reduce((acc, next) => acc + "\n" + next);
+
     console.log(x);
     
 })
