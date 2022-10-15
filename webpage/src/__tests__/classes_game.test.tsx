a60cdba45ecdb4998e3b33db68e751447b6859f8
r134x7 <r134x7@gmx.com>
2022-10-15 14:23:07 +1100

    composing field into X class

--- webpage/src/__tests__/classes_game.test.tsx
+++ webpage/src/__tests__/classes_game.test.tsx
@@ -3,13 +3,13 @@ export {}
 class Field {
     private xPosition: number[] = [0, 1, 2];
     private yPosition: number[] = [0, 1, 2];
-    private currentPositionX: number;
-    private currentPositionY: number;
+    // private currentPositionX: number;
+    // private currentPositionY: number;
     
-    constructor(currentPositionX: number, currentPositionY: number) {
-        this.currentPositionX = currentPositionX;
-        this.currentPositionY = currentPositionY;
-    }
+    // constructor(currentPositionX: number, currentPositionY: number) {
+    //     this.currentPositionX = currentPositionX;
+    //     this.currentPositionY = currentPositionY;
+    // }
 
     get getX() {
         return this.xPosition;
@@ -18,26 +18,20 @@ class Field {
     get getY() {
         return this.yPosition;
     }
-
-    get getCurrentPositionX() {
-        return this.currentPositionX;
-    }
-
-    get getCurrentPositionY() {
-        return this.currentPositionY;
-    }
 }
 
 class X {
 
     // private moveX: number;
     // private moveY: number;
+    private fieldBoundaries: Field;
     private currentPositionX: number;
     private currentPositionY: number;
 
     constructor(
         // moveX: number,
         // moveY: number,
+        fieldBoundaries: Field,
         currentPositionX: number,
         currentPositionY: number,
     ) {
@@ -45,6 +39,7 @@ class X {
         // this.moveY = moveY;
         this.currentPositionX = currentPositionX;
         this.currentPositionY = currentPositionY;
+        this.fieldBoundaries = fieldBoundaries;
     }
 
     get getCurrentPositionX() {
@@ -62,25 +57,34 @@ class X {
     set setCurrrentPositionY(y: number) {
         this.currentPositionY = y;
     }
+
+    get getFieldBoundaries() {
+        return this.fieldBoundaries
+    }
 }
 
 test("make field and get...", () => {
 
-        const field = new Field(0,2);
+        // const field = new Field(0,2);
+        const field = new Field();
         console.log(field.getX);
         console.log(field.getY);
-        console.log(field.getCurrentPositionX);
-        console.log(field.getCurrentPositionY);
+        // console.log(field.getCurrentPositionX);
+        // console.log(field.getCurrentPositionY);
         
 })
 
 test("make X, set and get", () => {
 
-    const person = new X(99, 324);
+    const field = new Field();
+    const person = new X(field, 10, 10);
     console.log(person.getCurrentPositionX);
     console.log(person.getCurrentPositionY);
     console.log(person.setCurrentPositionX = 20);
     console.log(person.getCurrentPositionX);
     console.log(person.setCurrrentPositionY = 0);
     console.log(person.getCurrentPositionY);
+    console.log(person.getFieldBoundaries.getX);
+    console.log(person.getFieldBoundaries.getY);
+    console.log(person.getFieldBoundaries);
 })
