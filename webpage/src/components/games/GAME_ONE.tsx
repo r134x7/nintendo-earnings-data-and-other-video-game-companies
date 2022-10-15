2a8b439cda2628bdeb656b0b614d3aca963e424f
r134x7 <r134x7@gmx.com>
2022-10-15 21:27:33 +1100

    setting up game one...

--- webpage/src/components/games/GAME_ONE.tsx
+++ webpage/src/components/games/GAME_ONE.tsx
@@ -3,6 +3,12 @@ import { Unit } from "../../classes/Unit";
 
 export default function GAME_ONE() {
 
+    const field = new Field(2,2);
+    const playerOne = new Unit(field, 0, 0, 100, 10);
+    const playerTwo = new Unit(field, field.getX, field.getY, 100, 10);
+
+    // need to think how to set up a field visually...
+
     return (
         <div>
             test...
