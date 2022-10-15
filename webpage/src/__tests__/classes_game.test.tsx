421096299bab8ed71c0171c13e4d03c8206e79ef
r134x7 <r134x7@gmx.com>
2022-10-15 14:12:45 +1100

    created X object test

--- webpage/src/__tests__/classes_game.test.tsx
+++ webpage/src/__tests__/classes_game.test.tsx
@@ -28,6 +28,42 @@ class Field {
     }
 }
 
+class X {
+
+    // private moveX: number;
+    // private moveY: number;
+    private currentPositionX: number;
+    private currentPositionY: number;
+
+    constructor(
+        // moveX: number,
+        // moveY: number,
+        currentPositionX: number,
+        currentPositionY: number,
+    ) {
+        // this.moveX = moveX;
+        // this.moveY = moveY;
+        this.currentPositionX = currentPositionX;
+        this.currentPositionY = currentPositionY;
+    }
+
+    get getCurrentPositionX() {
+        return this.currentPositionX
+    }
+
+    get getCurrentPositionY() {
+        return this.currentPositionY
+    }
+
+    set setCurrentPositionX(x: number) {
+        this.currentPositionX = x;
+    }
+
+    set setCurrrentPositionY(y: number) {
+        this.currentPositionY = y;
+    }
+}
+
 test("make field and get...", () => {
 
         const field = new Field(0,2);
@@ -36,7 +72,15 @@ test("make field and get...", () => {
         console.log(field.getCurrentPositionX);
         console.log(field.getCurrentPositionY);
         
-        
+})
 
-        
+test("make X, set and get", () => {
+
+    const person = new X(99, 324);
+    console.log(person.getCurrentPositionX);
+    console.log(person.getCurrentPositionY);
+    console.log(person.setCurrentPositionX = 20);
+    console.log(person.getCurrentPositionX);
+    console.log(person.setCurrrentPositionY = 0);
+    console.log(person.getCurrentPositionY);
 })
