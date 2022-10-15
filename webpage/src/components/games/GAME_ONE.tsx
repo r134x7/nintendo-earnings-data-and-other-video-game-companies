e408f15929290d82d97f78e212d1e788fd2001fc
r134x7 <r134x7@gmx.com>
2022-10-15 17:56:35 +1100

    setting up component and page

---
+++ webpage/src/components/games/GAME_ONE.tsx
@@ -0,0 +1,6 @@
+import { Field } from "../../classes/Field";
+import { Unit } from "../../classes/Unit";
+
+export default function GAME_ONE() {
+
+}
---
+++ webpage/src/pages/Games.tsx
@@ -0,0 +1,10 @@
+import GAME_ONE from "../components/games/GAME_ONE"
+
+export default function Games() {
+
+    return (
+        <div>
+            test...
+        </div>
+    )
+}
