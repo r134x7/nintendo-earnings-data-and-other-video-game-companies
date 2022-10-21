e3e14023739dc7d45f30f3ec67d715c953ff3568
r134x7 <r134x7@gmx.com>
2022-10-21 22:43:39 +1100

    changed x length co-ordinates

--- webpage_v2/src/components/games/GAME_TWO.tsx
+++ webpage_v2/src/components/games/GAME_TWO.tsx
@@ -1,20 +1,20 @@
 import { Code, Button, SimpleGrid } from "@mantine/core";
-import { useHotkeys } from "@mantine/hooks";
+import { useHotkeys, useInterval } from "@mantine/hooks";
 import { useState } from "react";
 import { Field } from "../../classes/Field";
 import { Unit } from "../../classes/Unit";
 
 // need to try making a function here that spawns objects...
 function ball() {
-    return new Unit(objectField, 0, 3, 1, 0, "Pizza")
+    return new Unit(objectField, 0, 4, 1, 0, "Pizza")
 }
 
-const objectField = new Field(2,3);
+const objectField = new Field(6,4);
 const field = new Field(2,0);
 const playerOne = new Unit(field, 0, 0, 3, 1, "X=----=O")
 
-// const makeBall = ball();
-const makeBall = new Unit(objectField, 0, 3, 1, 0, "Pizza")
+const makeBall = ball();
+// const makeBall = new Unit(objectField, 0, 3, 1, 0, "Pizza")
 // this is where I need to think about holding an array of objects
 
 export default function GAME_TWO() {
@@ -50,7 +50,7 @@ export default function GAME_TWO() {
         (xPosition: number) => {
 
             return (xPosition === player.getCurrentPositionX && yPosition === player.getCurrentPositionY)
-            ? " " + player.getAvatar + " "
+            ? " " + player.getAvatar + " ".repeat(10 - (player.getAvatar.length + 1))
             : "          "
         }
 
@@ -62,12 +62,14 @@ export default function GAME_TWO() {
     const ballPosOneX = ballPos(1);
     const ballPosTwoX = ballPos(2);
     const ballPosThreeX = ballPos(3);
+    const ballPosFourX = ballPos(4);
 
 const visualField = () =>
-`_|${ballPosThreeX(0)}
-_| ${ballPosTwoX(0)}   ${ballPosTwoX(1)}
-_|  ${ballPosOneX(0)} ${ballPosOneX(1)}  ${ballPosOneX(2)}
-_|   ${ballPosZeroX(0)}  ${ballPosZeroX(1)}  ${ballPosZeroX(2)}
+`_|${ballPosFourX(0)}       ${ballPosFourX(1)}
+_| ${ballPosThreeX(0)}   ${ballPosThreeX(1)}  ${ballPosThreeX(2)}    ${ballPosThreeX(3)}
+_|  ${ballPosTwoX(0)}   ${ballPosTwoX(1)}      ${ballPosTwoX(2)}    ${ballPosTwoX(3)}  ${ballPosTwoX(4)}   ${ballPosTwoX(5)}
+_|   ${ballPosOneX(0)} ${ballPosOneX(1)}        ${ballPosOneX(2)} ${ballPosOneX(3)}  ${ballPosOneX(4)}   ${ballPosOneX(5)} ${ballPosOneX(6)}
+_|    ${ballPosZeroX(0)}  ${ballPosZeroX(1)}  ${ballPosZeroX(2)}                      ${ballPosZeroX(4)}
 _|${playerPosX(0)}|${playerPosX(1)}|${playerPosX(2)}| PINEAPPLE
 ----------------------------------------`;
 
