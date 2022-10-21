518f0de4af011e3532fea83c6af757da6b5cf24f
r134x7 <r134x7@gmx.com>
2022-10-21 19:29:43 +1100

    setting up field and players

--- webpage_v2/src/components/games/GAME_TWO.tsx
+++ webpage_v2/src/components/games/GAME_TWO.tsx
@@ -1,12 +1,53 @@
-import { Code } from "@mantine/core";
+import { Code, Button, SimpleGrid } from "@mantine/core";
+import { useHotkeys } from "@mantine/hooks";
+import { Field } from "../../classes/Field";
+import { Unit } from "../../classes/Unit";
+
+// need to try making a function here that spawns objects...
+
+const field = new Field(3,0);
+const playerOne = new Unit(field, 0, 0, 3, 1, "X=----=O")
 
 export default function GAME_TWO() {
+
+    const right = () => {
+        // if (playerOne.getHitPoints <= 0 || playerTwo.getHitPoints <= 0) {
+        //     return
+        // }
+
+            // playerOne.incrementPositionXPlus()
+            // setPlayerField(visualField)
+            // setHitPoints(displayHP);
+    }
+
+    const left = () => {
+        // if (playerOne.getHitPoints <= 0 || playerTwo.getHitPoints <= 0) {
+        //     return
+        // }
+
+        //     playerOne.incrementPositionXMinus()
+        //     setPlayerField(visualField)
+        //     setHitPoints(displayHP);
+    }
+
+    useHotkeys([
+        ["ArrowLeft", () => left()],
+        ["ArrowRight", () => right()],
+    ]);
 
     return (
         <div>
             <Code block>
                 something
             </Code>
+            <SimpleGrid cols={2}>
+                <Button variant="outline" radius={"lg"} color="red"  fullWidth>
+                   Left 
+                </Button>
+                <Button variant="outline" radius={"lg"} color="red"  fullWidth>
+                    Right
+                </Button>
+            </SimpleGrid>
         </div>
     )
 }
