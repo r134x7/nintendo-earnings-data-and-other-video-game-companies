813494dcbce9768920ec964a629363e19cdf1c20
r134x7 <r134x7@gmx.com>
2022-10-21 18:50:14 +1100

    setting up selecting from multiple games

--- webpage_v2/src/pages/Games.tsx
+++ webpage_v2/src/pages/Games.tsx
@@ -1,8 +1,21 @@
 import GAME_ONE from "../components/games/GAME_ONE"
+import { Button } from "@mantine/core"
+import { useState } from "react"
 
 export default function Games() {
 
+    const [openGame, setOpenGame] = useState("");
+
     return (
-        <GAME_ONE />
+        (openGame === "")
+        ?
+        <div>
+            <Button aria-label='Play game one' radius="lg" fullWidth onClick={() => setOpenGame("Game One")} variant="outline" color="cyan">Game One</Button>
+                <br />
+            <Button aria-label='Play game two' radius="lg" fullWidth onClick={() => setOpenGame("Game Two")} variant="outline" color="cyan">Game Two</Button>
+        </div>
+        : (openGame === "Game One")
+        ? <GAME_ONE />
+        : null
     )
 }
