b0b636a5d382483c55a07ecfa6af41519e026d8f
r134x7 <r134x7@gmx.com>
2022-10-21 19:54:39 +1100

    function to create objects

--- webpage_v2/src/components/games/GAME_TWO.tsx
+++ webpage_v2/src/components/games/GAME_TWO.tsx
@@ -5,9 +5,16 @@ import { Field } from "../../classes/Field";
 import { Unit } from "../../classes/Unit";
 
 // need to try making a function here that spawns objects...
+function ball() {
+    return new Unit(objectField, 0, 3, 1, 0, "Pizza")
+}
 
-const field = new Field(3,0);
+const objectField = new Field(3,3);
+const field = new Field(2,0);
 const playerOne = new Unit(field, 0, 0, 3, 1, "X=----=O")
+
+let makeBall = ball();
+// this is where I need to think about holding an array of objects
 
 export default function GAME_TWO() {
 
@@ -51,7 +58,11 @@ export default function GAME_TWO() {
 
 
 const visualField = () =>
-`|${playerPosX(0)}|${playerPosX(1)}|${playerPosX(2)}|
+`_|
+_|
+_|
+_|
+_|${playerPosX(0)}|${playerPosX(1)}|${playerPosX(2)}|
 ----------------------------------------`;
 
     const [playerField, setPlayerField] = useState(visualField);
@@ -62,10 +73,10 @@ const visualField = () =>
                 {playerField}
             </Code>
             <SimpleGrid cols={2}>
-                <Button variant="outline" radius={"lg"} color="red"  fullWidth>
+                <Button variant="outline" radius={"lg"} color="red" onClick={left} fullWidth>
                    Left 
                 </Button>
-                <Button variant="outline" radius={"lg"} color="red"  fullWidth>
+                <Button variant="outline" radius={"lg"} color="red" onClick={right} fullWidth>
                     Right
                 </Button>
             </SimpleGrid>
