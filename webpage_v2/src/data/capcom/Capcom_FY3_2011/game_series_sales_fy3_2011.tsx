20035c4746f7f3764ad77ed8c4206ab30d5486da
r134x7 <r134x7@gmx.com>
2022-10-29 19:02:38 +1100

    setting up capcom game series logic and data

---
+++ webpage_v2/src/data/capcom/Capcom_FY3_2011/game_series_sales_fy3_2011.tsx
@@ -0,0 +1,57 @@
+import { 
+        Header,
+        Series,
+        printHead,
+        printSeriesOutput,
+    } from "../../../utils/capcom_game_series_logic";
+
+const series1: Series =
+    {
+        title: "Commando",
+        numberOfTitles: 2,
+        releaseDate: " April 1985 ",
+        value: 1.2,
+        valueLastFY: 0,
+        valueLastTwoFYs: 0,
+    };
+
+const series2: Series =
+    {
+        title: "1942",
+        numberOfTitles: 3,
+        releaseDate: " November 1984 ",
+        value: 1.2,
+        valueLastFY: 0,
+        valueLastTwoFYs: 0,
+    };
+
+
+const series3: Series =
+    {
+        title: "Ghosts'n Goblins",
+        numberOfTitles: 14,
+        releaseDate: " July 1985 ",
+        value: 4.1,
+        valueLastFY: 0,
+        valueLastTwoFYs: 0,
+    };
+
+const series4: Series = 
+    {
+        title: "Mega Man",
+        numberOfTitles: 129,
+        releaseDate: " December 1987 ",
+        value: 28.0,
+        valueLastFY: 0,
+        valueLastTwoFYs: 0,
+    };
+
+const series5: Series = 
+    {
+        title: "Mega Man",
+        numberOfTitles: 129,
+        releaseDate: " December 1987 ",
+        value: 28.0,
+        valueLastFY: 0,
+        valueLastTwoFYs: 0,
+    };
---
+++ webpage_v2/src/utils/capcom_game_series_logic.tsx
@@ -0,0 +1,172 @@
+export type Series = {
+    title: string,
+    numberOfTitles: number,
+    releaseDate: string,
+    value: number,
+    valueLastFY: number,
+    valueLastTwoFYs: number,
+    rank?: number,
+    miscellaneous?: string,
+};
+
+export type Header = {
+    bandaiNamcoHeader: "| Capcom - Game Series Data  |",
+    secondHeader: "| First Appearance and Rank      |",
+    thirdHeader: "| Number of Titles               |",
+    fourthHeader: "| Units                          |",
+    fiscalYear: string,
+    fiscalYearYoY: string,
+    ltd: "| Life-To-Date       |",
+    summaryHeader: string,
+};
+
+export const printHead = (header: Header) =>
+`+${"-".repeat(32)}+
+${header.bandaiNamcoHeader}
++${"-".repeat(32)}+
+${header.secondHeader}
++${"-".repeat(32)}+
+${header.thirdHeader}
++${"-".repeat(32)}+
+${header.fourthHeader}
++${"-".repeat(32)}+`;
+
+const printRank = (seriesIP: Series) => {
+
+    let ranking: string = ` Rank ${seriesIP.rank} `;
+
+    return (blockLength: number) => {
+
+        return (ranking.length >= blockLength)
+            ? ranking
+            : ranking + " ".repeat(blockLength - ranking.length); 
+    }; 
+};
+
+const printTextBlock = (text: string) => {
+
+    return (blockLength: number) => {
+
+        let textSplit: string[] = text.split(" ");
+         
+        let arrayCheckText = 0; // a mutating variable for splicing textSplit below in textReduce
+
+        let printText: string | never[] = Array.from({length:Math.ceil((text.length + textSplit.length)/blockLength)}, (v,i) => {
+
+            let textSplice = textSplit.slice(arrayCheckText)
+
+            let nextCheckAlert = false;
+            
+            let textReduce = textSplice.reduce((prev, next) => 
+            {
+                if (nextCheckAlert) { // if this isn't here and next is small enough to pass the next if statement then words end up missing due to the arrayCheckText + increment
+                    return prev
+                }
+
+                let nextCheck = prev + " " + next + " ";
+                
+                if (nextCheck.length > blockLength) {
+                    nextCheckAlert = true;
+                    return prev // repeat prev until reduce finishes
+                } 
+                
+                arrayCheckText++ 
+
+                return prev + " " + next
+                
+            }, "")
+
+            let textFixed = (textReduce.length >= blockLength || textReduce.length === 0) // latter condition is to return an empty array
+                ? textReduce
+                : textReduce + " ".repeat(blockLength - textReduce.length)
+
+            return (textFixed.length === 0) 
+                ? []
+                : "|" + textFixed + "|"
+
+        }).filter(elem => elem.length !== 0).reduce((prev, next) => prev + "\n" + next)
+        
+        return printText
+    }
+};
+
+const printReleaseDate = (seriesIP: Series) => {
+
+    let releaseDate: string = seriesIP.releaseDate;
+
+    return (blockLength: number) => {
+
+        return (releaseDate.length >= blockLength)
+            ? releaseDate + "|" + printRank(seriesIP)(11)
+            : releaseDate + " ".repeat(blockLength - releaseDate.length - printRank(seriesIP)(11).length - 1) + "|" + printRank(seriesIP)(11); // not the best solution since I am having to hardcode printRank length...
+    };
+};
+
+const printCmlValue = (seriesIP: Series) => {
+
+        let CmlValue: string = `${(seriesIP.value - seriesIP.valueLastFY).toFixed(2)}M `
+
+        return (blockLength: number) => 
+               (header: Header) => {
+                    return (CmlValue.length >= blockLength)
+                        ? header.fiscalYear + CmlValue + "|"
+                        : header.fiscalYear + " ".repeat(blockLength - CmlValue.length) + CmlValue + "|";
+        };
+};
+
+const printCmlYoY = (seriesIP: Series) => {
+
+        let FYCmlYoY = (seriesIP.valueLastFY === 0)
+                ? " New! "
+                : ((seriesIP.value - seriesIP.valueLastFY) >=  (seriesIP.valueLastFY - seriesIP.valueLastTwoFYs) && !((seriesIP.valueLastFY - seriesIP.valueLastTwoFYs) < 0))
+                ? `+${((
+                    ((seriesIP.value - seriesIP.valueLastFY) / (seriesIP.valueLastFY - seriesIP.valueLastTwoFYs)) - 1) * 100).toFixed(2)}% ` 
+                : `${((
+                    ((seriesIP.value - seriesIP.valueLastFY) / (seriesIP.valueLastFY - seriesIP.valueLastTwoFYs)) - 1) * 100).toFixed(2)}% ` 
+
+        return (blockLength: number) =>
+               (header: Header) => {
+
+                return (FYCmlYoY.length >= blockLength) 
+                    ? header.fiscalYearYoY + FYCmlYoY + "|"
+                    : header.fiscalYearYoY + " ".repeat(blockLength - FYCmlYoY.length) + FYCmlYoY + "|"
+            };
+    };
+
+const printLTDValue = (seriesIP: Series) => {
+
+        let printLTDValue: string = `${seriesIP.value}M `
+       
+        return (blockLength: number) => 
+               (header: Header) => {
+
+            return (printLTDValue.length >= blockLength)
+            ? header.ltd + printLTDValue + "|"
+            : header.ltd + " ".repeat(blockLength - printLTDValue.length) + printLTDValue + "|";
+        };
+};
+
+const printSeriesName = (seriesIP: Series) => {
+
+    return (blockLength: number) => {
+       return (!seriesIP.miscellaneous) 
+            ? "+"+"-".repeat(blockLength)+"+\n" + printTextBlock(seriesIP.title)(blockLength) + "\n+" + "-".repeat(blockLength) + "+\n|" + printReleaseDate(seriesIP)(blockLength) + "|"
+            : "+"+"-".repeat(blockLength)+"+\n" + printTextBlock(seriesIP.title)(blockLength) + "\n+" + "-".repeat(blockLength) + "+\n|" + printReleaseDate(seriesIP)(blockLength) + "|\n+" + "-".repeat(blockLength) + "+\n" + printTextBlock(seriesIP.miscellaneous)(blockLength);
+    };
+};
+
+export const printSeriesOutput = (seriesIP: Series) => {
+
+    return (header: Header) =>
+           (blockLength: number) =>
+           (valueLength: number) =>
+           (shortLineLength: number) => {
+
+                return printSeriesName(seriesIP)(blockLength) + "\n" + printDoubleLine(blockLength) + "\n" + printCmlValue(seriesIP)(valueLength)(header) + "\n" + printCmlYoY(seriesIP)(valueLength)(header) + "\n" + printLTDValue(seriesIP)(valueLength)(header) + "\n" + printLine(shortLineLength) 
+    };
+};
+
+const printLine = (lineLength: number) => "+" + "-".repeat(lineLength) + "+"; 
+
+const printDoubleLine = (lineLength: number) => "+" + "=".repeat(lineLength) + "+"; 
+
