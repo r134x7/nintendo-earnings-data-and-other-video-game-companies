40e8d38f463d04973c67f237503de16c7a0276f7
r134x7 <r134x7@gmx.com>
2022-10-29 23:30:20 +1100

    setting up components

--- webpage_v2/src/components/capcom/CAPCOM_FY3_2022.tsx
+++ webpage_v2/src/components/capcom/CAPCOM_FY3_2022.tsx
@@ -2,8 +2,15 @@ import { useState, useEffect } from "react";
 import { Code, SegmentedControl, Anchor, Text, Stack, Space } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { printFYPlatinumTitles, printAllPlatinumTitles } from "../../data/capcom/Capcom_FY3_2022/platinum_titles_fy3_22";
+import { printSeriesFY } from "../../data/capcom/Capcom_FY3_2022/game_series_sales_fy3_2022"
 
 export default function CAPCOM_FY3_2022() {
+
+    const platinumTitlesFY = printFYPlatinumTitles;
+
+    const platinumTitlesAll = printAllPlatinumTitles;
+
+    const gameSeriesSales = printSeriesFY;
 
     const [data, setData] = useState("");
     const [value, setValue] = useState("");
@@ -11,12 +18,44 @@ export default function CAPCOM_FY3_2022() {
 
     const state: any = useSelector(state => state);
 
+    const dataList = [ 
+        "Data Sources",
+        "FY Platinum Titles",
+        "All Platinum Titles",
+        "FY Game Series",
+        ];
+
+    const componentList = [
+        {
+            name: "FY Platinum Titles", 
+            value: platinumTitlesFY
+        },
+        {
+            name: "All Platinum Titles", 
+            value: platinumTitlesAll
+        },
+        {
+            name: "FY Game Series", 
+            value: gameSeriesSales
+        },
+    ]
+
+    const selectDataComponent = (objList: {name: string, value: string}[]) =>
+    (dataUsed: string): string => {
+
+        let [dataSelected] = objList.filter(elem => dataUsed === elem.name)
+
+        return (dataSelected) ? dataSelected.value : ""
+    };
+
+    const selectData = selectDataComponent(componentList);
+
     useEffect(() => {
-        (value === "FY Platinum Titles")
-            ? setData(platinumTitlesFY)
-            : (value === "All Platinum Titles")
-            ? setData(platinumTitlesAll)
-            : setData("");
+        // (value === "FY Platinum Titles")
+        //     ? setData(platinumTitlesFY)
+        //     : (value === "All Platinum Titles")
+        //     ? setData(platinumTitlesAll)
+        //     : setData("");
 
         (value === "Data Sources")
             ? setSources(DATA_SOURCES)
@@ -33,14 +72,15 @@ export default function CAPCOM_FY3_2022() {
                     <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
                         https://www.capcom.co.jp/ir/english/business/million.html
                     </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
                 </Stack>  
             </Text> 
         )
     };
 
-    const platinumTitlesFY = printFYPlatinumTitles;
-
-    const platinumTitlesAll = printAllPlatinumTitles;
 
     return (
 
@@ -50,14 +90,14 @@ export default function CAPCOM_FY3_2022() {
                     orientation="vertical"
                     value={value}
                     onChange={setValue}
-                    data={[ "Data Sources",
-                            "FY Platinum Titles",
-                            "All Platinum Titles",
-                        ]}
+                    data={dataList}
             />
             
             {sources}
-            <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
+            <Code style={{backgroundColor: `${state.colour}`}} block>
+                {/* {data} */}
+                {selectData(value)}
+                </Code>
             <Space h="xl" />
             <Space h="xl" />
             <Space h="xl" />
