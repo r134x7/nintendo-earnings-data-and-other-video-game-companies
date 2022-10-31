9c708de2682d8d384217fff3bb859987722bb820
r134x7 <r134x7@gmx.com>
2022-10-31 19:02:18 +1100

    added fy3/23 data, put data to component

--- webpage_v2/src/components/capcom/CAPCOM_FY3_2023.tsx
+++ webpage_v2/src/components/capcom/CAPCOM_FY3_2023.tsx
@@ -2,21 +2,55 @@ import { useState, useEffect } from "react";
 import { Code, SegmentedControl, Anchor, Text, Stack, Space } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { printFYPlatinumTitles, printAllPlatinumTitles } from "../../data/capcom/Capcom_FY3_2023/platinum_titles_fy3_23";
+import { printSalesPerSoftwareUnit } from "../../data/capcom/Capcom_FY3_2023/software_sales_fy3_2022";
 
 export default function CAPCOM_FY3_2023() {
 
-    const [data, setData] = useState("");
+    const softwareSales = printSalesPerSoftwareUnit; 
+
+    const platinumTitlesFY = printFYPlatinumTitles;
+
+    const platinumTitlesAll = printAllPlatinumTitles;
+
+    // const gameSeriesSales = printSeriesFY;
+
     const [value, setValue] = useState("");
     const [sources, setSources] = useState(<></>)
 
     const state: any = useSelector(state => state);
 
+    const componentList = [
+        {
+            name: "Software Sales",
+            value: softwareSales,
+        },
+        {
+            name: "FY Platinum Titles", 
+            value: platinumTitlesFY
+        },
+        {
+            name: "All Platinum Titles", 
+            value: platinumTitlesAll
+        },
+        // {
+        //     name: "FY Game Series", 
+        //     value: gameSeriesSales
+        // },
+    ]
+
+    const dataList = ["Data Sources"].concat(componentList.map(elem => elem.name));
+
+    const selectDataComponent = (objList: {name: string, value: string}[]) =>
+    (dataUsed: string): string => {
+
+        let [dataSelected] = objList.filter(elem => dataUsed === elem.name)
+
+        return (dataSelected) ? dataSelected.value : ""
+    };
+
+    const selectData = selectDataComponent(componentList);
+
     useEffect(() => {
-        (value === "FY Platinum Titles")
-            ? setData(platinumFYTitles)
-            : (value === "All Platinum Titles")
-            ? setData(platinumTitlesAll)
-            : setData("");
 
         (value === "Data Sources")
             ? setSources(DATA_SOURCES)
@@ -29,6 +63,14 @@ export default function CAPCOM_FY3_2023() {
         return (
             <Text>
                 <Stack align="center">
+                        1st Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/1st/explanation_2022_1st_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/1st/explanation_2022_1st_01.pdf
+                    </Anchor>
+                        2nd Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/2nd/explanation_2022_2nd_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/2nd/explanation_2022_2nd_01.pdf
+                    </Anchor>
                         Capcom Platinum Titles Page: 
                     <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
                         https://www.capcom.co.jp/ir/english/business/million.html
@@ -38,10 +80,6 @@ export default function CAPCOM_FY3_2023() {
         )
     };
 
-    const platinumFYTitles = printFYPlatinumTitles;
-
-    const platinumTitlesAll = printAllPlatinumTitles;
-
     return (
 
         <div>  
@@ -50,14 +88,13 @@ export default function CAPCOM_FY3_2023() {
                     orientation="vertical"
                     value={value}
                     onChange={setValue}
-                    data={[ "Data Sources",
-                            "FY Platinum Titles",
-                            "All Platinum Titles",
-                        ]}
+                    data={dataList}
             />
             
             {sources}
-            <Code style={{backgroundColor: `${state.colour}`}} block>{data}</Code>
+            <Code style={{backgroundColor: `${state.colour}`}} block>
+                {selectData(value)}
+                </Code>
             <Space h="xl" />
             <Space h="xl" />
             <Space h="xl" />
---
+++ webpage_v2/src/data/capcom/Capcom_FY3_2023/software_sales_fy3_2022.tsx
@@ -0,0 +1,95 @@
+import { Header, Section, CapcomPrint } from "../../../utils/segment_data_logic";
+
+const sales: Section[] = [
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "currency",
+        value: 19.2 // billion yen
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "currency",
+        value: 34.9 // billion yen
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "currency",
+        value: 34.9 // billion yen
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "currency",
+        value: 34.9 // billion yen
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " Last FY Cumulative ",
+        cmlPeriod: "Cml. ",
+        units: "currency",
+        value: 83.3 // billion yen
+    },
+]
+
+const units: Section[] = [
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 1st Quarter ",
+        cmlPeriod: " 1st Quarter ",
+        units: "units",
+        value: 11700 // thousand
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 2nd Quarter ",
+        cmlPeriod: " First Half  ",
+        units: "units",
+        value: 21300,
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 3rd Quarter ",
+        cmlPeriod: " 1st 3 Qtrs  ",
+        units: "currency",
+        value: 21300,
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " 4th Quarter ",
+        cmlPeriod: "Cml. ",
+        units: "currency",
+        value: 21300,
+    },
+    {
+        name: " Digital Contents ",
+        region: " Group Total ",
+        period: " Last FY Cumulative ",
+        cmlPeriod: "Cml. ",
+        units: "currency",
+        value: 32600,
+    },
+]
+
+const header: Header = {
+    fiscalYear: " FY3/2023 ",
+    secondHeader: "| Segment Information |",
+    firstHeader: "| Capcom         |",
+}
+
+export const printSalesPerSoftwareUnit = CapcomPrint(sales, units, header, 2);
