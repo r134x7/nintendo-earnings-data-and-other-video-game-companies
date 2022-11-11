ee7c5d119464dc923590af1eedbba92f261aad00
r134x7 <r134x7@gmx.com>
2022-11-11 17:21:35 +1100

    adding data sources

---
+++ webpage_v2/src/data/bandaiNamco/Data_Sources/data_sources_full_list.tsx
@@ -0,0 +1,69 @@
+import { Anchor, Text, Stack, Card } from "@mantine/core";
+
+// newest data is first in. Latest FY, FY3/2023
+export const dataSourcesList = [
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Financial Hightlights: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20210805_Report.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20210805_Report.pdf
+                    </Anchor>
+                        2nd Quarter Financial Hightlights: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20211109_Report.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20211109_Report.pdf
+                    </Anchor>
+                        3rd Quarter Financial Hightlights: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20220208_Report.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/en_20220208_Report.pdf
+                    </Anchor>
+                        4th Quarter Financial Hightlights: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/20220511_en_Report1.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/financialstatements/pdf/20220511_en_Report1.pdf
+                    </Anchor>
+                        Bandai Namco Group Fact Book 2022: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/integrated/pdf/2022EN_fact.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/integrated/pdf/2022EN_fact.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Bandai Namco Group Fact Book 2021: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/integrated/pdf/2021EN_fact1.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/integrated/pdf/2021EN_fact1.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Bandai Namco Group Fact Book 2020: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/integrated/pdf/2020EN_fact.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/integrated/pdf/2020EN_fact.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Bandai Namco Group Fact Book 2019: 
+                    <Anchor mb="sm" href="https://www.bandainamco.co.jp/files/ir/integrated/pdf/en_2019_fact.pdf" target="_blank" >
+                        https://www.bandainamco.co.jp/files/ir/integrated/pdf/en_2019_fact.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+];
