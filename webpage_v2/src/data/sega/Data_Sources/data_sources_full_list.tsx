79e1a9745481d11b7231e9375b738b20d0aafdec
r134x7 <r134x7@gmx.com>
2022-11-11 23:53:06 +1100

    added data sources, adding json data, setting up component

---
+++ webpage_v2/src/data/sega/Data_Sources/data_sources_full_list.tsx
@@ -0,0 +1,157 @@
+import { Anchor, Text, Stack, Card } from "@mantine/core";
+
+// newest data is first in. Latest FY, FY3/2023
+export const dataSourcesList = [
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2023/20220804_appendix_e_final.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2023/20220804_appendix_e_final.pdf
+                    </Anchor>
+                        2nd Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2023/20221031_appendix_e_final.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2023/20221031_appendix_e_final.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20210806_appendix_e_final.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20210806_appendix_e_final.pdf
+                    </Anchor>
+                        2nd Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20211108_appendix_e_final.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20211108_appendix_e_final.pdf
+                    </Anchor>
+                        3rd Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20220210_appendix_e_final_.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20220210_appendix_e_final_.pdf
+                    </Anchor>
+                        4th Quarter Data Appendix: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20220513_appendix_en_final_.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/settlement/2022/20220513_appendix_en_final_.pdf
+                    </Anchor>
+                        Sega Sammy Integrated Report 2022: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2022/ir_2022_web_all_e.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2022/ir_2022_web_all_e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2021: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2021/ir_2021_print_en.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2021/ir_2021_print_en.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2020: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2020/al2020_e_all.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2020/al2020_e_all.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2019: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2019/al2019_all_e__.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2019/al2019_all_e__.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2018: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2018/ssh_ar18e_web_1025.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2018/ssh_ar18e_web_1025.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2017: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2017/2017ar_all_e_.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2017/2017ar_all_e_.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2016: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2016/all_ar2016_e.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2016/all_ar2016_e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2015: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2015/all_ar2015_e.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2015/all_ar2015_e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2014: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2014/all_ar2014_e.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2014/all_ar2014_e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Sega Sammy Integrated Report 2013: 
+                    <Anchor mb="sm" href="https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2013/all_2013ar_e.pdf" target="_blank" >
+                        https://www.segasammy.co.jp/english/ir/library/pdf/printing_annual/2013/all_2013ar_e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+];
