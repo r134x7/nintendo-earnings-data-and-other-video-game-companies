034200d2a5bca568a292549ac09045bc3554a8d3
r134x7 <r134x7@gmx.com>
2022-11-13 17:10:51 +1100

    added data sources

---
+++ webpage_v2/src/data/capcom/Data_Sources/data_sources_full_list.tsx
@@ -0,0 +1,245 @@
+import { Anchor, Text, Stack, Card } from "@mantine/core";
+
+// newest data is first in. Latest FY, FY3/2023
+export const dataSourcesList = [
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/1st/explanation_2022_1st_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/1st/explanation_2022_1st_01.pdf
+                    </Anchor>
+                        2nd Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/2nd/explanation_2022_2nd_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2022/2nd/explanation_2022_2nd_01.pdf
+                    </Anchor>
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/1st/explanation_2021_1st_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/1st/explanation_2021_1st_01.pdf
+                    </Anchor>
+                        2nd Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/2nd/explanation_2021_2nd_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/2nd/explanation_2021_2nd_01.pdf
+                    </Anchor>
+                        3rd Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/3rd/explanation_2021_3rd_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/3rd/explanation_2021_3rd_01.pdf
+                    </Anchor>
+                        4th Quarter Financial Results Presentation: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/full/explanation_2021_full_01.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/explanation/2021/full/explanation_2021_full_01.pdf
+                    </Anchor>
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                        Capcom Fact Book 2022:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/data/pdf/annual/2022/annual_2022_07.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/data/pdf/annual/2022/annual_2022_07.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                        Capcom Fact Book 2020:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/annual/2020/annual_2020_09.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/annual/2020/annual_2020_09.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                        Capcom Fact Book 2019:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/data/pdf/annual/2019/annual_2019_09.pdf" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/data/pdf/annual/2019/annual_2019_09.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        Capcom Platinum Titles Page: 
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/million.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/million.html
+                    </Anchor>
+                        Capcom Game Series Sales Page:
+                    <Anchor mb="sm" href="https://www.capcom.co.jp/ir/english/business/salesdata.html" target="_blank" >
+                        https://www.capcom.co.jp/ir/english/business/salesdata.html
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+];
