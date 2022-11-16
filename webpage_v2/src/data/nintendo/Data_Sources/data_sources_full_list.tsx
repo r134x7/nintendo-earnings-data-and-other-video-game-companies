0afc26441ed0b323b2b6a20d61a46c9dcafe24da
r134x7 <r134x7@gmx.com>
2022-11-16 17:49:21 +1100

    setting up data sources list

---
+++ webpage_v2/src/data/nintendo/Data_Sources/data_sources_full_list.tsx
@@ -0,0 +1,294 @@
+import { Anchor, Text, Stack, Card } from "@mantine/core";
+
+// newest data is first in. Latest FY, FY3/2023
+export const dataSourcesList = [
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220803e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220803e.pdf
+                    </Anchor>
+                        1st Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220803_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220803_2e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/221108e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/221108e.pdf 
+                    </Anchor>
+                        2nd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/221108_8e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/221108_8e.pdf 
+                    </Anchor>
+                
+                        {/* 3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="" target="_blank" >
+                        
+                    </Anchor>
+                        3rd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="" target="_blank" >
+                        
+                    </Anchor> */}
+                
+                        {/* 4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="" target="_blank" >
+                        
+                    </Anchor>
+                        4th Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="" target="_blank" >
+                        
+                    </Anchor> */}
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210805e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210805e.pdf
+                    </Anchor>
+                        1st Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210805_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210805_3e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/211104e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/211104e.pdf
+                    </Anchor>
+                        2nd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/211104_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/211104_4e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220203e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220203e.pdf
+                    </Anchor>
+                        3rd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220203_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220203_4e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220510e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220510e.pdf
+                    </Anchor>
+                        4th Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2022/220510_7e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2022/220510_7e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200806e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200806e.pdf
+                    </Anchor>
+                        1st Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200806_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200806_2e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/201105e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/201105e.pdf
+                    </Anchor>
+                        2nd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/201105_5e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/201105_5e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210201e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210201e.pdf
+                    </Anchor>
+                        3rd Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210201_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210201_4e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210506e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210506e.pdf
+                    </Anchor>
+                        4th Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2021/210506_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2021/210506_4e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190730e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190730e.pdf
+                    </Anchor>
+                        1st Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190730_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190730_2e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/191031e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/191031e.pdf
+                    </Anchor>
+                        2nd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/191031_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/191031_3e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200130e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200130e.pdf
+                    </Anchor>
+                        3rd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200130_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200130_3e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200507e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200507e.pdf
+                    </Anchor>
+                        4th Quarter Financial Results Explanatory Material:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2020/200507_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2020/200507_3e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180731e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180731e.pdf
+                    </Anchor>
+                        1st Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180731_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180731_2e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/181030e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/181030e.pdf
+                    </Anchor>
+                        2nd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/181030_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/181030_3e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190131e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190131e.pdf
+                    </Anchor>
+                        3rd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190131_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190131_2e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190425e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190425e.pdf
+                    </Anchor>
+                        4th Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2019/190425_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2019/190425_3e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),   
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170726e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170726e.pdf
+                    </Anchor>
+                        1st Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170726_2e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170726_2e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/171030e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/171030e.pdf
+                    </Anchor>
+                        2nd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/171030_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/171030_4e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180131e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180131e.pdf
+                    </Anchor>
+                        3rd Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180131_3e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180131_3e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180426e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180426e.pdf
+                    </Anchor>
+                        4th Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2018/180426_4e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2018/180426_4e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+(
+        <Card shadow="sm" p="sm" radius="md" withBorder  style={{margin: "1em"}}>
+            <Text size={"md"} style={{overflowWrap: "anywhere"}}>
+                <Stack align="center">
+                        1st Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2016/160727e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2016/160727e.pdf
+                    </Anchor>
+                
+                        2nd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2016/161026e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2016/161026e.pdf
+                    </Anchor>
+                
+                        3rd Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170131e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170131e.pdf
+                    </Anchor>
+                
+                        4th Quarter Earnings Release: 
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170427e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170427e.pdf
+                    </Anchor>
+                        4th Quarter Supplementary Information:
+                    <Anchor mb="sm" href="https://www.nintendo.co.jp/ir/pdf/2017/170427_6e.pdf" target="_blank" >
+                        https://www.nintendo.co.jp/ir/pdf/2017/170427_6e.pdf
+                    </Anchor>
+                </Stack>  
+            </Text> 
+        </Card>
+        ),
+];
