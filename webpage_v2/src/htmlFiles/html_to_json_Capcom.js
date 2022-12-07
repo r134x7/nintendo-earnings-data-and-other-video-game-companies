3db03ac191463257152af2487bd49fc865109389
r134x7 <r134x7@gmx.com>
2022-12-08 01:54:48 +1100

    function to make array is setup

--- webpage_v2/src/htmlFiles/html_to_json_Capcom.js
+++ webpage_v2/src/htmlFiles/html_to_json_Capcom.js
@@ -8,6 +8,73 @@ import fs from "fs";
 // run file using node command not npm
 // starting with FY3/2021
 
+let currentQuarter = 4;
+
+
+const readQuarter = (currentQuarterLocal) => {
+
+    return (currentQuarterLocal = 1)
+            ? fs.readFileSync("firstQuarter.html", "utf-8")
+            : (currentQuarterLocal = 2)
+            ? fs.readFileSync("secondQuarter.html", "utf-8") 
+            : (currentQuarterLocal = 3)
+            ? fs.readFileSync("thirdQuarter.html", "utf-8")
+            : fs.readFileSync("fourthQuarter.html", "utf-8");
+};
+
+// make if statement to call different functions
+const extractData = (readQuarterLocal) => {
+
+    return readQuarterLocal.match(/(?<=td>)(.+?)(?=<\/td>)/g); // regex taken from: https://www.octoparse.com/blog/using-regular-expression-to-match-html#regex3
+};
+
+const getJSON = (jsonLocal, currentQuarterLocal) => {
+
+    return (currentQuarterLocal > 1)
+        ? fs.readFileSync(jsonLocal, "utf-8")
+        : undefined
+}; 
+
+const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal) => {
+
+    return Array.from({length:(newQuarterLocal.length/4)}, (v,i) => {
+    // if current quarter = 1
+    let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.title === dataMatchSecondQuarter[(i*4)+1]) && (elem.releaseDate === dataMatchSecondQuarter[i*4])); // searching by title name and release date should only match one title
+
+        return (!searchTitle[0])
+            ? {
+                title: newQuarterLocal[(i*4)+1],
+                releaseDate: newQuarterLocal[i*4],
+                platforms: newQuarterLocal[(i*4)+2],
+                Q1CmlValue: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*4)+3]),
+                Q2CmlValue: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*4)+3]),
+                Q3CmlValue: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*4)+3]),
+                Q4CmlValue: Number(newQuarterLocal[(i*4)+3]),
+            } 
+            : {
+                title: newQuarterLocal[(i*4)+1],
+                releaseDate: newQuarterLocal[i*4],
+                platforms: newQuarterLocal[(i*4)+2],
+                Q1CmlValue: searchTitle[0].Q1CmlValue,
+                Q2CmlValue: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*4)+3]) : searchTitle[0].Q2CmlValue,
+                Q3CmlValue: (currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*4)+3]) : searchTitle[0].Q3CmlValue,
+                Q4CmlValue: Number(newQuarterLocal[(i*4)+3]),
+            };
+    });
+};
+
+let readNewestQuarter = readQuarter(currentQuarter);
+let extractNQ = extractData(readNewestQuarter);
+
+let getCurrentData = getJSON("platinum_titles_fy3_2021_test.json", currentQuarter);
+let extractCD = (!getCurrentData) ? undefined : extractData(getCurrentData);
+
+let makeNewArray = makeArray(extractNQ, extractCD, currentQuarter);
+
+
+
+
+
 // const dataReadFirstQuarter = fs.readFileSync("firstQuarter.html", "utf-8")
 
 // let dataMatch = dataReadFirstQuarter.match(/(?<=td>)(.+?)(?=<\/td>)/g); // regex taken from: https://www.octoparse.com/blog/using-regular-expression-to-match-html#regex3
