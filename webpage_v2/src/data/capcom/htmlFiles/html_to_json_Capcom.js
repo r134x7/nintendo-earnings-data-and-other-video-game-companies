99eea94d39140e08987b154355065a3e230e52f4
r134x7 <r134x7@gmx.com>
2022-12-08 00:41:26 +1100

    created one json file covering one quarter

--- webpage_v2/src/data/capcom/htmlFiles/html_to_json_Capcom.js
+++ webpage_v2/src/data/capcom/htmlFiles/html_to_json_Capcom.js
@@ -5,32 +5,33 @@
 
 import fs from "fs";
 
-
-// fs.readFile("fourthQuarter.html", "utf-8", (error, data) => error ? console.error(error) : testData = data.toString());
 // run file using node command not npm
 // starting with FY3/2021
 
-const testData = fs.readFileSync("fourthQuarter.html", "utf-8")
+const dataRead = fs.readFileSync("firstQuarter.html", "utf-8")
 
-let testData1 = testData.match(/(?<=td>)(.+?)(?=<\/td>)/g); // regex taken from: https://www.octoparse.com/blog/using-regular-expression-to-match-html#regex3
+let dataMatch = dataRead.match(/(?<=td>)(.+?)(?=<\/td>)/g); // regex taken from: https://www.octoparse.com/blog/using-regular-expression-to-match-html#regex3
 
-console.log(testData1);
-
-let testData2 = {
-    date: testData1[0],
-    title: testData1[1],
-    platforms: testData1[2],
-    value: testData1[3],
-};
-
-let testArray = Array.from({length:(testData1.length/4)}, (v,i) => {
-
+let testArray = Array.from({length:(dataMatch.length/4)}, (v,i) => {
+    // if current quarter = 1
     return {
-        title: testData1[(i*4)+1],
-        releaseDate: testData1[i*4],
-        platforms: testData1[(i*4)+2],
-        value: Number(testData1[(i*4)+3]),
+        title: dataMatch[(i*4)+1],
+        releaseDate: dataMatch[i*4],
+        platforms: dataMatch[(i*4)+2],
+        Q1CmlValue: Number(dataMatch[(i*4)+3]),
+        Q2CmlValue: Number(dataMatch[(i*4)+3]),
+        Q3CmlValue: Number(dataMatch[(i*4)+3]),
+        Q4CmlValue: Number(dataMatch[(i*4)+3]),
     }
-})
+});
 
-console.log(JSON.stringify(testArray));
+// need to read from the JSON file
+// const jsonRead = fs.readFileSync("platinum_titles_fy3_2021_test.json", "utf-8")
+
+// console.log(JSON.parse(jsonRead));
+
+// console.log(JSON.stringify(testArray));
+
+fs.writeFile('platinum_titles_fy3_2021_test.json', JSON.stringify(testArray), (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
