e2c3d2e40496ca0bef2047ac158bc5a57534f2ad
r134x7 <r134x7@gmx.com>
2022-12-25 02:10:45 +1100

    regex setup before testing

--- webpage_v2/src/txtFiles/globalDataTxtToJSON.js
+++ webpage_v2/src/txtFiles/globalDataTxtToJSON.js
@@ -1,6 +1,6 @@
 import { readFileSync, writeFile } from "fs";
 
-let currentQuarter = 4;
+let currentQuarter = 1;
 
 const readQuarter = (currentQuarterLocal) => {
 
@@ -20,3 +20,55 @@ const getTotals = (readQuarterLocal) => {
     return getMatch
 };
 
+const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal) => {
+    if (newQuarterLocal === null) {
+        return null
+    };
+
+    return Array.from({length:(newQuarterLocal.length/4)}, (v,i) => {
+
+        let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.name === newQuarterLocal[(i*4)].trim())); // searching by name
+
+        return (!searchTitle[0])
+            ? {
+                name: newQuarterLocal[(i*4)].trim(),
+                units: "units",
+                Q1CmlValue: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*4)+2]),
+                Q2CmlValue: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*4)+2]),
+                Q3CmlValue: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*4)+2]),
+                Q4CmlValue: Number(newQuarterLocal[(i*4)+2]),
+                cmlValueLastFY: Number(newQuarterLocal[(i*4)+3]) - Number(newQuarterLocal[(i*4)+2]),
+            } 
+            : {
+                name: newQuarterLocal[(i*4)].trim(),
+                units: "units",
+                Q1CmlValue: searchTitle[0].Q1CmlValue,
+                Q2CmlValue: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*4)+2]) : searchTitle[0].Q2CmlValue,
+                Q3CmlValue: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*4)+2]) : searchTitle[0].Q3CmlValue,
+                Q4CmlValue: Number(newQuarterLocal[(i*4)+2]),
+                cmlValueLastFY: Number(newQuarterLocal[(i*4)+3]) - Number(newQuarterLocal[(i*4)+2]),
+            };
+    });
+};
+
+const getJSON = (jsonLocal, currentQuarterLocal) => {
+
+    return (currentQuarterLocal > 1)
+        ? readFileSync(jsonLocal, "utf-8")
+        : undefined
+}; 
+
+let readNewestQuarter = readQuarter(currentQuarter);
+let extractNQ = getTotals(readNewestQuarter);
+console.log(extractNQ);
+
+let getCurrentData = getJSON("global_data_test.json", currentQuarter);
+let parseCurrentData = (!getCurrentData) ? undefined : JSON.parse(getCurrentData);
+
+let newArray = makeArray(extractNQ, parseCurrentData, currentQuarter);
+
+let newArrayStringify = JSON.stringify(newArray);
+
+writeFile("global_data_test.json", newArrayStringify, (err) =>
+  err ? console.error(err) : console.log('totals')
+);
