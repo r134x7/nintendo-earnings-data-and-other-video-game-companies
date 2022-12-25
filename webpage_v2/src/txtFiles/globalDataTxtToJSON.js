7047002de45e0ab3dd19bfe2fb6899e2d464708c
r134x7 <r134x7@gmx.com>
2022-12-25 16:35:17 +1100

    added fy3/2007 wii data

--- webpage_v2/src/txtFiles/globalDataTxtToJSON.js
+++ webpage_v2/src/txtFiles/globalDataTxtToJSON.js
@@ -5,12 +5,12 @@ let currentQuarter = 4;
 const readQuarter = (currentQuarterLocal) => {
 
     return (currentQuarterLocal === 1)
-            ? readFileSync("regional_data/wii_fy3_2008/firstQuarter.txt", "utf-8")
+            ? readFileSync("regional_data/wii_fy3_2007/firstQuarter.txt", "utf-8")
             : (currentQuarterLocal === 2)
-            ? readFileSync("regional_data/wii_fy3_2008/secondQuarter.txt", "utf-8") 
+            ? readFileSync("regional_data/wii_fy3_2007/secondQuarter.txt", "utf-8") 
             : (currentQuarterLocal === 3)
-            ? readFileSync("regional_data/wii_fy3_2008/thirdQuarter.txt", "utf-8")
-            : readFileSync("regional_data/wii_fy3_2008/fourthQuarter.txt", "utf-8");
+            ? readFileSync("regional_data/wii_fy3_2007/thirdQuarter.txt", "utf-8")
+            : readFileSync("regional_data/wii_fy3_2007/fourthQuarter.txt", "utf-8");
 };
 
 const getTotals = (readQuarterLocal) => {
