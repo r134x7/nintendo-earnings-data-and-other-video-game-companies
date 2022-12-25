92559ea0e1deb1ba81077d92ae9804317758615e
r134x7 <r134x7@gmx.com>
2022-12-25 16:20:34 +1100

    added fy3/2015 data

--- webpage_v2/src/txtFiles/globalDataTxtToJSON.js
+++ webpage_v2/src/txtFiles/globalDataTxtToJSON.js
@@ -5,12 +5,12 @@ let currentQuarter = 4;
 const readQuarter = (currentQuarterLocal) => {
 
     return (currentQuarterLocal === 1)
-            ? readFileSync("regional_data/wii_fy3_2016/firstQuarter.txt", "utf-8")
+            ? readFileSync("regional_data/wii_fy3_2015/firstQuarter.txt", "utf-8")
             : (currentQuarterLocal === 2)
-            ? readFileSync("regional_data/wii_fy3_2016/secondQuarter.txt", "utf-8") 
+            ? readFileSync("regional_data/wii_fy3_2015/secondQuarter.txt", "utf-8") 
             : (currentQuarterLocal === 3)
-            ? readFileSync("regional_data/wii_fy3_2016/thirdQuarter.txt", "utf-8")
-            : readFileSync("regional_data/wii_fy3_2016/fourthQuarter.txt", "utf-8");
+            ? readFileSync("regional_data/wii_fy3_2015/thirdQuarter.txt", "utf-8")
+            : readFileSync("regional_data/wii_fy3_2015/fourthQuarter.txt", "utf-8");
 };
 
 const getTotals = (readQuarterLocal) => {
