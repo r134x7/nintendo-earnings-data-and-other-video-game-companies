233c85feb761edf6e9944d2136ba75110b6b6ade
r134x7 <r134x7@gmx.com>
2022-12-28 16:13:35 +1100

    added n64 data from fy3/1997 to fy3/2004

--- webpage_v2/src/txtFiles/threeRegionsTxtToJSON.js
+++ webpage_v2/src/txtFiles/threeRegionsTxtToJSON.js
@@ -10,7 +10,7 @@ const readQuarter = (currentQuarterLocal) => {
             ? readFileSync("secondQuarter.txt", "utf-8") 
             : (currentQuarterLocal === 3)
             ? readFileSync("thirdQuarter.txt", "utf-8")
-            : readFileSync("regional_data/nintendo_gamecube_fy3_2002/fourthQuarter.txt", "utf-8");
+            : readFileSync("regional_data/nintendo_64_fy3_2004/fourthQuarter.txt", "utf-8");
 };
 
 const removeCommas = (readQuarterLocal) => {
