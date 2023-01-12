5f31c9f4f18c89502103393bfd6cd39e5ed2017a
r134x7 <r134x7@gmx.com>
2023-01-12 15:32:55 +1100

    earnings json maker is setup

--- webpage_v2/src/txtFiles/earningsJSON/earningsTxtToJSON.js
+++ webpage_v2/src/txtFiles/earningsJSON/earningsTxtToJSON.js
@@ -1,13 +1,6 @@
 import { readFileSync, writeFile } from "fs";
 
-let currentQuarter = 4;
-// let currentPlatform = "Nintendo Switch";
-// let currentPlatform = "Wii U";
-let currentPlatform = "Nintendo 3DS";
-// let currentPlatform = "Wii";
-// let currentPlatform = "Nintendo DS";
-// let currentPlatform = "Game Boy Advance";
-// let currentPlatform = "Nintendo GameCube";
+let currentQuarter = 1;
 
 const readQuarter = (currentQuarterLocal) => {
 
@@ -30,58 +23,34 @@ const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal, platf
         return null
     };
 
-    return Array.from({length:(newQuarterLocal.length/5)}, (v,i) => {
+    return Array.from({length:(newQuarterLocal.length/3)}, (v,i) => {
 
-        let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.name === newQuarterLocal[(i*5)])); // searching by name
+        let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.name.trim() === newQuarterLocal[(i*3)].trim())); // searching by name
 
         return (!searchTitle[0])
             ? {
-                name: newQuarterLocal[(i*5)],
-                platform: platformLocal,
-                regionA: "Japan",
-                Q1CmlValueA: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*5)+2]),
-                Q2CmlValueA: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*5)+2]),
-                Q3CmlValueA: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*5)+2]),
-                Q4CmlValueA: Number(newQuarterLocal[(i*5)+2]),
-                regionB: "Overseas",
-                Q1CmlValueB: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*5)+3]),
-                Q2CmlValueB: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*5)+3]),
-                Q3CmlValueB: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*5)+3]),
-                Q4CmlValueB: Number(newQuarterLocal[(i*5)+3]),
-                regionC: "WW FY",
-                Q1CmlValueC: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*5)+1]),
-                Q2CmlValueC: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*5)+1]),
-                Q3CmlValueC: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*5)+1]),
-                Q4CmlValueC: Number(newQuarterLocal[(i*5)+1]),
-                regionD: "WW LTD",
-                Q1CmlValueD: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*5)+4]),
-                Q2CmlValueD: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*5)+4]),
-                Q3CmlValueD: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*5)+4]),
-                Q4CmlValueD: Number(newQuarterLocal[(i*5)+4]),
+                name: newQuarterLocal[(i*3)].trim(),
+                Q1CmlValue: (currentQuarterLocal > 1) ? 0 : Number(newQuarterLocal[(i*3)+1]),
+                Q2CmlValue: (currentQuarterLocal > 2) ? 0 : Number(newQuarterLocal[(i*3)+1]),
+                Q3CmlValue: (currentQuarterLocal > 3) ? 0 : Number(newQuarterLocal[(i*3)+1]),
+                Q4CmlValue: Number(newQuarterLocal[(i*3)+1]),
+                forecastThisFY: (currentQuarterLocal > 1) ? null : Number(newQuarterLocal[(i*3)+2]),
+                forecastRevision1: null,
+                forecastRevision2: null,
+                forecastRevision3: null,
+                forecastNextFY: (currentQuarterLocal === 4) ? null : Number(newQuarterLocal[(i*3)+2]),
             } 
             : {
-                name: newQuarterLocal[(i*5)],
-                platform: platformLocal,
-                regionA: "Japan",
-                Q1CmlValueA: searchTitle[0].Q1CmlValueA,
-                Q2CmlValueA: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*5)+2]) : searchTitle[0].Q2CmlValueA,
-                Q3CmlValueA: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*5)+2]) : searchTitle[0].Q3CmlValueA,
-                Q4CmlValueA: Number(newQuarterLocal[(i*5)+2]),
-                regionB: "Overseas",
-                Q1CmlValueB: searchTitle[0].Q1CmlValueB,
-                Q2CmlValueB: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*5)+3]) : searchTitle[0].Q2CmlValueB,
-                Q3CmlValueB: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*5)+3]) : searchTitle[0].Q3CmlValueB,
-                Q4CmlValueB: Number(newQuarterLocal[(i*5)+3]),
-                regionC: "WW FY",
-                Q1CmlValueC: searchTitle[0].Q1CmlValueC,
-                Q2CmlValueC: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*5)+1]) : searchTitle[0].Q2CmlValueC,
-                Q3CmlValueC: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*5)+1]) : searchTitle[0].Q3CmlValueC,
-                Q4CmlValueC: Number(newQuarterLocal[(i*5)+1]),
-                regionD: "WW LTD",
-                Q1CmlValueD: searchTitle[0].Q1CmlValueD,
-                Q2CmlValueD: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*5)+4]) : searchTitle[0].Q2CmlValueD,
-                Q3CmlValueD: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*5)+4]) : searchTitle[0].Q3CmlValueD,
-                Q4CmlValueD: Number(newQuarterLocal[(i*5)+4]),
+                name: newQuarterLocal[(i*3)].trim(),
+                Q1CmlValue: searchTitle[0].Q1CmlValue,
+                Q2CmlValue: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*3)+1]) : searchTitle[0].Q2CmlValue,
+                Q3CmlValue: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*3)+1]) : searchTitle[0].Q3CmlValue,
+                Q4CmlValue: Number(newQuarterLocal[(i*3)+1]),
+                forecastThisFY: searchTitle[0].forecastThisFY,
+                forecastRevision1: (searchTitle[0].forecastThisFY === Number(newQuarterLocal[(i*3)+2])) ? null : Number(newQuarterLocal[(i*3)+2]),
+                forecastRevision2: (searchTitle[0].forecastRevision1 === Number(newQuarterLocal[(i*3)+2])) ? null : Number(newQuarterLocal[(i*3)+2]),
+                forecastRevision3: (searchTitle[0].forecastRevision2 === Number(newQuarterLocal[(i*3)+2])) ? null : Number(newQuarterLocal[(i*3)+2]),
+                forecastNextFY: (currentQuarterLocal === 4) ? null : Number(newQuarterLocal[(i*3)+2]),
             };
     });
 };
@@ -97,13 +66,13 @@ let readNewestQuarter = readQuarter(currentQuarter);
 let extractNQ = checkMatch(readNewestQuarter);
 console.log(extractNQ);
 
-let getCurrentData = getJSON("million_sellers_test.json", currentQuarter);
+let getCurrentData = getJSON("earnings_data_test.json", currentQuarter);
 let parseCurrentData = (!getCurrentData) ? undefined : JSON.parse(getCurrentData);
 
 let newArray = makeArray(extractNQ, parseCurrentData, currentQuarter, currentPlatform);
 
 let newArrayStringify = JSON.stringify(newArray);
 
-writeFile("million_sellers_test.json", newArrayStringify, (err) =>
+writeFile("earnings_data_test.json", newArrayStringify, (err) =>
   err ? console.error(err) : console.log('json? JSSSOOONNNNNN!')
 );
