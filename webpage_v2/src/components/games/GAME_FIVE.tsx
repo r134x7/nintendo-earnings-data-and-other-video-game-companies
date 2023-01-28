5fd6277c594ebda4919aa1587c5ae3d646a6e91d
r134x7 <r134x7@gmx.com>
2023-01-28 23:48:17 +1100

    short setup, having to think of functions

--- webpage_v2/src/components/games/GAME_FIVE.tsx
+++ webpage_v2/src/components/games/GAME_FIVE.tsx
@@ -2,12 +2,42 @@ import { Code, Space } from "@mantine/core";
 import { useInterval } from "@mantine/hooks";
 import { useState, useEffect } from "react";
 import { liner, spacer, printTextBlock } from "../../utils/table_design_logic";
+import gameFiveScript from "../../gameScript/gameFive/script.json";
 
 export default function GAME_FIVE() {
 
+    const [line, setLine] = useState(0);
+
+    const intro = gameFiveScript.intro?.[line] ?? "Nil";
+
+    const splitIntro = (intro + " ").split("");
+    // I need to create a function to handle the script...
+
+    const [text, setText] = useState("");
+
+    const [textBlock, setTextBlock] = useState("");
+
+    const [seconds, setSeconds] = useState(0);
+    const interval = useInterval(() => setSeconds((s) => s + 1), 80);
+
+    useEffect(() => {
+        if (seconds === splitIntro.length) {
+            interval.stop();
+        } else {
+            interval.start();
+
+            setText(text + splitIntro[seconds])
+
+            setTextBlock(liner(printTextBlock(text + " ".repeat(intro.length-text.length),40),"−","both",true,40))
+        }
+
+    }, [seconds])
+
+
     return (
         <div>
-            <Code style={{backgroundColor: `rgba(75, 0, 130, .20)`}} block>
+            <Code style={{backgroundColor: `rgba(0, 255, 255,0.2)`}} block>
+            {textBlock}
             </Code>
             <Space h="xl" />
         </div>
