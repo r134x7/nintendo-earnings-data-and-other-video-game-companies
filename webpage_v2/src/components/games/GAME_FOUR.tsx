0234a882e4108196f9361b775fcdc6476f5c0173
r134x7 <r134x7@gmx.com>
2023-01-28 16:54:58 +1100

    progress made so far on dialogue action

--- webpage_v2/src/components/games/GAME_FOUR.tsx
+++ webpage_v2/src/components/games/GAME_FOUR.tsx
@@ -5,24 +5,71 @@ import { Field } from "../../classes/Field";
 import { Unit } from "../../classes/Unit";
 import { liner, border, spacer, printTextBlock } from "../../utils/table_design_logic";
 
+import gameFourScript from "../../gameScript/gameFour/script.json"
+
 
 export default function GAME_FOUR() {
 
-const gameOverOne = 
-`--------------------------------
-| You struggled and lost.      |
-|                              |
-| Game Over                    |
---------------------------------`; 
+    const [lineOne, setLineOne] = useState(0);
+    const [lineTwo, setLineTwo] = useState(0);
 
+    const playerOne = gameFourScript?.playerOneBox[lineOne] ?? "No lines here P1";
+
+    const splitOne = (playerOne + " ").split("");
+
+    const [textOne, setTextOne] = useState("");
+
+    const [textBlockOne, setTextBlockOne] = useState("");
+
+    const playerTwo = gameFourScript?.playerTwoBox[lineTwo] ?? "No lines here P2";
+
+    const splitTwo = (playerTwo + " ").split("");
+
+    const [textTwo, setTextTwo] = useState("");
+
+    const [textBlockTwo, setTextBlockTwo] = useState("");
+
+    const [seconds, setSeconds] = useState(0);
+    const interval = useInterval(() => setSeconds((s) => s + 1), 80);
+
+    useEffect(() => {
+        if (seconds === splitOne.length && lineOne === lineTwo) {
+            interval.stop();
+            setSeconds(0);
+            setLineOne(lineOne+1)
+            setTimeout(() => {
+
+              }, 3000)
+        } else if (seconds === splitTwo.length && lineOne !== lineTwo) {
+            interval.stop();
+            setSeconds(0);
+            setLineTwo(lineTwo+1)
+            setTimeout(() => {
+
+              }, 3000)
+        } else if (lineOne === lineTwo) {
+            interval.start();
+
+            setTextOne(textOne + splitOne[seconds])
+
+            setTextBlockOne(liner(printTextBlock(textOne + " ".repeat(playerOne.length-textOne.length),40),"−","both",true,40))
+        } else {
+            interval.start();
+
+            setTextTwo(textTwo + splitTwo[seconds])
+
+            setTextBlockTwo(liner(printTextBlock(textTwo + " ".repeat(playerTwo.length-textTwo.length),40),"−","both",true,40))
+        }
+
+    }, [seconds])
 
     return (
         <div>
-            <Code block>
-            </Code>
+            <Code style={{backgroundColor: `rgba(0, 255, 255,0.2)`}} block>{textBlockOne}</Code>
             <Space h="xl" />
-            <Code block>
-            </Code>
+            <Code style={{backgroundColor: `rgba(75, 0, 130, .20)`}} block>{textBlockTwo}</Code>
+            <Space h="xl" />
+            Dialogue source: https://www.youtube.com/watch?v=-9NBvsmQ2Ik
         </div>
     )
 }
