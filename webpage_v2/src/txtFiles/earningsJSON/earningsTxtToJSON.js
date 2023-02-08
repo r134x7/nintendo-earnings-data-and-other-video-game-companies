132abd7f5cfaa9c4b4162c6bb355063cbb8d0689
r134x7 <r134x7@gmx.com>
2023-02-08 16:38:14 +1100

    fixed issue where forecast revisions were not appearing

--- webpage_v2/src/txtFiles/earningsJSON/earningsTxtToJSON.js
+++ webpage_v2/src/txtFiles/earningsJSON/earningsTxtToJSON.js
@@ -8,14 +8,13 @@ const readQuarter = (currentQuarterLocal) => {
     return (currentQuarterLocal === 1)
             ? readFileSync("firstQuarter.txt", "utf-8")
             : (currentQuarterLocal === 2)
-            ? readFileSync("secondQuarter.txt", "utf-8") 
-            : (currentQuarterLocal === 3)
-            ? readFileSync("thirdQuarter.txt", "utf-8")
-            : readFileSync("fourthQuarter.txt", "utf-8");
+                ? readFileSync("secondQuarter.txt", "utf-8") 
+                : (currentQuarterLocal === 3)
+                    ? readFileSync("thirdQuarter.txt", "utf-8")
+                    : readFileSync("fourthQuarter.txt", "utf-8");
 };
 
 const checkMatch = (readingQuarter) => {
-
     return readingQuarter.match(/.+/g)
 };
 
@@ -26,8 +25,10 @@ const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal) => {
 
     return Array.from({length:(newQuarterLocal.length/3)}, (v,i) => {
 
-        let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.name.trim() === newQuarterLocal[(i*3)].trim())); // searching by name
+        // through each iteration of Array.from, it will find Net Sales, then Operating Income and then Net Income hence the array will only have a length of 1.
+        let searchTitle = (!currentDataLocal) ? [undefined] : currentDataLocal.filter((elem,index,array) => (elem.name.trim() === newQuarterLocal[(i*3)].trim()));
 
+        console.log(searchTitle?.[0]?.forecastRevision1);
         let latestForecasts = {
             netSalesForecast: (Number.isNaN(Number(newQuarterLocal[2])) === true) ? null : Number(newQuarterLocal[2]),
             operatingIncomeForecast: (Number.isNaN(Number(newQuarterLocal[5])) === true) ? null : Number(newQuarterLocal[5]),
@@ -38,28 +39,28 @@ const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal) => {
             ? [undefined]
             : [
                 {
-                    value1: currentDataLocal[0].forecastThisFY,
-                    value2: currentDataLocal[1].forecastThisFY,
-                    value3: currentDataLocal[2].forecastThisFY,
+                    value1: currentDataLocal?.[0]?.forecastThisFY,
+                    value2: currentDataLocal?.[1]?.forecastThisFY,
+                    value3: currentDataLocal?.[2]?.forecastThisFY,
                 },
                 {
-                    value1: currentDataLocal[0].forecastRevision1,
-                    value2: currentDataLocal[1].forecastRevision1,
-                    value3: currentDataLocal[2].forecastRevision1,
+                    value1: currentDataLocal?.[0]?.forecastRevision1,
+                    value2: currentDataLocal?.[1]?.forecastRevision1,
+                    value3: currentDataLocal?.[2]?.forecastRevision1,
                 },
                 {
-                    value1: currentDataLocal[0].forecastRevision2,
-                    value2: currentDataLocal[1].forecastRevision2,
-                    value3: currentDataLocal[2].forecastRevision2,
+                    value1: currentDataLocal?.[0]?.forecastRevision2,
+                    value2: currentDataLocal?.[1]?.forecastRevision2,
+                    value3: currentDataLocal?.[2]?.forecastRevision2,
                 }
-            ].flatMap((elem) => (elem.value1 === null && elem.value2 === null && elem.value3 === null) ? [] : elem).flatMap((elem, index ,array) => {
-                // need to check values only at the end of the array, older forecast numbers do not need to be checked
+                // each object is holding values for a forecast type, need to filter out types where data is undefined.
+            ].filter((elem) => elem?.value1 && elem?.value2 && elem?.value3).flatMap((elem, index ,array) => {
+                // need to check values only at the end of the array, older forecast numbers do not need to be checked, hence the array should only be of length 1 when it gets to else if
                 if (index !== array.length-1) {
                     return []
                 } else if (elem.value1 === latestForecasts.netSalesForecast && elem.value2 === latestForecasts.operatingIncomeForecast && elem.value3 === latestForecasts.netIncomeForecast) {
-                    return []
+                    return false
                 } else {
-                    // the array length should only be 1 or 0 for this to work correctly.
                     return true
                 };
             });
@@ -83,39 +84,23 @@ const makeArray = (newQuarterLocal, currentDataLocal, currentQuarterLocal) => {
                 Q2CmlValue: (currentQuarterLocal === 2) ? Number(newQuarterLocal[(i*3)+1]) : searchTitle[0].Q2CmlValue,
                 Q3CmlValue: (currentQuarterLocal === 2 || currentQuarterLocal === 3) ? Number(newQuarterLocal[(i*3)+1]) : searchTitle[0].Q3CmlValue,
                 Q4CmlValue: Number(newQuarterLocal[(i*3)+1]),
-                forecastThisFY: (searchTitle[0].forecastThisFY !== null) 
-                        ? searchTitle[0].forecastThisFY
-                        : (currentQuarterLocal === 4)
-                            ? searchTitle[0].forecastThisFY
-                            : Number(newQuarterLocal[(i*3)+2]),
-                forecastRevision1: (searchTitle[0].forecastRevision1 !== null)
-                         ? searchTitle[0].forecastRevision1
-                         : (searchForecasts.at(-1) === true)
-                         ? Number(newQuarterLocal[(i*3)+2])
-                         : null,
-                        //  : (currentQuarter < 4 && searchTitle[0].forecastThisFY !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastThisFY !== null) 
-                        //      ? Number(newQuarterLocal[(i*3)+2])
-                        //      : null,
-                forecastRevision2: (searchTitle[0].forecastRevision2 !== null)
-                         ? searchTitle[0].forecastRevision2
-                         : (searchForecasts.at(-1) === true && searchTitle[0].forecastRevision1 !== null)
-                         ? Number(newQuarterLocal[(i*3)+2])
-                         : null,
-                        // (searchTitle[0].forecastRevision2 !== null)
-                        // ? searchTitle[0].forecastRevision2
-                        // : (currentQuarter < 4 && searchTitle[0].forecastThisFY !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastRevision1 !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastRevision1 !== null) 
-                        //     ? Number(newQuarterLocal[(i*3)+2]) 
-                        //     : null,
-                forecastRevision3: (searchTitle[0].forecastRevision3 !== null)
-                         ? searchTitle[0].forecastRevision3
-                         : (searchForecasts.at(-1) === true && searchTitle[0].forecastRevision2 !== null)
-                         ? Number(newQuarterLocal[(i*3)+2])
-                         : null,
-                        // (searchTitle[0].forecastRevision3 !== null)
-                        // ? searchTitle[0].forecastRevision3
-                        // : (currentQuarter < 4 && searchTitle[0].forecastThisFY !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastRevision1 !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastRevision1 !== null && searchTitle[0].forecastRevision2 !== Number(newQuarterLocal[(i*3)+2]) && searchTitle[0].forecastRevision2 !== null) 
-                        //     ? Number(newQuarterLocal[(i*3)+2])
-                        //     : null,
+                forecastThisFY: searchTitle?.[0]?.forecastThisFY ?? Number(newQuarterLocal[(i*3)+2]),
+                // cannot chain ?. ?? with ternary operator because ternary operator is not chainable and will override ?? operators.
+                forecastRevision1: (searchTitle?.[0]?.forecastRevision1) 
+                        ? searchTitle[0].forecastRevision1
+                        : (searchForecasts?.at(-1) === true)
+                            ? Number(newQuarterLocal[(i*3)+2])
+                            : null,
+                forecastRevision2: (searchTitle?.[0]?.forecastRevision2) 
+                        ? searchTitle[0].forecastRevision2
+                        : (searchForecasts?.at(-1) === true && searchTitle?.[0]?.forecastRevision1)
+                            ? Number(newQuarterLocal[(i*3)+2])
+                            : null,
+                forecastRevision3: (searchTitle?.[0]?.forecastRevision3) 
+                        ? searchTitle[0].forecastRevision3
+                        : (searchForecasts?.at(-1) === true && searchTitle?.[0]?.forecastRevision2)
+                            ? Number(newQuarterLocal[(i*3)+2])
+                            : null,
                 forecastNextFY: (currentQuarterLocal === 4) ? Number(newQuarterLocal[(i*3)+2]) : null,
             };
     });
