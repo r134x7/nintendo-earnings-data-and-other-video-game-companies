4f0054d90be83a0a584e1795e97785ebf1de76e8
r134x7 <r134x7@gmx.com>
2023-04-10 01:23:11 +1000

    applied lazy loading at route level. couldn't figure out how to get it to work using lazy from react-router... had to resort to using react.lazy and suspense from react

--- webpage_v2/src/main.tsx
+++ webpage_v2/src/main.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from "react-redux";
 import store from "./utils/store";
-
 import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+import { Loader } from '@mantine/core';
 
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
-import Nintendo from './pages/Nintendo';
-import Capcom from "./pages/Capcom";
-import Events from "./pages/Events";
-import Sega from "./pages/Sega";
-import BandaiNamco from './pages/BandaiNamco';
-import Games from './pages/Games';
-import KoeiTecmo from './pages/KoeiTecmo';
-import SquareEnix from './pages/SquareEnix';
+// import Nintendo from './pages/Nintendo';
+// import Capcom from "./pages/Capcom";
+// import Events from "./pages/Events";
+// import Sega from "./pages/Sega";
+// import BandaiNamco from './pages/BandaiNamco';
+// import Games from './pages/Games';
+// import KoeiTecmo from './pages/KoeiTecmo';
+// import SquareEnix from './pages/SquareEnix';
+
+const LazyNintendo = React.lazy(() => import("./pages/Nintendo"));
+const LazyCapcom = React.lazy(() => import("./pages/Capcom"));
+const LazySega = React.lazy(() => import("./pages/Sega"));
+const LazyBandaiNamco = React.lazy(() => import("./pages/BandaiNamco"));
+const LazyKoeiTecmo = React.lazy(() => import("./pages/KoeiTecmo"));
+const LazySquareEnix = React.lazy(() => import("./pages/SquareEnix"));
+const LazyEvents = React.lazy(() => import("./pages/Events"));
+const LazyGames = React.lazy(() => import("./pages/Games"));
+
 
 const router = createHashRouter([
   {
@@ -25,15 +36,24 @@ const router = createHashRouter([
     // routing elements, i.e. the pages
     children: [
       {path:"/", element:<Home />},
-      {path:"/nintendo", element:<Nintendo />},
-      {path:"/capcom", element:<Capcom />},
-      {path:"/sega", element:<Sega />},
-      {path:"/bandai-namco", element:<BandaiNamco />},
-      {path:"/koei-tecmo", element:<KoeiTecmo />},
-      {path:"/square-enix", element:<SquareEnix />},
-      {path:"/events", element:<Events />},
-      {path:"/games", element:<Games />},
+      // {path:"/nintendo", element:<Nintendo />},
+      {path:"/nintendo", element:<LazyNintendo />},
+      // {path:"/capcom", element:<Capcom />},
+      {path:"/capcom", element:<LazyCapcom />},
+      // {path:"/sega", element:<Sega />},
+      {path:"/sega", element:<LazySega />},
+      // {path:"/bandai-namco", element:<BandaiNamco />},
+      {path:"/bandai-namco", element:<LazyBandaiNamco />},
+      // {path:"/koei-tecmo", element:<KoeiTecmo />},
+      {path:"/koei-tecmo", element:<LazyKoeiTecmo />},
+      // {path:"/square-enix", element:<SquareEnix />},
+      {path:"/square-enix", element:<LazySquareEnix />},
+      // {path:"/events", element:<Events />},
+      {path:"/events", element:<LazyEvents />},
+      // {path:"/games", element:<Games />},
+      {path:"/games", element:<LazyGames />},
       {path:"*", element:<NoMatch />},
+
     ]
 
   }
@@ -42,7 +62,9 @@ const router = createHashRouter([
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <Suspense fallback={<Loader color='red' variant='bars' size="xl" />}>
+        <RouterProvider router={router} fallbackElement={<Loader />} />
+      </Suspense>
     </React.StrictMode>
   </Provider>
 )
