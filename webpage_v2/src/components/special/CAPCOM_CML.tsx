a7ef881422109dfcffb50ceb8263bae64f98b9aa
r134x7 <r134x7@gmx.com>
2023-04-19 03:43:26 +1000

    implemented search, forgot I could have used useEffect regarding amount of titles listed

--- webpage_v2/src/components/special/CAPCOM_CML.tsx
+++ webpage_v2/src/components/special/CAPCOM_CML.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Code, SegmentedControl, Space } from "@mantine/core";
+import { Code, SegmentedControl, Space, TextInput, Select } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { printSpecialList } from "../../data/capcom/platinum_titles_Capcom";
 import { CapcomSalesPerSoftwareUnitCml } from "../../data/generalTables/sales_per_software_unit_cml";
@@ -9,11 +9,37 @@ import { factBookCapcom } from "../../data/capcom/software_shipments_capcom_cml_
 
 import {cite, citeCopy} from "../../utils/copySetCitation";
 
+// mutating this variable is the only way I can get this to reliably work
+let titleListCheck = 0;
+
 export default function CAPCOM_CML() {
 
     const [value, setValue] = useState("");
 
+    const [titleValue, setTitleValue] = useState("");
+    const [platformValue, setPlatformValue] = useState<string | null>("All" ?? "All");
+
     const state: any = useSelector(state => state);
+
+    let filteredPlatforms = printSpecialList.titleData.filter(elem => (platformValue === "" || platformValue === "All") ? elem : elem.platforms.includes((platformValue === "NES") 
+        ? "FC"
+        : platformValue === "SNES"
+            ? "SFC" 
+            : platformValue ?? "All"));
+
+    let filterTitles = filteredPlatforms.filter(elem => (titleValue === "") ? elem : elem.title.toLowerCase().includes(titleValue))
+
+    // forgot that I could have applied a useEffect but this method works fine
+    titleListCheck = filterTitles.length;
+
+    // this creates recursion because you are re-rendering here at this point at all times
+    // setTitlesLength(filterTitles.length);
+    
+    let titlesReduce = filterTitles.reduce((acc, next) => acc + next.table, "");
+
+    let completeSpecialList = printSpecialList.header + printSpecialList.date + titlesReduce + printSpecialList.platformsNote;
+
+    // const [titlesLength, setTitlesLength] = useState(filterTitles.length); 
 
     const componentList = [
         {
@@ -22,7 +48,8 @@ export default function CAPCOM_CML() {
         },
         {
             name: "Capcom Platinum Titles - Cumulative",
-            value: printSpecialList 
+            // value: printSpecialList 
+            value: completeSpecialList
         },
         {
             name: "Capcom Sales Per Software Unit - Cumulative",
@@ -62,6 +89,49 @@ export default function CAPCOM_CML() {
             />
             
             <Code onCopy={e => citeCopy(e, cite)} style={{backgroundColor:`${state.colour}`, color:(state.fontColor === "dark") ? "#fff" : "#000000"}} block>
+                {(value === "Capcom Platinum Titles - Cumulative")
+                    ? <Select
+                        data={[
+                         "All",
+                         "NES",
+                         "SNES",
+                         "GB",
+                         "GBA",
+                         "3DS",
+                         "GC",
+                         "MD",
+                         "DC",
+                         "PS",
+                         "PS2",
+                         "PS3",
+                         "PS4",
+                         "PS5",
+                         "PSP",
+                         "DL",
+                         "NSW",
+                         "XSX",
+                     ]}
+                    defaultValue={"All"} 
+                    label="Select all or one platform:"
+                    radius="xl"
+                    value={platformValue}
+                    onChange={setPlatformValue}
+                  /> 
+                    : undefined
+                }
+                {(value === "Capcom Platinum Titles - Cumulative")
+                    ? <TextInput
+                    placeholder="Search specific titles (use lower case)"
+                    label={`Title Search - Number of Titles shown: ${titleListCheck}`}
+                    description="Clear field to show all titles of the selected platform"
+                    radius="xl"
+                    value={titleValue}
+                    onChange={e => {
+                        setTitleValue(e.target.value)
+                    }}
+                    />  
+                    : undefined
+                }
                 {selectData(value)}
                 </Code>
             <Space h="xl" />
