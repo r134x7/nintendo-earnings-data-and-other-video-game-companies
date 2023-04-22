87cebb212f23f275ca13b9eaafabdcf2330ed995
r134x7 <r134x7@gmx.com>
2023-04-23 03:15:10 +1000

    updated status, can't fix unicode issue with regex at this time

--- webpage_v2/src/pages/Home.tsx
+++ webpage_v2/src/pages/Home.tsx
@@ -7,17 +7,17 @@ const Home = () => {
 
     const state: any = useSelector(state => state);
 
-    const nintendoStatus = liner(printTextBlock("Nintendo page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: 3rd Quarter Earnings release data for FY3/2023 added.",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
+    const nintendoStatus = liner(printTextBlock("Nintendo page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Region and/or Platform and/or title search feature and predictive single word search applied to Special page: Nintendo Consolidated Sales Information - Cumulative, Nintendo Hardware/Software - Cumulative, Nintendo FY Million-Seller Titles - Cumulative and Nintendo Top Selling Titles - Cumulative.",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
 
-    const capcomStatus = liner(printTextBlock("Capcom page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Platform and/or title search feature applied to Software Platform Shipments, FY Platinum Titles, All Platinum Titles, FY Game Series, Capcom Platinum Titles - Cumulative, Capcom Software Platform Shipments - Cumulative, Capcom FY Game Series - Cumulative.",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
+    const capcomStatus = liner(printTextBlock("Capcom page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Platform and/or title search feature and predictive single word search applied to Software Platform Shipments, FY Platinum Titles, All Platinum Titles, FY Game Series, Capcom Platinum Titles - Cumulative, Capcom Software Platform Shipments - Cumulative, Capcom FY Game Series - Cumulative.",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
 
-    const segaStatus = liner(printTextBlock("Sega page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: IP Type and/or title search feature applied to Software Sales, Software Units, Sega Sammy Software Units - Cumulative, Sega Sammy FY Series IP - Cumulative",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
+    const segaStatus = liner(printTextBlock("Sega page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: IP Type and/or title search feature and predictive single word search applied to Software Sales, Software Units, Sega Sammy Software Units - Cumulative, Sega Sammy FY Series IP - Cumulative",40),"−","both",true,40) + liner(printTextBlock("Future update planned: Notes section.",40),"−","bottom",true,40);
 
-    const bandaiNamcoStatus = liner(printTextBlock("Bandai Namco page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Title search feature applied to FY Series IP, Bandai Namco FY Series IP - Cumulative.",40),"−","both",true,40);
+    const bandaiNamcoStatus = liner(printTextBlock("Bandai Namco page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Title search feature and predictive single word search applied to FY Series IP, Bandai Namco FY Series IP - Cumulative.",40),"−","both",true,40);
 
     const koeiTecmoStatus = liner(printTextBlock("Koei Tecmo page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: 3rd Quarter Earnings release data for FY3/2023 added.",40),"−","both",true,40);
 
-    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Title search feature applied to FY Series IP, Square Enix FY Series IP - Cumulative.",40),"−","both",true,40);
+    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Title search feature applied and predictive single word search to FY Series IP, Square Enix FY Series IP - Cumulative.",40),"−","both",true,40);
 
     const eventsStatus = liner(printTextBlock("Events page status",32),"=","top",true,32) + liner(printTextBlock("Page update: Updated calendar for 16 of 22 companies from April to May 2023.",40),"−","both",true,40);
 
