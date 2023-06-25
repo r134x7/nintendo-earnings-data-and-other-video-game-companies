12267abaffc9cc33ceeadf57c850cfd488c7ee64
r134x7 <r134x7@gmx.com>
2023-06-25 14:59:00 +1000

    added filter for consolidated earnings

--- webpage_v2/src/components/special/KOEI_TECMO_CML.tsx
+++ webpage_v2/src/components/special/KOEI_TECMO_CML.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Code, SegmentedControl, Space } from "@mantine/core";
+import { useState, useEffect } from "react";
+import { Code, SegmentedControl, Space, Select } from "@mantine/core";
 import { useSelector } from "react-redux";
 import { koeiTecmoSalesPerSoftwareUnitCml } from "../../data/generalTables/sales_per_software_unit_cml";
 import { cumulativeEarningsListKoeiTecmo } from "../../data/generalTables/consolidated_earnings_cml_data";
@@ -10,12 +10,44 @@ export default function KOEI_TECMO_CML() {
 
     const [value, setValue] = useState("");
 
+    const [timePeriod, setTimePeriod] = useState(6);
+    const [timePeriodValue, setTimePeriodValue] = useState<string | null>("FY Cumulative" ?? "FY Cumulative");
+
     const state: any = useSelector(state => state);
+
+    useEffect(() => {
+
+        switch (value) {
+            case "Koei Tecmo Consolidated Operating Results - Cumulative":
+                if (timePeriodValue === "1st Quarter") {
+                    setTimePeriod(0)
+                } else if (timePeriodValue === "2nd Quarter") {
+                    setTimePeriod(1)
+                } else if (timePeriodValue === "3rd Quarter") {
+                    setTimePeriod(2)
+                } else if (timePeriodValue === "4th Quarter") {
+                    setTimePeriod(3)
+                } else if (timePeriodValue === "First Half") {
+                    setTimePeriod(4)
+                } else if (timePeriodValue === "First Three Quarters") {
+                    setTimePeriod(5)
+                } else if (timePeriodValue === "FY Cumulative") {
+                    setTimePeriod(6)
+                } else {
+                    setTimePeriod(6)
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }, [value, timePeriodValue])
 
     const componentList = [
         {
             name: "Koei Tecmo Consolidated Operating Results - Cumulative",
-            value: cumulativeEarningsListKoeiTecmo
+            value: cumulativeEarningsListKoeiTecmo[timePeriod]
         },
         {
             name: "Koei Tecmo Sales Per Software Unit - Cumulative",
@@ -47,6 +79,25 @@ export default function KOEI_TECMO_CML() {
             />
             
             <Code onCopy={e => citeCopy(e, cite)} style={{backgroundColor:`${state.colour}`, color:(state.fontColor === "dark") ? "#fff" : "#000000"}} block>
+                {(value === "Koei Tecmo Consolidated Operating Results - Cumulative")
+                    ? <Select
+                        data={[
+                         "1st Quarter",
+                         "2nd Quarter",
+                         "3rd Quarter",
+                         "4th Quarter",
+                         "First Half",
+                         "First Three Quarters",
+                         "FY Cumulative",
+                     ]}
+                    defaultValue={"FY Cumulative"} 
+                    label="Select a time period:"
+                    radius="xl"
+                    value={timePeriodValue}
+                    onChange={setTimePeriodValue}
+                  /> 
+                    : undefined
+                }
                 {selectData(value)}
                 </Code>
             <Space h="xl" />
