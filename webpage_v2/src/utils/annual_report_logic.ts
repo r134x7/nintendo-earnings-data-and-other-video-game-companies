077598523f337a76b801853f9b42bfcd8ae65208
r134x7 <r134x7@gmx.com>
2023-07-03 10:36:48 +1000

    added print rank, release date and cumulative value functions

--- webpage_v2/src/utils/annual_report_logic.ts
+++ webpage_v2/src/utils/annual_report_logic.ts
@@ -1,36 +1,76 @@
-import { printTextBlock, border, liner, spacer} from "./table_design_logic";
+import { printTextBlock, border, liner, spacer, borderV2} from "./table_design_logic";
+import { printValuePrimitive, numberType } from "./general_earnings_logic";
 
 export type AnnualReportValue =
     | { 
         kind: "Annual Report", 
         units: "units" | "currency" | "percentage",
+        fiscalYear: string,
         value: number
       } 
     | { kind: "Nothing" }
 
-export type AnnualReportMake = 
+export type AnnualReportTitle = 
     | { kind: "General",
         title: string,
         releaseDate: string,
         fyEndMonth: string,
-        value: AnnualReportValue,
+        valueLTD: AnnualReportValue,
+        valueThisFY: AnnualReportValue,
         valueLastFY: AnnualReportValue,
         valueLastTwoFYs: AnnualReportValue,
         rank?: number,
         footnotes?: string,
-        fiscalYear?: string,
       }
     | { kind: "Sega",
         title: string,
         releaseDate: string,
         platforms: string,
         totalEditions: number,
-        value: AnnualReportValue,
+        valueLTD: AnnualReportValue,
+        valueThisFY: AnnualReportValue,
         valueLastFY: AnnualReportValue,
         valueLastTwoFYs: AnnualReportValue,
         ipType: "Acquired IP" | "Developed in-house IP" | "Licensed third party IP" | "Undefined",
         rank?: number,
-        miscellaneous1?: string,
-        miscellaneous2?: string,
-        fiscalYear?: string,
+        footnotes?: string,
       }
+
+export function printReleaseDate(series: AnnualReportTitle, textLength: number) {
+
+    const releaseDate = (series.kind === "General")
+        ? series.releaseDate + " to " + series.fyEndMonth
+        : series.kind === "Sega"
+            ? "First Release Year: " + series.releaseDate
+            : ""
+    
+    return borderV2([
+        spacer(releaseDate, textLength, "left")
+    ],"both","noNewLine")
+}
+
+export function printRank(series: AnnualReportTitle, textLength: number) {
+
+    const ranking = `Rank ${series.rank}`;
+
+    return borderV2([
+        spacer(ranking, textLength, "right")
+    ],"right","newLine")
+}
+
+export function printCumulativeValue(series: AnnualReportTitle, textLength: number): string {
+
+    if (series.valueThisFY.kind === "Annual Report") {
+
+        const getFY = series.valueThisFY.fiscalYear + " Cumulative";
+
+        const getFixed: number = Number(series.valueThisFY.value.toFixed(2))
+
+        return border([
+            spacer(getFY, getFY.length+1, "left"),
+            spacer(printValuePrimitive(getFixed,numberType("Million"),"None"), textLength, "right")
+        ]);
+    } else {
+        return "ERROR";
+    }
+}
