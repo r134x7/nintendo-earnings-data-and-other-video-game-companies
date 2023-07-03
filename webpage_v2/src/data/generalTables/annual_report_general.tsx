647dab76f29dba4de934fdb5ba7ec3f736429127
r134x7 <r134x7@gmx.com>
2023-07-03 16:21:25 +1000

    figured out how to deal with different data types not assigned a kind while still using D-Unions

--- webpage_v2/src/data/generalTables/annual_report_general.tsx
+++ webpage_v2/src/data/generalTables/annual_report_general.tsx
@@ -1,4 +1,4 @@
-import { type AnnualReportTitle, type AnnualReportValue } from "../../utils/annual_report_logic";
+import { thisFYCalculation, type AnnualReportTitle, type AnnualReportValue } from "../../utils/annual_report_logic";
 
 import bandaiNamcoAnnualReport2022 from "../bandaiNamco/Annual_Report/annual_report_fy3_2022.json";
 import bandaiNamcoAnnualReport2021 from "../bandaiNamco/Annual_Report/annual_report_fy3_2021.json";
@@ -11,14 +11,53 @@ export type SeriesJSON = {
     series: SeriesMake[],
 }
 
-export type SeriesMake = {
-   title: string,
-   releaseDate: string,
-   fyEndMonth: string,
-   value: number,
-   valueLastFY: number | string | null,
-   valueLastTwoFYs: number | string | null,
-   footnotes?: string,
+export type SeriesMake = 
+    | {
+        kind?: "General",
+        title: string,
+        releaseDate: string, //
+        fyEndMonth: string, //
+        value: number,
+        valueLastFY: number | string | null,
+        valueLastTwoFYs: number | string | null,
+        footnotes?: string,
+    }
+    | {
+        kind?: "Sega",
+        title: string,
+        firstReleaseYear: string, //
+        platforms: string, //
+        totalEditions: number, //
+        ipType: string, //
+        units: string, //
+        value: number,
+        valueLastFY: number | string | null,
+        valueLastTwoFYs: number | string | null,
+        footnotes?: string,
+    }
+
+
+export type SeriesMakeGeneral = {
+        title: string,
+        releaseDate: string,
+        fyEndMonth: string,
+        value: number,
+        valueLastFY: number | string | null,
+        valueLastTwoFYs: number | string | null,
+        footnotes?: string,
+    }
+
+export type SeriesMakeSega = {
+    title: string,
+    firstReleaseYear: number,
+    platforms: string,
+    totalEditions: number,
+    ipType: string,
+    units: string,
+    value: number,
+    valueLastFY: number,
+    valueLastTwoFYs: number,
+    footnotes?: string,
 }
 
 const collectionBandaiNamco = new Map<number, SeriesJSON>();
@@ -49,17 +88,83 @@ export function annualReportNothingCheck(
 
 }
 
+export function getIPType(value: string): "Acquired IP" | "Developed in-house IP" | "Licensed third party IP" | "Undefined" {
+
+    switch (value) {
+        case "Acquired IP":
+            return "Acquired IP" 
+    
+        case "Developed in-house IP":
+            return "Developed in-house IP"
+        
+        case "Licensed third party IP":
+            return "Licensed third party IP" 
+    
+        default:
+            return "Undefined"
+    }
+}
+
 export function annualReportValuesMake(obj: undefined | SeriesMake, fiscalYear: string, kind: "General" | "Sega"): AnnualReportTitle {
 
     if (kind === "General") {
 
+        const getObject = (!obj)
+            ? undefined
+            : {
+                ...obj,
+                kind: "General",
+                fyEndMonth: obj.kind === "General" ? obj.fyEndMonth : "ERROR",
+                releaseDate: obj.kind === "General" ? obj.releaseDate : "ERROR",
+            } satisfies SeriesMake
+
         const values: AnnualReportTitle = {
             kind: "General",
             title: obj?.title ?? "ERROR",
-            releaseDate: obj?.releaseDate ?? "ERROR",
-            fyEndMonth: obj?.fyEndMonth ?? "ERROR",
+            releaseDate: getObject?.releaseDate ?? "ERROR",
+            fyEndMonth: getObject?.fyEndMonth ?? "ERROR",
             footnotes: obj?.footnotes,
-            valueLTD
+            valueLTD: annualReportNothingCheck(obj?.value, "units", fiscalYear),
+            valueLastFY: annualReportNothingCheck(obj?.valueLastFY, "units", fiscalYear),
+            valueLastTwoFYs: annualReportNothingCheck(obj?.valueLastTwoFYs, "units", fiscalYear),
+            valueThisFY: thisFYCalculation(
+                annualReportNothingCheck(obj?.value, "units", fiscalYear),
+                annualReportNothingCheck(obj?.valueLastFY, "units", fiscalYear),
+            )
         }
+
+        return values
+    } else {
+
+        const getObject = (!obj)
+            ? undefined
+            : {
+                ...obj,
+                kind: "Sega",
+                firstReleaseYear: obj.kind === "Sega" ? obj.firstReleaseYear : "ERROR",
+                ipType: obj.kind === "Sega" ? obj.ipType : "ERROR",
+                platforms: obj.kind === "Sega" ? obj.platforms : "ERROR",
+                totalEditions: obj.kind === "Sega" ? obj.totalEditions : 0,
+                units: obj.kind === "Sega" ? obj.units : "ERROR",
+            } satisfies SeriesMake
+
+        const values: AnnualReportTitle = {
+            kind: "Sega",
+            title: obj?.title ?? "ERROR",
+            ipType: getIPType(getObject?.ipType ?? "ERROR"),
+            platforms: getObject?.platforms ?? "ERROR",
+            releaseDate: getObject?.firstReleaseYear ?? "ERROR",
+            totalEditions: getObject?.totalEditions ?? 0,
+            footnotes: obj?.footnotes,
+            valueLTD: annualReportNothingCheck(obj?.value, "units", fiscalYear),
+            valueLastFY: annualReportNothingCheck(obj?.valueLastFY, "units", fiscalYear),
+            valueLastTwoFYs: annualReportNothingCheck(obj?.valueLastTwoFYs, "units", fiscalYear),
+            valueThisFY: thisFYCalculation(
+                annualReportNothingCheck(obj?.value, "units", fiscalYear),
+                annualReportNothingCheck(obj?.valueLastFY, "units", fiscalYear),
+            )
+        }
+
+        return values
     }
 }
