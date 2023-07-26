7d89b846aacc00d771be98fbdb1fc889968f1654
r134x7 <r134x7@gmx.com>
2023-07-27 02:16:06 +1000

    created optimised solution for importJSON function

--- webpage_v2/src/data/capcom/index.ts
+++ webpage_v2/src/data/capcom/index.ts
@@ -1,27 +1,19 @@
 import type { EarningsJSONV2 } from "../generalTables/consolidated_earnings_general";
 
-export const softwareSalesCollectionCapcom = await importJSON(2024, 2021).then(values => values);
+export const softwareSalesCollectionCapcom = await importJSON(new Map<number, EarningsJSONV2>(), 2024, 2021);
 
-async function importJSON(currentFiscalYear: number, firstFiscalYear: number): Promise<Map<any, any>> {
+async function importJSON<T>(toMap: Map<number, T> , currentFiscalYear: number, firstFiscalYear: number) {
 
     const yearRange = currentFiscalYear - firstFiscalYear;
 
-    const toMap = new Map();
-
-    await Promise.all<EarningsJSONV2>(
+    // dynamic imports create a module object where import data needed is kept in the default key!
+    await Promise.all<{ default: T }>(
             Array.from({length: yearRange + 1}, (value, index) => {
                 return import(`./Software_Sales/software_sales_fy3_${currentFiscalYear - index}.json`)
             })
         ).then((values) => {
-            values.forEach((elem) => {
-                return toMap.set(toMap.size, {
-                    companyName: elem.companyName,
-                    currentQuarter: elem.currentQuarter,
-                    data: elem.data,
-                    fiscalYear: elem.fiscalYear
-                } satisfies EarningsJSONV2)
-            })
-        } )
+            values.forEach((elem) => toMap.set(toMap.size, elem.default))
+    })
 
     return toMap
 };
