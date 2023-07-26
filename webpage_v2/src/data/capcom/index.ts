ef8ec7d8e390f1eec7fb63ddea65d88f2ffda073
r134x7 <r134x7@gmx.com>
2023-07-27 02:49:41 +1000

    vite esbuild cannot build because of top-level await even when target is set to es2022

--- webpage_v2/src/data/capcom/index.ts
+++ webpage_v2/src/data/capcom/index.ts
@@ -1,6 +1,8 @@
 import type { EarningsJSONV2 } from "../generalTables/consolidated_earnings_general";
 
-export const softwareSalesCollectionCapcom = await importJSON(new Map<number, EarningsJSONV2>(), 2024, 2021);
+export const softwareSalesCollectionCapcom = (async () => {
+    return await importJSON(new Map<number, EarningsJSONV2>(), 2024, 2021);
+})(); 
 
 async function importJSON<T>(toMap: Map<number, T> , currentFiscalYear: number, firstFiscalYear: number) {
 
@@ -9,7 +11,11 @@ async function importJSON<T>(toMap: Map<number, T> , currentFiscalYear: number, 
     // dynamic imports create a module object where import data needed is kept in the default key!
     await Promise.all<{ default: T }>(
             Array.from({length: yearRange + 1}, (value, index) => {
-                return import(`./Software_Sales/software_sales_fy3_${currentFiscalYear - index}.json`)
+
+                // can't use the minus "-" operator inside the import statement
+                let getYear = currentFiscalYear - index;
+
+                return import(`./Software_Sales/software_sales_fy3_${getYear}.json`)
             })
         ).then((values) => {
             values.forEach((elem) => toMap.set(toMap.size, elem.default))
