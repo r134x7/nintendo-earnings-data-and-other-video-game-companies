068ea8ec2ade4581570f2db0c8cf985b4bb994b4
r134x7 <r134x7@gmx.com>
2023-08-07 03:36:15 +1000

    partially completed titlesMake data structure process

--- webpage_v2/src/data/nintendo/Nintendo_Cumulative_Data/game_series_sales_cml.tsx
+++ webpage_v2/src/data/nintendo/Nintendo_Cumulative_Data/game_series_sales_cml.tsx
@@ -3,7 +3,8 @@ import { printTextBlock, border, liner, spacer, headerPrint, dateLabel, globImpo
 
 import { Header, Titles, decimateCalculation } from "../../../utils/fy_million_seller_titles_logic"
 import { searchTitles } from "../../capcom/platinum_titles_Capcom";
-import { EarningsValue } from "../../../utils/general_earnings_logic";
+import { EarningsValue, quarterlyCalculationV2 } from "../../../utils/general_earnings_logic";
+import { nothingCheck } from "../../generalTables/consolidated_earnings_general";
 
 const totalCollection: Map<number, collectionJSON> = globImport(new Map<number, collectionJSON>, import.meta.glob("../FY_Million_Seller_Titles/*.json", { import: "default", eager: true }), "Ascending")
 
@@ -60,11 +61,41 @@ export function fyTitlesMake(obj: collectionJSON) {
         rank: undefined,
         yearsCount: undefined,
     }
+
+    obj.titles.flat().map((elem, index, array) => {
+
+        titles.set(titles.size, {
+            title: elem.name,
+            platform: elem.platform,
+            fiscalYear: obj.fiscalYear,
+            footnote: elem.miscellaneous,
+            regionData: [
+              {
+                region: elem.regionA,
+                Q1QtrValue: nothingCheck(elem.Q1CmlValueA,"Quarter","units","1st Quarter","1st Quarter","Current FY FCST",obj.fiscalYear),
+                Q2QtrValue: quarterlyCalculationV2(
+                    nothingCheck(elem.Q2CmlValueA, "Quarter", "units", "2nd Quarter", "First Half", "Current FY FCST", obj.fiscalYear),
+                    nothingCheck(elem.Q1CmlValueA, "Quarter", "units", "1st Quarter", "1st Quarter", "Current FY FCST", obj.fiscalYear)),
+                Q3QtrValue: quarterlyCalculationV2(
+                    nothingCheck(elem.Q3CmlValueA, "Quarter", "units", "3rd Quarter", "First Three Quarters", "Current FY FCST", obj.fiscalYear),
+                    nothingCheck(elem.Q2CmlValueA, "Quarter", "units", "2nd Quarter", "First Half", "Current FY FCST", obj.fiscalYear)),
+                Q4QtrValue: quarterlyCalculationV2(
+                    nothingCheck(elem.Q4CmlValueA, "Quarter", "units", "4th Quarter", "FY Cumulative", "Current FY FCST", obj.fiscalYear),
+                    nothingCheck(elem.Q3CmlValueA, "Quarter", "units", "3rd Quarter", "First Three Quarters", "Current FY FCST", obj.fiscalYear),
+                    nothingCheck(elem.Q2CmlValueA, "Quarter", "units", "2nd Quarter", "First Half", "Current FY FCST", obj.fiscalYear)),
+                Q1CmlValue: nothingCheck(elem.Q1CmlValueA,"Cumulative","units","1st Quarter","1st Quarter","Current FY FCST",obj.fiscalYear),
+                Q2CmlValue: nothingCheck(elem.Q2CmlValueA,"Cumulative","units","2nd Quarter","First Half","Current FY FCST",obj.fiscalYear),
+                Q3CmlValue: nothingCheck(elem.Q3CmlValueA,"Cumulative","units","3rd Quarter","First Three Quarters","Current FY FCST",obj.fiscalYear),
+                Q4CmlValue: nothingCheck(elem.Q4CmlValueA,"Cumulative","units","4th Quarter","FY Cumulative","Current FY FCST",obj.fiscalYear),
+              } satisfies TitleRegionData  
+            ],
+        } satisfies FiscalYearMillionSellersTitle)
+    })
 }
 
 function gameSeriesSalesMaker(completeCollection: Map<number, collectionJSON>) {
 
     const makeData = new Map<number, FiscalYearMillionSellersTitle[]>();
-    completeCollection.forEach((value, key, map) => makeData.set(key, ))
+    // completeCollection.forEach((value, key, map) => makeData.set(key, ))
 
 }
