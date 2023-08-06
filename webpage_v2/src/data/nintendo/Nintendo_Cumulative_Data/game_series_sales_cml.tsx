08d2e6b6de94e02a6a311b5badfc6a912412f4e8
r134x7 <r134x7@gmx.com>
2023-08-07 02:52:54 +1000

    setting up data structure

--- webpage_v2/src/data/nintendo/Nintendo_Cumulative_Data/game_series_sales_cml.tsx
+++ webpage_v2/src/data/nintendo/Nintendo_Cumulative_Data/game_series_sales_cml.tsx
@@ -5,7 +5,7 @@ import { Header, Titles, decimateCalculation } from "../../../utils/fy_million_s
 import { searchTitles } from "../../capcom/platinum_titles_Capcom";
 import { EarningsValue } from "../../../utils/general_earnings_logic";
 
-const totalCollection: collectionJSON[] = [...globImport(new Map<number, collectionJSON>, import.meta.glob("../FY_Million_Seller_Titles/*.json", { import: "default", eager: true }), "Ascending").values()]
+const totalCollection: Map<number, collectionJSON> = globImport(new Map<number, collectionJSON>, import.meta.glob("../FY_Million_Seller_Titles/*.json", { import: "default", eager: true }), "Ascending")
 
 export type FiscalYearMillionSellersTitle = {
     title: string,
@@ -28,4 +28,43 @@ export type TitleRegionData = {
     Q2CmlValue: EarningsValue,
     Q3CmlValue: EarningsValue,
     Q4CmlValue: EarningsValue,
+}
+
+export type TopSellingTitles = {
+    title: string,
+    platform: string,
+    Q1QtrValue: EarningsValue,
+    Q2QtrValue: EarningsValue,
+    Q3QtrValue: EarningsValue,
+    Q4QtrValue: EarningsValue,
+    Q1CmlValue: EarningsValue,
+    Q2CmlValue: EarningsValue,
+    Q3CmlValue: EarningsValue,
+    Q4CmlValue: EarningsValue,
+    rank?: number,
+    footnote?: string,
+    fiscalYear?: string
+}
+
+export function fyTitlesMake(obj: collectionJSON) {
+
+    const titles = new Map<number, FiscalYearMillionSellersTitle>();
+
+    const makeTitle: FiscalYearMillionSellersTitle = {
+        title: "",
+        platform: "",
+        regionData: [],
+        fiscalYear: obj.fiscalYear,
+        footnote: "",
+        label: undefined,
+        rank: undefined,
+        yearsCount: undefined,
+    }
+}
+
+function gameSeriesSalesMaker(completeCollection: Map<number, collectionJSON>) {
+
+    const makeData = new Map<number, FiscalYearMillionSellersTitle[]>();
+    completeCollection.forEach((value, key, map) => makeData.set(key, ))
+
 }
