b9b86f1a3c34ebd7e53377f2669f951c8d8394f5
r134x7 <r134x7@gmx.com>
2023-11-07 18:32:37 +1100

    added top selling titles wii u fy3/2024 data

--- webpage_v2/src/htmlFiles/html_to_json_Nintendo.js
+++ webpage_v2/src/htmlFiles/html_to_json_Nintendo.js
@@ -6,8 +6,8 @@ let currentQuarter = Number(process.argv[2]);
 
 let platformInput = process.argv[3];
 
-// let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2023/nintendo_switch/"
-let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_switch/"
+// let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_switch/"
+let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/wii_u/"
 
 function platformMake (platformInputLocal) {
 
