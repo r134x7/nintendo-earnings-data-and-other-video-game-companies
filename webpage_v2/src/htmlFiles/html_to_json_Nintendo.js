102e4f3c73087f9e34ff44c7dd076a8273b1c7ff
r134x7 <r134x7@gmx.com>
2024-05-07 17:53:54 +1000

    added nintendo 3ds top selling titles data nintendo fy3/24 4th quarter

--- webpage_v2/src/htmlFiles/html_to_json_Nintendo.js
+++ webpage_v2/src/htmlFiles/html_to_json_Nintendo.js
@@ -6,9 +6,9 @@ let currentQuarter = Number(process.argv[2]);
 
 let platformInput = process.argv[3];
 
-let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_switch/"
+// let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_switch/"
 // let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/wii_u/"
-// let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_3ds/"
+let filePathRead = "nintendo_top_selling_titles_data/nintendo_top_selling_titles_fy3_2024/nintendo_3ds/"
 
 function platformMake (platformInputLocal) {
 
