e5479fa77e7ea09873715a00600074856f1e8c71
r134x7 <r134x7@gmx.com>
2024-05-13 17:20:18 +1000

    added square enix q4 fy3/24 earnings data

--- webpage_v2/src/pages/Home.tsx
+++ webpage_v2/src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 
     const koeiTecmoStatus = liner(printTextBlock("Koei Tecmo page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 4th Quarter earnings data for FY3/2024.",40),"−","both",true,40);
 
-    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 3rd Quarter earnings data for FY3/2024.",40),"−","both",true,40);
+    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 4th Quarter earnings data for FY3/2024.",40),"−","both",true,40);
 
     const eventsStatus = liner(printTextBlock("Events page status",32),"=","top",true,32) + liner(printTextBlock("Page update: Updated calendar for 19 of 21 companies from April to May 2024.",40),"−","both",true,40);
 
