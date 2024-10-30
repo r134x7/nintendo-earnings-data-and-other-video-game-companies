dd66388bcad089ec66f092bd1b4ec8ed7eabb2c1
r134x7 <r134x7@gmx.com>
2024-10-31 01:29:07 +1100

    added fy9_2024 cyberagent 4th quarter earnings data

--- webpage_v2/src/pages/Home.tsx
+++ webpage_v2/src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home = () => {
 
     const konamiStatus = liner(printTextBlock("Konami page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2025.",40),"−","both",true,40);
 
-    const cyberAgentStatus = liner(printTextBlock("CyberAgent page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 3rd Quarter earnings data for FY9/2024.",40),"−","both",true,40); 
+    const cyberAgentStatus = liner(printTextBlock("CyberAgent page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 4th Quarter earnings data for FY9/2024.",40),"−","both",true,40); 
 
     const kadokawaStatus = liner(printTextBlock("Kadokawa page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2025.",40),"−","both",true,40); 
 
