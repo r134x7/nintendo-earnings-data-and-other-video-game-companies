15c405488b45b493a71ee12f75fff292a951056b
r134x7 <r134x7@gmx.com>
2025-08-08 02:18:52 +1000

    added fy3_2026 q1 kadokawa earnings data

--- webpage_v2/src/pages/Home.tsx
+++ webpage_v2/src/pages/Home.tsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     const cyberAgentStatus = liner(printTextBlock("CyberAgent page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 2nd Quarter earnings data for FY9/2025.",40),"−","both",true,40); 
 
-    const kadokawaStatus = liner(printTextBlock("Kadokawa page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 4th Quarter earnings data for FY3/2025.",40),"−","both",true,40); 
+    const kadokawaStatus = liner(printTextBlock("Kadokawa page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2026.",40),"−","both",true,40); 
 
     const eventsStatus = liner(printTextBlock("Events page status",32),"=","top",true,32) + liner(printTextBlock("Page update: Updated calendar for 19 of 22 companies from July to August 2025.",40),"−","both",true,40);
 
