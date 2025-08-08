1413376cea86231416ebeeccecf520b10800553c
r134x7 <r134x7@gmx.com>
2025-08-09 01:24:26 +1000

    added q1 fy3_2026 square enix earnings data

--- webpage_v2/src/pages/Home.tsx
+++ webpage_v2/src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 
     const koeiTecmoStatus = liner(printTextBlock("Koei Tecmo page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2026.",40),"−","both",true,40);
 
-    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 4th Quarter earnings data for FY3/2025, added annual report 2024.",40),"−","both",true,40);
+    const squareEnixStatus = liner(printTextBlock("Square Enix page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2026.",40),"−","both",true,40);
 
     const konamiStatus = liner(printTextBlock("Konami page status",32),"=","top",true,32) + liner(printTextBlock("Latest update: Added 1st Quarter earnings data for FY3/2026.",40),"−","both",true,40);
 
